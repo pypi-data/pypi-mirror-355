b56434d003aa4b2c4c1daf600bf921329631f76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:26 +0000

    Add package swiss-pollen/0.1.1/swiss-pollen-0.1.1.tar.gz

---
+++ packages/swiss-pollen/swiss-pollen-0.1.1.tar.gz/swiss-pollen-0.1.1/swiss_pollen/main.py
@@ -0,0 +1,21 @@
+from swiss_pollen import (PollenService, Plant)
+
+
+def print_pollen_data(pollen_data):
+    for station in pollen_data.keys():
+        print(f"* {station}")
+        for measurement in pollen_data.get(station):
+            print(f" - {measurement}")
+
+
+def main():
+    # get pollen data for all available plants
+    print_pollen_data(PollenService.current_values())
+    print()
+
+    # get pollen data for a restricted list of plants
+    print_pollen_data(PollenService.current_values(plants=[Plant.HAZEL, Plant.GRASSES]))
+
+
+if __name__ == "__main__":
+    main()
