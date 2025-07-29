0968d0b1edbdf64354c61589c592d0901fc75ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:34 +0000

    Add package herokulls/0.1.12/herokulls-0.1.12-cp310-cp310-manylinux2014_aarch64.whl

---
+++ packages/herokulls/herokulls-0.1.12-cp310-cp310-manylinux2014_aarch64.whl/herokulls/methods/utilities/start.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from ...exceptions import HerokullsAlreadyRunning
+from ...scaffold import Scaffold
+
+
+class Start(Scaffold):
+    async def start(self):
+        """Start the client.
+
+        This method start and then connects to the NodeJS core.
+
+        Raises:
+            herokullsAlreadyRunning: In case you try
+                to start an already started client.
+
+        Example:
+            .. code-block:: python
+                :emphasize-lines: 5
+
+                from herokulls import Client
+                from herokulls import idle
+                ...
+                app = Client(client)
+                app.start()
+
+                ...  # Call API methods
+
+                idle()
+        """
+        if not self._is_running:
+            self._is_running = True
+            loop = asyncio.get_running_loop()
+            self._wait_until_run = loop.create_future()
+            self._env_checker.check_environment()
+            await self._init_mtproto()
+            self._handle_mtproto()
+            await self._start_binding()
+        else:
+            raise HerokullsAlreadyRunning()
