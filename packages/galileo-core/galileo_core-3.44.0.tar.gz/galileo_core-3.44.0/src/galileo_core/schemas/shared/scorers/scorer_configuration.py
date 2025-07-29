f50feed62874477fc0cdeeb453d0a18d2bf9bbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:00 +0000

    Add package galileo-core/3.44.0/galileo_core-3.44.0.tar.gz

---
+++ packages/galileo-core/galileo_core-3.44.0.tar.gz/galileo_core-3.44.0/src/galileo_core/schemas/shared/scorers/scorer_configuration.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from galileo_core.schemas.shared.scorers.base_configs import (
+    FinetunedScorerConfig,
+    GeneratedScorerConfig,
+    RegisteredScorerConfig,
+)
+from galileo_core.schemas.shared.scorers.scorers import GalileoScorer
+
+
+class ScorerConfiguration(BaseModel):
+    scorers: List[GalileoScorer] = Field(default_factory=list, description="List of Galileo scorers to enable.")
+    registered_scorers: List[RegisteredScorerConfig] = Field(
+        default_factory=list, description="List of registered scorers to enable."
+    )
+    generated_scorers: List[GeneratedScorerConfig] = Field(
+        default_factory=list, description="List of generated scorers to enable."
+    )
+    finetuned_scorers: List[FinetunedScorerConfig] = Field(
+        default_factory=list, description="List of finetuned scorers to enable."
+    )
