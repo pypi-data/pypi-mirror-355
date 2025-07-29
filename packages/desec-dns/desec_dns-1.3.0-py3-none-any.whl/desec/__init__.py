6dceb3f1133cc3fdcdf9e670bc2dc37746245684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:03 +0000

    Add package desec-dns/1.3.0/desec_dns-1.3.0-py3-none-any.whl

---
+++ packages/desec-dns/desec_dns-1.3.0-py3-none-any.whl/desec/__init__.py
@@ -0,0 +1,26 @@
+"""Simple API client for desec.io.
+
+It consists of a python module and a CLI tool.
+For more information on the CLI, run 'desec --help'.
+For more information on the module's classes and functions, refer to the respective
+docstrings.
+"""
+
+from __future__ import annotations
+
+import typing as t
+
+import desec.types
+
+# For backwards compatibility, we import submodule content into the top-level scope.
+# To be removed in version 2.0.
+from desec.api import *  # noqa: F403
+from desec.exceptions import *  # noqa: F403
+from desec.tlsa import *  # noqa: F403
+from desec.types import *  # noqa: F403
+from desec.utils import *  # noqa: F403
+
+__version__ = "1.3.0"
+
+
+RECORD_TYPES = t.get_args(desec.types.DnsRecordTypeType)
