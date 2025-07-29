2510056b26d0730ea7791fbcaa5a0852709884c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:26 +0000

    Add package benigne/0.0.6/benigne-0.0.6-py3-none-any.whl

---
+++ packages/benigne/benigne-0.0.6-py3-none-any.whl/benigne/__init__.py
@@ -0,0 +1,2 @@
+from .ft_filter import ft_filter
+from .get_args import get_args
---
+++ packages/benigne/benigne-0.0.6-py3-none-any.whl/benigne/get_args.py
@@ -0,0 +1,6 @@
+import sys
+
+def get_args() -> list:
+    """Get command line arguments.
+    Return a list."""
+    return sys.argv[1:]
