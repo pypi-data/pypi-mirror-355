e5d2b16d8ca14ea75627e44ea82d40e558bd87a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:08 +0000

    Add package archinstall/3.0.8/archinstall-3.0.8-py3-none-any.whl

---
+++ packages/archinstall/archinstall-3.0.8-py3-none-any.whl/archinstall/default_profiles/servers/docker.py
@@ -0,0 +1,31 @@
+from typing import TYPE_CHECKING, override
+
+from archinstall.default_profiles.profile import Profile, ProfileType
+
+if TYPE_CHECKING:
+	from archinstall.lib.installer import Installer
+
+
+class DockerProfile(Profile):
+	def __init__(self) -> None:
+		super().__init__(
+			'Docker',
+			ProfileType.ServerType,
+		)
+
+	@property
+	@override
+	def packages(self) -> list[str]:
+		return ['docker']
+
+	@property
+	@override
+	def services(self) -> list[str]:
+		return ['docker']
+
+	@override
+	def post_install(self, install_session: 'Installer') -> None:
+		from archinstall.lib.args import arch_config_handler
+
+		for user in arch_config_handler.config.users:
+			install_session.arch_chroot(f'usermod -a -G docker {user.username}')
