4a934a728093cca5feff4a60292e6f569274d884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:13 +0000

    Add package ex-bushra/0.0/ex_bushra-0.0.tar.gz

---
+++ packages/ex-bushra/ex_bushra-0.0.tar.gz/ex_bushra-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ex_bushra',
+      version='0.0',
+      description='ex_bushra',
+      packages=['ex_bushra'],
+      author_email='ilapronin0700@gmail.com',
+      zip_safe=False)
