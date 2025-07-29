11792abb8e2af4d770b036a47373e2e290c25534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:18 +0000

    Add package ir-support/1.2.0/ir_support-1.2.0-py3-none-any.whl

---
+++ packages/ir-support/ir_support-1.2.0-py3-none-any.whl/ir_support/functions/__init__.py
@@ -0,0 +1,11 @@
+from .draw_possible_ellipse_given_foci import draw_possible_ellipse_given_foci
+from .line_plane_intersection import line_plane_intersection
+from .make_ellipsoid import make_ellipsoid
+from .tranimate_custom import tranimate_custom
+from .orthogonalize_rotation import orthogonalize_rotation
+
+__all__ = ["draw_possible_ellipse_given_foci",
+           "line_plane_intersection",
+           "make_ellipsoid",
+           "tranimate_custom",
+           "orthogonalize_rotation"]
