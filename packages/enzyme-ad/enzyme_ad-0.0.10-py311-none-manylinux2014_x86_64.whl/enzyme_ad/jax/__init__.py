88d0e378919b0dd85dbe60a7d0a269fb442f53aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:51 +0000

    Add package enzyme-ad/0.0.10/enzyme_ad-0.0.10-py311-none-manylinux2014_x86_64.whl

---
+++ packages/enzyme-ad/enzyme_ad-0.0.10-py311-none-manylinux2014_x86_64.whl/enzyme_ad/jax/__init__.py
@@ -0,0 +1,12 @@
+from enzyme_ad.jax.primitives import (
+    cpp_call,
+    ffi_call,
+    hlo_call,
+    enzyme_jax_ir,
+    XLAPipeline,
+    JaXPipeline,
+    optimize_module,
+    export,
+    full_optimization_pass_pipeline,
+    optimization_passes,
+)
