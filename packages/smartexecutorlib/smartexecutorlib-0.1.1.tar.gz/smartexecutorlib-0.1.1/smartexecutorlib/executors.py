6cecc0344afb7112c587b62317a90a8de6f2c4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:33 +0000

    Add package smartexecutorlib/0.1.1/smartexecutorlib-0.1.1.tar.gz

---
+++ packages/smartexecutorlib/smartexecutorlib-0.1.1.tar.gz/smartexecutorlib-0.1.1/smartexecutorlib/executors.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2025, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+import os
+import subprocess
+
+
+class SubExecutor:
+
+    @classmethod
+    def execute(cls, command: str):
+        p = subprocess.Popen(command, shell=True, stderr=subprocess.DEVNULL)
+        status = p.wait()
+        return not bool(status)
+
+
+class OsExecutor:
+    @classmethod
+    def execute(cls, command: str):
+        status = os.system(command)
+        return not bool(status)
+
+
+class SmartExecutor:
+    os = OsExecutor()
+    sub = SubExecutor()
+
+
+class ExecutorsFactory:
+    @classmethod
+    def get_os_executor(cls):
+        return OsExecutor()
+
+    @classmethod
+    def get_sub_executor(cls):
+        return SubExecutor()
+
+    @classmethod
+    def get_executor(cls):
+        return SmartExecutor()
