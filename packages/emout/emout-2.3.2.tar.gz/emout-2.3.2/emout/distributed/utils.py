4863e2bc0157030c89ce8071a52209b87fa8eed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package emout/2.3.2/emout-2.3.2.tar.gz

---
+++ packages/emout/emout-2.3.2.tar.gz/emout-2.3.2/emout/distributed/utils.py
@@ -0,0 +1,20 @@
+def run_backend(func, *args, **kwargs):
+    """
+    • compute=True: 即 execute(func) して結果を返す
+    • compute=False: Dask Future を返す (Client が無ければ即時実行して返す)
+    """
+    from dask import delayed
+    from dask.distributed import default_client
+
+    try:
+        client = default_client()
+    except ValueError:
+        client = None
+
+    if client is None:
+        # Dask Client が存在しなければ同期実行
+        return func(*args, **kwargs)
+
+    task = delayed(func)(*args, **kwargs)
+    future = client.compute(task)
+    return future.result()
