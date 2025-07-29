f5ced8d59ac6683833a31469dc7070978cdfa726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:41 +0000

    Add package types-aiobotocore-lite/2.23.0/types_aiobotocore_lite-2.23.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-lite/types_aiobotocore_lite-2.23.0-py3-none-any.whl/aiobotocore-stubs/stub.pyi
@@ -0,0 +1,22 @@
+"""
+Type annotations for aiobotocore.stub module.
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from typing import Any, Mapping
+
+from botocore.stub import Stubber
+
+class AioStubber(Stubber):
+    def add_client_error(
+        self,
+        method: str,
+        service_error_code: str = ...,
+        service_message: str = ...,
+        http_status_code: int = ...,
+        service_error_meta: Mapping[str, Any] | None = ...,
+        expected_params: Mapping[str, Any] | None = ...,
+        response_meta: Mapping[str, Any] | None = ...,
+        modeled_fields: Mapping[str, Any] | None = ...,
+    ) -> None: ...
