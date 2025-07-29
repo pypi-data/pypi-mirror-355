dd94cf73c75b02430956910656fcdc0bfd03be6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:13 +0000

    Add package pdf4llm/0.0.25/pdf4llm-0.0.25.tar.gz

---
+++ packages/pdf4llm/pdf4llm-0.0.25.tar.gz/pdf4llm-0.0.25/setup.py
@@ -0,0 +1,40 @@
+import os
+
+import setuptools
+
+setup_py_cwd = os.path.dirname(__file__)
+with open(os.path.join(setup_py_cwd, "README.md"), encoding="utf-8") as f:
+    readme = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Topic :: Utilities",
+]
+requires = ["pymupdf4llm==0.0.25"]
+
+setuptools.setup(
+    name="pdf4llm",
+    version="0.0.25",
+    author="Artifex",
+    author_email="support@artifex.com",
+    description="PyMuPDF Utilities for LLM/RAG",
+    packages=setuptools.find_packages(),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    license="GNU AFFERO GPL 3.0",
+    url="https://github.com/pymupdf/RAG",
+    classifiers=classifiers,
+    package_data={
+        "pdf4llm": ["LICENSE"],
+    },
+    project_urls={
+        "Documentation": "https://pymupdf.readthedocs.io/",
+        "Source": "https://github.com/pymupdf/RAG/tree/main/pdf4llm/pdf4llm",
+        "Tracker": "https://github.com/pymupdf/RAG/issues",
+        "Changelog": "https://github.com/pymupdf/RAG/blob/main/CHANGES.md",
+    },
+)
