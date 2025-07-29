02cfcf512f087b5d8a2a725f36811bc8fe35f687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:41 +0000

    Add package types-aiobotocore-lite/2.23.0/types_aiobotocore_lite-2.23.0.tar.gz

---
+++ packages/types-aiobotocore-lite/types_aiobotocore_lite-2.23.0.tar.gz/types_aiobotocore_lite-2.23.0/aiobotocore-stubs/eventstream.pyi
@@ -0,0 +1,17 @@
+"""
+Type annotations for aiobotocore.eventstream module.
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from botocore.eventstream import EventStream
+
+_T = TypeVar("_T")
+
+class AioEventStream(EventStream[_T], Generic[_T]):
+    def __iter__(self) -> Iterator[_T]: ...
+    def __aiter__(self) -> AsyncIterator[_T]: ...
+    async def __anext__(self) -> _T: ...
+    async def get_initial_response(self) -> _T: ...  # type: ignore[override]
