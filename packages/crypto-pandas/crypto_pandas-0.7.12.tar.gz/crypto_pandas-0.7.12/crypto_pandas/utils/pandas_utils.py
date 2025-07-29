8aaff2ebad06d9466dd33991210f25c5ef6aac21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:18 +0000

    Add package crypto-pandas/0.7.12/crypto_pandas-0.7.12.tar.gz

---
+++ packages/crypto-pandas/crypto_pandas-0.7.12.tar.gz/crypto_pandas-0.7.12/crypto_pandas/utils/pandas_utils.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import pandera as pa
+
+
+def format_timestamp(timestamp: int | pd.Timestamp | dict | str | None) -> pd.Timestamp:
+    now = pd.Timestamp.now(tz="UTC")
+    if isinstance(timestamp, dict):
+        timestamp = now + pd.DateOffset(**timestamp)
+    elif isinstance(timestamp, str):
+        timestamp = now + pd.Timedelta(timestamp)
+    return timestamp
+
+
+def timestamp_to_int(timestamp: int | pd.Timestamp | dict | str | None) -> int:
+    timestamp = format_timestamp(timestamp)
+    if isinstance(timestamp, pd.Timestamp):
+        timestamp = int(timestamp.timestamp() * 1000)
+    return timestamp
+
+
+def date_time_fields_to_int_str(data: dict) -> dict:
+    def transform_value(value):
+        if isinstance(value, pd.Timestamp):
+            return str(int(value.timestamp() * 1000))
+        elif isinstance(value, dict):
+            return date_time_fields_to_int_str(value)
+        elif isinstance(value, list):
+            return [transform_value(v) for v in value]
+        return value
+
+    return {key: transform_value(value) for key, value in data.items()}
+
+
+def date_time_columns_to_int_str(data: pd.DataFrame) -> pd.DataFrame:
+    columns = (
+        data.select_dtypes("datetimetz").columns.tolist()
+        + data.select_dtypes("datetime").columns.tolist()
+    )
+    data[columns] = (data[columns].astype("int64") // 10**6).astype(str)
+    return data
+
+
+def expand_dict_columns(data: pd.DataFrame, separator: str = ".") -> pd.DataFrame:
+    data = data.reset_index(drop=True)
+    dict_columns = [
+        x for x in data.columns if all(data[x].apply(lambda y: isinstance(y, dict)))
+    ]
+    columns_list = [data.drop(columns=dict_columns).copy()]
+    for dict_column in dict_columns:
+        exploded_column = pd.json_normalize(data[dict_column])
+        exploded_column.columns = [
+            f"{dict_column}{separator}{x}" for x in exploded_column.columns
+        ]
+        columns_list.append(exploded_column.copy())
+    return pd.concat(columns_list, axis=1)
+
+
+def determine_mandatory_optional_fields_pandera(model: pa.DataFrameModel) -> dict:
+    schema = model.to_schema()
+    fields = {"mandatory": [], "optional": []}
+    for col_name, col_obj in schema.columns.items():
+        if col_obj.nullable:
+            fields["optional"].append(col_name)
+        else:
+            fields["mandatory"].append(col_name)
+    return fields
+
+
+def combine_params(row: pd.Series, param_cols: list) -> dict:
+    return {
+        column.replace("params.", ""): row[column]
+        for column in param_cols
+        if pd.notnull(row[column])
+    }
