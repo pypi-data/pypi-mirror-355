19dc9668fa3ddd6ba23d2eae8cc3411d284977b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:08 +0000

    Add package limits/5.3.0/limits-5.3.0.tar.gz

---
+++ packages/limits/limits-5.3.0.tar.gz/limits-5.3.0/tests/test_limits.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from collections import defaultdict
+
+from limits import limits
+
+
+class TestLimits:
+    class FakeLimit(limits.RateLimitItem):
+        GRANULARITY = limits.Granularity(1, "fake")
+
+    class OtherFakeLimit(limits.RateLimitItem):
+        GRANULARITY = limits.Granularity(1, "otherfake")
+
+    def test_key_all_strings_default_namespace(self):
+        item = self.FakeLimit(1, 1)
+        assert item.key_for("a", "b", "c") == "LIMITER/a/b/c/1/1/fake"
+
+    def test_key_with_none_default_namespace(self):
+        item = self.FakeLimit(1, 1)
+        assert item.key_for("a", None, None) == "LIMITER/a/None/None/1/1/fake"
+
+    def test_key_with_int_default_namespace(self):
+        item = self.FakeLimit(1, 1)
+        assert item.key_for("a", 1) == "LIMITER/a/1/1/1/fake"
+
+    def test_key_with_mixed_string_types_default_namespace(self):
+        item = self.FakeLimit(1, 1)
+        assert item.key_for(b"a", "b") == "LIMITER/a/b/1/1/fake"
+
+    def test_equality(self):
+        item = self.FakeLimit(1, 1)
+        assert item == self.FakeLimit(1, 1)
+        assert item != self.FakeLimit(1, 2)
+        assert item != self.FakeLimit(2, 1)
+        assert item != "someething else"
+
+    def test_hashabilty(self):
+        mapping = defaultdict(lambda: 1)
+        mapping[self.FakeLimit(1, 1)] += 1
+        mapping[self.FakeLimit(1, 1)] += 1
+        mapping[self.FakeLimit(1, 2)] += 1
+        mapping[self.FakeLimit(1, 2)] += 1
+        mapping[self.OtherFakeLimit(1, 2)] += 1
+
+        assert len(mapping) == 3
