3d1670cd2d0ab6081d5402c58775e203b561e67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:36 +0000

    Add package iban-oplata/0.0.2/iban_oplata-0.0.2-py3-none-any.whl

---
+++ packages/iban-oplata/iban_oplata-0.0.2-py3-none-any.whl/iban_oplata/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for IBAN Oplata API wrapper."""
+
+__author__ = 'Dmytro Litvinov'
+__email__ = 'me@dmytrolitvinov.com'
+__version__ = '0.0.2'
+
+from .client import IBANOplataAPIClient
