c6d2ef26b1ce7ffcc0049057d83151f0110af6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:23 +0000

    Add package xrl-python/0.2.0/xrl_python-0.2.0-py3-none-any.whl

---
+++ packages/xrl-python/xrl_python-0.2.0-py3-none-any.whl/xrl/__init__.py
@@ -0,0 +1,15 @@
+"""
+XRL (eXtended Rate Limiter) - A distributed rate limiting library for Python.
+
+This library provides multiple rate limiting algorithms implemented using Redis
+for distributed environments.
+"""
+
+from .token_bucket import TokenBucketRateLimiter
+from .fixed_window import FixedWindowRateLimiter
+
+__version__ = "0.2.0"
+__all__ = [
+    "TokenBucketRateLimiter",
+    "FixedWindowRateLimiter",
+]
