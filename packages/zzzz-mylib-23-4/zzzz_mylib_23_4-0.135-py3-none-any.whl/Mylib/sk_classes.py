a64a33e4c6da5499fe385a53bcbe2f8da3afd985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:49 +0000

    Add package zzzz-mylib-23-4/0.135/zzzz_mylib_23_4-0.135-py3-none-any.whl

---
+++ packages/zzzz-mylib-23-4/zzzz_mylib_23_4-0.135-py3-none-any.whl/Mylib/sk_classes.py
@@ -0,0 +1,44 @@
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
+from sklearn.ensemble import BaggingClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+class LRBaggingClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(
+        self,
+        penalty,
+        solver,
+        C,
+        l1_ratio,
+        max_iter,
+        warm_start,
+        n_estimators,
+        max_samples,
+        max_features,
+    ):
+        lr = LogisticRegression(
+            penalty=penalty,
+            solver=solver,
+            C=C,
+            l1_ratio=l1_ratio,
+            max_iter=max_iter,
+            warm_start=warm_start,
+        )
+        self.model = BaggingClassifier(
+            base_estimator=lr,
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=max_features,
+        )
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+        self.is_fitted_ = True
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
