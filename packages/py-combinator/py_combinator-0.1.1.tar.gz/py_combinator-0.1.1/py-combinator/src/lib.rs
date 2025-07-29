f079de000fccdc18c3d092af31b23418a2a60d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:24 +0000

    Add package py-combinator/0.1.1/py_combinator-0.1.1.tar.gz

---
+++ packages/py-combinator/py_combinator-0.1.1.tar.gz/py_combinator-0.1.1/py-combinator/src/lib.rs
@@ -0,0 +1,42 @@
+#![warn(clippy::pedantic)]
+
+mod base_iterator;
+mod double_ended_iterator;
+mod exact_size_iterator;
+mod list_iterator;
+mod sized_double_ended_iterator;
+
+mod iterators {
+    #[allow(unused_imports)]
+    pub use crate::{
+        base_iterator::PyBaseIterator, double_ended_iterator::PyDoubleEndedIterator,
+        exact_size_iterator::PyExactSizeIterator,
+        sized_double_ended_iterator::PySizedDoubleEndedIterator,
+    };
+}
+
+use pyo3::{IntoPyObjectExt, exceptions::PyTypeError, prelude::*, types::PyList};
+
+#[pyfunction]
+fn iterator_from(iterable: &Bound<'_, PyAny>) -> PyResult<Py<PyAny>> {
+    Python::with_gil(|py| {
+        if let Ok(list) = iterable.downcast::<PyList>() {
+            let list_iter = list_iterator::PyListIterator::new(list);
+            sized_double_ended_iterator::PySizedDoubleEndedIterator::new(Box::new(list_iter))
+                .into_py_any(py)
+        } else {
+            Err(PyTypeError::new_err(format!(
+                "Cannot construct iterator from type {}",
+                iterable.get_type().name()?
+            )))
+        }
+    })
+}
+
+#[pymodule]
+#[allow(clippy::unnecessary_wraps)]
+fn _py_combinator(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    let _ = m.add_class::<sized_double_ended_iterator::PySizedDoubleEndedIterator>();
+    let _ = m.add_function(wrap_pyfunction!(iterator_from, m)?);
+    Ok(())
+}
