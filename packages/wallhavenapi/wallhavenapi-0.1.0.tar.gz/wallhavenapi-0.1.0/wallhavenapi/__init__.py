7ea51f4bdf85db8b0dcba7b30a8dcbdb185b6e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:23 +0000

    Add package wallhavenapi/0.1.0/wallhavenapi-0.1.0.tar.gz

---
+++ packages/wallhavenapi/wallhavenapi-0.1.0.tar.gz/wallhavenapi-0.1.0/wallhavenapi/__init__.py
@@ -0,0 +1,18 @@
+from .wallhavenapi import WallhavenAPI, Category, Purity, Sorting, Order, TopRange, Color, Type, Seed
+from .wallhavenapi import RequestsLimitError, ApiKeyError, NoWallpaperError, UnhandledException
+
+__all__ = [
+    "WallhavenAPI",
+    "Category",
+    "Purity",
+    "Sorting",
+    "Order",
+    "TopRange",
+    "Color",
+    "Type",
+    "Seed",
+    "RequestsLimitError",
+    "ApiKeyError",
+    "NoWallpaperError",
+    "UnhandledException"
+]
