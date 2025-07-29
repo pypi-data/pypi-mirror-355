2403739c89892595a8a7bbc048d8edb6ea9fd7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:55 +0000

    Add package yplib/8.8.8/yplib-8.8.8-py3-none-any.whl

---
+++ packages/yplib/yplib-8.8.8-py3-none-any.whl/yplib/__init__.py
@@ -0,0 +1,30 @@
+# 基础的，通用的基础包
+from yplib.common_package import *
+
+# 引入自定义的全部的包
+from yplib.index import *
+
+# 有关文件操作类的包
+from yplib.file import *
+
+# 有关 图表的 html 代码的包
+from yplib.chart_html import *
+# 有关 图表的 操作类的包
+from yplib.chart import *
+
+# 有关 http 工具类的包
+from yplib.http_util import *
+
+# 有关 数据库操作的包
+from yplib.db import *
+
+# 有关 邮件 操作的包
+from yplib.mail import *
+from yplib.mail_html import *
+# 生成 markdown 的文本
+from yplib.markdown import *
+
+# 临时添加的工具类
+from yplib.temp import *
+# 多线程处理类的数据
+from yplib.multi_thread import *
