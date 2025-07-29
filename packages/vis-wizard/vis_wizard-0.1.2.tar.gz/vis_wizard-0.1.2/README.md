730d678943384f99ee43976eb42cdb3024d73051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:00 +0000

    Add package vis-wizard/0.1.2/vis_wizard-0.1.2.tar.gz

---
+++ packages/vis-wizard/vis_wizard-0.1.2.tar.gz/vis_wizard-0.1.2/README.md
@@ -0,0 +1,1 @@
+no readme file, but very soon.
---
+++ packages/vis-wizard/vis_wizard-0.1.2.tar.gz/vis_wizard-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vis_wizard',
+    version='0.1.2',
+    description='AI Data Visualization Made Easy',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Rayyan Ahmed',
+    author_email='rayyannabeel22@gmail.com',
+    # url='https://github.com/yourusername/vis_wizard',  # Optional but useful
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
