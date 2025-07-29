ae41ddaa5bf6a50864dd459d7a09268580b4c67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:15 +0000

    Add package pyams-content-themes/2.0.2/pyams_content_themes-2.0.2-py3-none-any.whl

---
+++ packages/pyams-content-themes/pyams_content_themes-2.0.2-py3-none-any.whl/pyams_content_themes/resources/src/js/pyams-darkgreen.js
@@ -0,0 +1,18 @@
+
+// Modules imports
+
+import "popper.js";
+import "bootstrap";
+import "moment";
+
+import "superfish";
+
+import "./app";
+
+
+// CSS imports
+
+import "../../../../../pkg/css/dev/pyams-darkgreen.css";
+
+
+/* Version: $version$ */
