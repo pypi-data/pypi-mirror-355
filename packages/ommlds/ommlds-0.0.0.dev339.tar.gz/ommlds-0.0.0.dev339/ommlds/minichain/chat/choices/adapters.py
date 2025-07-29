57d07b021102f607991b84b38ac6b16cdfb70f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:51 +0000

    Add package ommlds/0.0.0.dev339/ommlds-0.0.0.dev339.tar.gz

---
+++ packages/ommlds/ommlds-0.0.0.dev339.tar.gz/ommlds-0.0.0.dev339/ommlds/minichain/chat/choices/adapters.py
@@ -0,0 +1,25 @@
+from omlish import check
+from omlish import dataclasses as dc
+from omlish import lang
+
+from ...services import Response
+from ..messages import AiMessage
+from ..services import ChatRequest
+from ..services import ChatService
+from .services import ChatChoicesService
+from .types import ChatChoicesOutputs
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class ChatChoicesServiceChatService:
+    service: ChatChoicesService
+
+    def invoke(self, request: ChatRequest) -> Response[AiMessage, ChatChoicesOutputs]:
+        resp = self.service.invoke(request)
+        return Response(check.single(resp.v).m, resp.outputs)
+
+
+lang.static_check_issubclass[ChatService](ChatChoicesServiceChatService)
