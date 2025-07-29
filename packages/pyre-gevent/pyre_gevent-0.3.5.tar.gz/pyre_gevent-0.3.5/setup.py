72546589a44c411186c251de39d27162be630644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:54 +0000

    Add package pyre-gevent/0.3.5/pyre_gevent-0.3.5.tar.gz

---
+++ packages/pyre-gevent/pyre_gevent-0.3.5.tar.gz/pyre_gevent-0.3.5/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name='pyre-gevent',
+        version='0.3.5',
+        description='Python ZRE implementation (green version)',
+        author='Arnaud Loonstra',
+        author_email='arnaud@sphaero.org',
+        maintainer='Tanguy Bonneau',
+        maintainer_email='tanguy.bonneau@gmail.com',
+        url='http://www.github.com/tangb/pyre-gevent/',
+        packages=['pyre_gevent'],
+        include_package_data=True,
+        install_requires=['pyzmq', 'ipaddress', 'netaddr', 'netifaces-plus']
+)
---
+++ packages/pyre-gevent/pyre_gevent-0.3.5.tar.gz/pyre_gevent-0.3.5/tests/test_zhelper.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+sys.path.append('../')
+from pyre_gevent import zhelper
+from pyre_gevent import zbeacon
+import zmq
+import time
+
+class ZHelperTest(unittest.TestCase):
+    
+    def test_get_ifaddrs(self):
+        ifs = zhelper.get_ifaddrs()
+        self.assertIsInstance(ifs, list)
+        self.assertIsInstance(ifs[0], dict)
+    # end test_get_ifaddrs
+
+    def test_get_ifaddrs_loopback(self):
+        ifs = zhelper.get_ifaddrs()
+        addrs = []
+        for iface in ifs:
+            #print(iface)
+            for i,v in iface.items():
+                #print(i,v)
+                # 2 is for ipv4
+                if v.get(2):
+                    addrs.append(v[2]['addr'])
+        self.assertIn('127.0.0.1', addrs)
+    # end test_get_ifaddrs_loopback
+
+# end ZHelperTest
+
+if __name__ == '__main__':
+    unittest.main()
+
