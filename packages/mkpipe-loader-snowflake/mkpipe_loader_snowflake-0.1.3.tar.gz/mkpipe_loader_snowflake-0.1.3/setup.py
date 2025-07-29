bf2233f1d646927bbf88da9d93989f7514996a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:56 +0000

    Add package mkpipe-loader-snowflake/0.1.3/mkpipe_loader_snowflake-0.1.3.tar.gz

---
+++ packages/mkpipe-loader-snowflake/mkpipe_loader_snowflake-0.1.3.tar.gz/mkpipe_loader_snowflake-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkpipe-loader-snowflake',
+    version='0.1.3',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=['tests', 'scripts', 'deploy', 'install_jars.py']),
+    install_requires=['mkpipe'],
+    include_package_data=True,
+    entry_points={
+        'mkpipe.loaders': [
+            'snowflake = mkpipe_loader_snowflake:SnowflakeLoader',
+        ],
+    },
+    description='Snowflake loader for mkpipe.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Metin Karakus',
+    author_email='metin_karakus@yahoo.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.8',
+)
