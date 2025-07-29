9842c13d9654b8f145bd32aa051d59fc682cad1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package otmixmod/0.19/otmixmod-0.19-cp39-abi3-win_amd64.whl

---
+++ packages/otmixmod/otmixmod-0.19-cp39-abi3-win_amd64.whl/otmixmod/__init__.py
@@ -0,0 +1,19 @@
+"""
+    otmixmod --- The Mixmod interface for Open TURNS
+    ===================================================
+
+    Contents
+    --------
+      'otmixmod' is a python module for Open TURNS that allows to use
+      the Mixmod software to build Open TURNS objects.
+
+"""
+
+# flake8: noqa
+
+# ensures swig type tables order & dll load
+import openturns as _ot
+
+from .otmixmod import *
+
+__version__ = '0.19'
