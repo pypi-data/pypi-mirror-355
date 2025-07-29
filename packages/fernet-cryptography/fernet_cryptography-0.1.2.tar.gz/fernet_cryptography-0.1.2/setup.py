ed24eb90c01753a40d0014aa9c098fb62719a5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:35 +0000

    Add package fernet-cryptography/0.1.2/fernet_cryptography-0.1.2.tar.gz

---
+++ packages/fernet-cryptography/fernet_cryptography-0.1.2.tar.gz/fernet_cryptography-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fernet-cryptography',          # уникальное имя вашей библиотеки
+    version='0.1.2',
+    description='My asyncio-like library',
+    author='Ваше имя',
+    author_email='email@example.com',
+    packages=find_packages(),             # найдёт все пакеты, включая 'S', если есть __init__.py
+    include_package_data=True,            # позволяет включить файлы из MANIFEST.in
+    python_requires='>=3.6',
+)
