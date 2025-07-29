1c1f6e8f106d2a5b75050d4ba8d2e8a8b3486832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:50 +0000

    Add package ed-optimization-service/0.1.7/ed_optimization_service-0.1.7-py3-none-any.whl

---
+++ packages/ed-optimization-service/ed_optimization_service-0.1.7-py3-none-any.whl/ed_optimization/documentation/api/optimization_api_client.py
@@ -0,0 +1,28 @@
+from ed_domain.documentation.api.definitions import ApiResponse
+
+from ed_optimization.application.features.order.dtos import (
+    CalculateOrderDetailsDto, CreateOrderDto, RouteInformationDto)
+from ed_optimization.common.api_helpers import ApiClient
+from ed_optimization.documentation.api.abc_optimization_api_client import \
+    ABCOptimizationApiClient
+from ed_optimization.documentation.api.optimization_endpoint_descriptions import \
+    OptimizationEndpointDescriptions
+
+
+class OptimizationApiClient(ABCOptimizationApiClient):
+    def __init__(self, core_api: str) -> None:
+        self._endpoints = OptimizationEndpointDescriptions(core_api)
+
+    def create_order(self, create_order_dto: CreateOrderDto) -> ApiResponse[None]:
+        endpoint = self._endpoints.get_description("create_order")
+        api_client = ApiClient[None](endpoint)
+
+        return api_client({"request": create_order_dto})
+
+    def calcualte_order_details(
+        self, calculate_order_details_dto: CalculateOrderDetailsDto
+    ) -> ApiResponse[RouteInformationDto]:
+        endpoint = self._endpoints.get_description("calculate_order_details")
+        api_client = ApiClient[RouteInformationDto](endpoint)
+
+        return api_client({"request": calculate_order_details_dto})
