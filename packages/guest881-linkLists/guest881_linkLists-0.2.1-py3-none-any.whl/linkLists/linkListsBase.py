4bfdac483af6dd4849500efca666e8c03452410e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:22 +0000

    Add package guest881-linkLists/0.2.1/guest881_linkLists-0.2.1-py3-none-any.whl

---
+++ packages/guest881-linkLists/guest881_linkLists-0.2.1-py3-none-any.whl/linkLists/linkListsBase.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025/6/13 22:03
+@author: guest881
+"""
+class LinkListsBase:
+    def __init__(self, register,value) -> None:
+        self.register=register
+        self.value = value
+        self.next = ...
