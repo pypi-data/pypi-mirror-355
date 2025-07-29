4f6ddce9e98b4874e24050f7cdcd0f69c1200530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:24 +0000

    Add package eexp-engine/0.0.37/eexp_engine-0.0.37-py3-none-any.whl

---
+++ packages/eexp-engine/eexp_engine-0.0.37-py3-none-any.whl/eexp_engine/client.py
@@ -0,0 +1,87 @@
+from . import run_experiment
+from .executionware import proactive_runner as proactive_runner
+from .data_abstraction_layer.data_abstraction_api import set_data_abstraction_config, create_experiment
+import os
+import logging.config
+from . import exceptions
+
+logger = logging.getLogger(__name__)
+
+class Config:
+
+    def __init__(self, config):
+        self.TASK_LIBRARY_PATH = config.TASK_LIBRARY_PATH
+        self.EXPERIMENT_LIBRARY_PATH = config.EXPERIMENT_LIBRARY_PATH
+        self.DATASET_LIBRARY_RELATIVE_PATH = config.DATASET_LIBRARY_RELATIVE_PATH
+        self.PYTHON_DEPENDENCIES_RELATIVE_PATH = config.PYTHON_DEPENDENCIES_RELATIVE_PATH
+        if 'DATASET_MANAGEMENT' not in dir(config) or len(config.DATASET_MANAGEMENT) == 0:
+            raise exceptions.DatasetManagementNotSet(
+                "Please set the variable DATASET_MANAGEMENT in config.py to either \"LOCAL\" or \"ZENOH\"")
+        else:
+            self.DATASET_MANAGEMENT = config.DATASET_MANAGEMENT
+        if config.DATASET_MANAGEMENT == "ZENOH":
+            if 'ZENOH_URL' not in dir(config) or len(config.ZENOH_URL) == 0:
+                raise exceptions.DatasetManagementSetToZenohButNoURLProvided(
+                    "Please set the variable ZENOH_URL in config.py")
+        self.ZENOH_URL = config.ZENOH_URL if 'ZENOH_URL' in dir(config) else None
+        self.DATA_ABSTRACTION_BASE_URL = config.DATA_ABSTRACTION_BASE_URL
+        self.DATA_ABSTRACTION_ACCESS_TOKEN = config.DATA_ABSTRACTION_ACCESS_TOKEN
+        self.EXECUTIONWARE = config.EXECUTIONWARE
+        self.PROACTIVE_URL = config.PROACTIVE_URL
+        self.PROACTIVE_USERNAME = config.PROACTIVE_USERNAME
+        self.PROACTIVE_PASSWORD = config.PROACTIVE_PASSWORD
+        self.PROACTIVE_PYTHON_VERSIONS = config.PROACTIVE_PYTHON_VERSIONS if 'PROACTIVE_PYTHON_VERSIONS' in dir(config) else None
+        self.PYTHON_CONDITIONS = config.PYTHON_CONDITIONS if 'PYTHON_CONDITIONS' in dir(config) else None
+        self.PYTHON_CONFIGURATIONS = config.PYTHON_CONFIGURATIONS if 'PYTHON_CONFIGURATIONS' in dir(config) else None
+        if 'MAX_WORKFLOWS_IN_PARALLEL_PER_NODE' in dir(config):
+            logger.debug(f"Setting MAX_WORKFLOWS_IN_PARALLEL_PER_NODE to {config.MAX_WORKFLOWS_IN_PARALLEL_PER_NODE}")
+            self.MAX_WORKFLOWS_IN_PARALLEL_PER_NODE = config.MAX_WORKFLOWS_IN_PARALLEL_PER_NODE
+        else:
+            default_max_workflows_in_parallel_per_node = 1
+            logger.debug(f"Setting MAX_WORKFLOWS_IN_PARALLEL_PER_NODE to the default value of {default_max_workflows_in_parallel_per_node}")
+            self.MAX_WORKFLOWS_IN_PARALLEL_PER_NODE = default_max_workflows_in_parallel_per_node
+
+
+def run(runner_file, exp_name, config):
+    logger.info(f"abspath: {os.path.relpath(config.EXPERIMENT_LIBRARY_PATH)}")
+    logger.info(f"os.listdir: {os.listdir(config.EXPERIMENT_LIBRARY_PATH)}")
+
+    with open(os.path.join(config.EXPERIMENT_LIBRARY_PATH, exp_name + ".xxp"), 'r') as file:
+        workflow_specification = file.read()
+
+    if 'LOGGING_CONFIG' in dir(config):
+        logging.config.dictConfig(config.LOGGING_CONFIG)
+
+    new_exp = {
+        'name': exp_name,
+        'model': str(workflow_specification),
+    }
+
+    config_obj = Config(config)
+    set_data_abstraction_config(config_obj)
+
+    exp_id = create_experiment(new_exp, "dummy_user")
+
+    run_experiment(exp_id, workflow_specification, os.path.dirname(os.path.abspath(runner_file)), config_obj)
+
+    return exp_id
+
+
+def kill_job(job_id, config):
+    gateway = proactive_runner.create_gateway_and_connect_to_it(config.PROACTIVE_USERNAME, config.PROACTIVE_PASSWORD)
+    gateway.killJob(job_id)
+
+
+def pause_job(job_id, config):
+    gateway = proactive_runner.create_gateway_and_connect_to_it(config.PROACTIVE_USERNAME, config.PROACTIVE_PASSWORD)
+    gateway.pauseJob(job_id)
+
+
+def resume_job(job_id, config):
+    gateway = proactive_runner.create_gateway_and_connect_to_it(config.PROACTIVE_USERNAME, config.PROACTIVE_PASSWORD)
+    gateway.resumeJob(job_id)
+
+
+def kill_task(job_id, task_name, config):
+    gateway = proactive_runner.create_gateway_and_connect_to_it(config.PROACTIVE_USERNAME, config.PROACTIVE_PASSWORD)
+    gateway.killTask(job_id, task_name)
---
+++ packages/eexp-engine/eexp_engine-0.0.37-py3-none-any.whl/eexp_engine/exceptions.py
@@ -0,0 +1,53 @@
+class ImplementationFileNotFound(Exception):
+    "Raised when an Implementation file is not found"
+    pass
+
+
+class InputDataInWorkflowDoesNotMatchSignature(Exception):
+    "Raised when a task's input signature does not match with its use in a workflow"
+    pass
+
+
+class OutputDataInWorkflowDoesNotMatchSignature(Exception):
+    "Raised when a task's output signature does not match with its use in a workflow"
+    pass
+
+
+class SourceCodeAttemptsToReadVariableNotInTaskSignature(Exception):
+    "Raised when a proactive variable in a task's source code is not part of its signature (inputs/outputs/params)"
+    pass
+
+
+class SourceCodeAttemptsToLoadDatasetNotInTaskSignature(Exception):
+    "Raised when attempting to load a dataset as an input of a task, without this being part of its signature"
+    pass
+
+
+class SourceCodeAttemptsToSaveDatasetNotInTaskSignature(Exception):
+    "Raised when attempting to save a dataset as an output of a task, without this being part of its signature"
+    pass
+
+
+class InputDataInSubWorkflowDoesNotMatchOutputDataOfParentWorkflow(Exception):
+    "Raised when a subworkflow's input does not much the output of its previous task in the parent workflow"
+    pass
+
+
+class OutputDataInSubWorkflowDoesNotMatchInputDataOfParentWorkflow(Exception):
+    "Raised when a subworkflow's output does not much the input of its next task in the parent workflow"
+    pass
+
+
+class InteractionTaskDoesNotHaveInteractiveType(Exception):
+    "Raised when an interaction of an experiment is not implemented via a task whose type is interactive"
+    pass
+
+
+class DatasetManagementNotSet(Exception):
+    "Raised when the DATASET_MANAGEMENT variable in config.py is not set "
+    pass
+
+
+class DatasetManagementSetToZenohButNoURLProvided(Exception):
+    "Raised when the DATASET_MANAGEMENT variable in config.py is set to ZENOH but the DATASET_MANAGEMENT_URL is not set"
+    pass
