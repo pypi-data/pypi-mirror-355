8b2bb6315564ec53db874499bae711c9fb57f4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:40 +0000

    Add package llumo/0.2.15b2/llumo-0.2.15b2.tar.gz

---
+++ packages/llumo/llumo-0.2.15b2.tar.gz/llumo-0.2.15b2/llumo/__init__.py
@@ -0,0 +1,7 @@
+from .client import LlumoClient
+from .exceptions import LlumoAIError
+from .helpingFuntions import *
+from .models import AVAILABLEMODELS
+from .execution import ModelExecutor
+from .functionCalling import *
+
