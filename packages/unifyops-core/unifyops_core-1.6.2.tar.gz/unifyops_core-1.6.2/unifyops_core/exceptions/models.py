6599d65a1e21c145e222313dd7e1992e1e719e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:16 +0000

    Add package unifyops-core/1.6.2/unifyops_core-1.6.2.tar.gz

---
+++ packages/unifyops-core/unifyops_core-1.6.2.tar.gz/unifyops_core-1.6.2/unifyops_core/exceptions/models.py
@@ -0,0 +1,20 @@
+from uuid import uuid4
+from datetime import datetime, timezone
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+class ErrorDetail(BaseModel):
+    loc: Optional[List[str]]
+    msg: str
+    type: str
+    code: Optional[str] = None
+
+class ErrorResponse(BaseModel):
+    status_code: int
+    error_id: str = Field(default_factory=lambda: str(uuid4()))
+    message: str
+    details: Optional[List[ErrorDetail]] = None
+    timestamp: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
+    error_type: str
