c954fb5da5696f4555f6dd84a1de8fb6823d7233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:02 +0000

    Add package securitykey/0.2.2/securitykey-0.2.2.tar.gz

---
+++ packages/securitykey/securitykey-0.2.2.tar.gz/securitykey-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="securitykey",
+    version="0.2.2",
+    author="JTCoder8290",
+    author_email="jacktay.yujie@gmail.com",
+    description="A Python module to generate and verify security keys for unique identification",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["securitykey"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
