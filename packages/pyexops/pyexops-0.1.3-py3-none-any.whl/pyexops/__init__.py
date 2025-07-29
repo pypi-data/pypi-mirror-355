53d79f22ac2577cefe47b1b88264217104cff63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:16 +0000

    Add package pyexops/0.1.3/pyexops-0.1.3-py3-none-any.whl

---
+++ packages/pyexops/pyexops-0.1.3-py3-none-any.whl/pyexops/__init__.py
@@ -0,0 +1,9 @@
+# pyexops/__init__.py
+
+from .star import Star, Spot
+from .planet import Planet
+from .scene import Scene
+from .photometer import Photometer
+from .simulator import TransitSimulator
+
+__version__ = "0.1.3"
