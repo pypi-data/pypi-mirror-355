2f1ae621be202e42d95a0a47859bf4205d91fc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:13 +0000

    Add package gsrap/0.4.2/gsrap-0.4.2.tar.gz

---
+++ packages/gsrap/gsrap-0.4.2.tar.gz/gsrap-0.4.2/src/gsrap/runsims/__init__.py
@@ -0,0 +1,11 @@
+def runsims_command(args, logger):
+    
+    
+    # adjust out folder path                      
+    while args.outdir.endswith('/'):              
+        args.outdir = args.outdir[:-1]
+    os.makedirs(f'{args.outdir}/', exist_ok=True)
+
+    
+    
+    return 0
