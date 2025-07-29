cbdb1a8a61bcbec6a42c0ba4b2367a674d8ded0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:53 +0000

    Add package aiida-qe-app/0.0.2/aiida_qe_app-0.0.2.tar.gz

---
+++ packages/aiida-qe-app/aiida_qe_app-0.0.2.tar.gz/aiida_qe_app-0.0.2/aiida_qe_app/backend/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+import os
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run(
+        "app.api:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True,
+        log_level="debug",
+    )
