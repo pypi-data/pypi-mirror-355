f7df9ce0bef7db7ba5354084070c35dfaf9892f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:11 +0000

    Add package protovalidate/0.11.0/protovalidate-0.11.0.tar.gz

---
+++ packages/protovalidate/protovalidate-0.11.0.tar.gz/protovalidate-0.11.0/tests/validate_test.py
@@ -0,0 +1,120 @@
+# Copyright 2023-2025 Buf Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import protovalidate
+from gen.tests.example.v1 import validations_pb2
+
+
+class TestValidate(unittest.TestCase):
+    def test_ninf(self):
+        msg = validations_pb2.DoubleFinite()
+        msg.val = float("-inf")
+        violations = protovalidate.collect_violations(msg)
+        self.assertEqual(len(violations), 1)
+        self.assertEqual(violations[0].proto.rule_id, "double.finite")
+        self.assertEqual(violations[0].field_value, msg.val)
+        self.assertEqual(violations[0].rule_value, True)
+
+    def test_map_key(self):
+        msg = validations_pb2.MapKeys()
+        msg.val[1] = "a"
+        violations = protovalidate.collect_violations(msg)
+        self.assertEqual(len(violations), 1)
+        self.assertEqual(violations[0].proto.for_key, True)
+        self.assertEqual(violations[0].field_value, 1)
+        self.assertEqual(violations[0].rule_value, 0)
+
+    def test_sfixed64(self):
+        msg = validations_pb2.SFixed64ExLTGT(val=11)
+        protovalidate.validate(msg)
+
+        violations = protovalidate.collect_violations(msg)
+        self.assertEqual(len(violations), 0)
+
+    def test_oneofs(self):
+        msg1 = validations_pb2.Oneof()
+        msg1.y = 123
+        protovalidate.validate(msg1)
+
+        msg2 = validations_pb2.Oneof()
+        msg2.z.val = True
+        protovalidate.validate(msg2)
+
+        violations = protovalidate.collect_violations(msg1)
+        protovalidate.collect_violations(msg2, into=violations)
+        assert len(violations) == 0
+
+    def test_protovalidate_oneof_valid(self):
+        msg = validations_pb2.ProtovalidateOneof()
+        msg.a = "A"
+        protovalidate.validate(msg)
+        violations = protovalidate.collect_violations(msg)
+        assert len(violations) == 0
+
+    def test_protovalidate_oneof_violation(self):
+        msg = validations_pb2.ProtovalidateOneof()
+        msg.a = "A"
+        msg.b = "B"
+        with self.assertRaises(protovalidate.ValidationError) as cm:
+            protovalidate.validate(msg)
+        e = cm.exception
+        assert str(e) == "invalid ProtovalidateOneof"
+        assert len(e.violations) == 1
+        assert e.to_proto().violations[0].message == "only one of a, b can be set"
+
+    def test_protovalidate_oneof_required_violation(self):
+        msg = validations_pb2.ProtovalidateOneofRequired()
+        with self.assertRaises(protovalidate.ValidationError) as cm:
+            protovalidate.validate(msg)
+        e = cm.exception
+        assert str(e) == "invalid ProtovalidateOneofRequired"
+        assert len(e.violations) == 1
+        assert e.to_proto().violations[0].message == "one of a, b must be set"
+
+    def test_protovalidate_oneof_unknown_field_name(self):
+        msg = validations_pb2.ProtovalidateOneofUnknownFieldName()
+        with self.assertRaises(protovalidate.CompilationError) as cm:
+            protovalidate.validate(msg)
+        assert (
+            str(cm.exception) == 'field "xxx" not found in message tests.example.v1.ProtovalidateOneofUnknownFieldName'
+        )
+
+    def test_repeated(self):
+        msg = validations_pb2.RepeatedEmbedSkip()
+        msg.val.add(val=-1)
+        protovalidate.validate(msg)
+
+        violations = protovalidate.collect_violations(msg)
+        assert len(violations) == 0
+
+    def test_maps(self):
+        msg = validations_pb2.MapMinMax()
+        with self.assertRaises(protovalidate.ValidationError) as cm:
+            protovalidate.validate(msg)
+        e = cm.exception
+        assert len(e.violations) == 1
+        assert len(e.to_proto().violations) == 1
+        assert str(e) == "invalid MapMinMax"
+
+        violations = protovalidate.collect_violations(msg)
+        assert len(violations) == 1
+
+    def test_timestamp(self):
+        msg = validations_pb2.TimestampGTNow()
+        protovalidate.validate(msg)
+
+        violations = protovalidate.collect_violations(msg)
+        assert len(violations) == 0
