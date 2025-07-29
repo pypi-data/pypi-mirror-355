23721fd7f2c1a146adb4c9944a3885d52fc6d079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:36 +0000

    Add package herokulls/0.1.11/herokulls-0.1.11-cp39-cp39-manylinux2014_aarch64.whl

---
+++ packages/herokulls/herokulls-0.1.11-cp39-cp39-manylinux2014_aarch64.whl/herokulls/__init__.py
@@ -0,0 +1,11 @@
+from .custom_api import CustomApi
+from .herokulls import Herokulls
+from .stream_type import StreamType
+from .sync import idle
+
+__all__ = (
+    'idle',
+    'CustomApi',
+    'Herokulls',
+    'StreamType',
+)
