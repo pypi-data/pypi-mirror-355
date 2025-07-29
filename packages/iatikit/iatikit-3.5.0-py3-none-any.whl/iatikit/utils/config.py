10a4fc160648811efb5ccdf87b1524b469fd515e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:07:58 +0000

    Add package iatikit/3.5.0/iatikit-3.5.0-py3-none-any.whl

---
+++ packages/iatikit/iatikit-3.5.0-py3-none-any.whl/iatikit/utils/config.py
@@ -0,0 +1,22 @@
+from os.path import join
+
+from configparser import ConfigParser
+
+
+def _load_config():
+    defaults = {
+        'data_sources': {
+            'zip_url': '',
+        },
+        'paths': {
+            'registry': join('__iatikitcache__', 'registry'),
+            'standard': join('__iatikitcache__', 'standard'),
+        }
+    }
+    config = ConfigParser()
+    config.read_dict(defaults)
+    config.read('iatikit.ini')
+    return config
+
+
+CONFIG = _load_config()
