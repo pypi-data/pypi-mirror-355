8afc725a3927cedebc5addcf994380fa2a8819b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:18 +0000

    Add package openhems/0.2.12.1/openhems-0.2.12.1.tar.gz

---
+++ packages/openhems/openhems-0.2.12.1.tar.gz/openhems-0.2.12.1/src/openhems/modules/energy_strategy/__init__.py
@@ -0,0 +1,13 @@
+"""
+Module for choosing a energy management strategy.
+ i.e. An algorythme for optimization.
+"""
+
+from .energy_strategy import EnergyStrategy
+from .offpeak_strategy import OffPeakStrategy
+from .switchoff_strategy import SwitchoffStrategy
+# from .emhass_strategy import EmhassStrategy
+from .solarbased_strategy import SolarBasedStrategy
+from .hybridinverter_strategy import HybridInverterStrategy
+from .simulated_annealing_strategy import SimulatedAnnealingStrategy
+from .solarnosell_strategy import SolarNoSellStrategy
