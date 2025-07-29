0e6e4f0173729a9a527743ef944bff9cba226ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:20 +0000

    Add package damply/0.15.0/damply-0.15.0-py3-none-any.whl

---
+++ packages/damply/damply-0.15.0-py3-none-any.whl/damply/__init__.py
@@ -0,0 +1,9 @@
+from .logging_config import logger
+from .dmpdirs import dirs
+__version__ = '0.15.0'
+
+__all__ = [
+    'logger',
+	'dirs',
+	'__version__',
+]
---
+++ packages/damply/damply-0.15.0-py3-none-any.whl/damply/cli/project.py
@@ -0,0 +1,33 @@
+import pathlib
+import click
+
+@click.command(context_settings={'help_option_names': ['-h', '--help']})
+@click.argument(
+    "directory",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=pathlib.Path),
+    default=None,
+    required=False,
+)
+@click.option(
+    "--json",
+    "-j",
+    is_flag=True,
+    help="Output the audit information in JSON format.",
+)
+def project(directory: pathlib.Path | None, json: bool) -> None:
+    """Display information about the current project.
+    
+    DIRECTORY is the path to the project directory,
+    if not provided, the current working directory will be used.
+    """
+    from damply.project import DirectoryAudit
+    if  directory is None:
+        directory = pathlib.Path.cwd()
+    directory = directory.resolve()
+    audit = DirectoryAudit.from_path(directory)
+
+    if json:
+        print(audit.to_json())
+    else:
+        from rich import print as rprint
+        rprint(audit)
