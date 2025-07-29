142dacf911fe711e3c40f68ffdd5ca87583d4bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:05 +0000

    Add package gaimiao/0.0.1/gaimiao-0.0.1.tar.gz

---
+++ packages/gaimiao/gaimiao-0.0.1.tar.gz/gaimiao-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gaimiao',
+    version='0.0.1',
+    description='Reserved for Gaimiao brand',
+    packages=find_packages(),
+    install_requires=[],
+)
