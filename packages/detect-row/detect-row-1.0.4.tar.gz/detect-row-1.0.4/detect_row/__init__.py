cbeefee5b46d89c0dc99e26a3d8a9325a6f49460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:02 +0000

    Add package detect-row/1.0.4/detect-row-1.0.4.tar.gz

---
+++ packages/detect-row/detect-row-1.0.4.tar.gz/detect-row-1.0.4/detect_row/__init__.py
@@ -0,0 +1,5 @@
+from detect_row.advanced_row_extractor import *
+from detect_row.basic_row_extractor import *
+from detect_row.tesseract_ocr_extractor import *
+from detect_row.base import *
+from detect_row.advanced_table_extractor import * 
---
+++ packages/detect-row/detect-row-1.0.4.tar.gz/detect-row-1.0.4/detect_row/base.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+import numpy as np
+import logging
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Dict, Any
+
+# ... (phần còn lại của code, đã đọc ở trên) ... 
