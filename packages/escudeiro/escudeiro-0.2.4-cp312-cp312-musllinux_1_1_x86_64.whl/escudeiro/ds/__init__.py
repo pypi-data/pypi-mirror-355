95b8f35d943b2b1b47cefe62c32fd8d4bf61fc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:27 +0000

    Add package escudeiro/0.2.4/escudeiro-0.2.4-cp312-cp312-musllinux_1_1_x86_64.whl

---
+++ packages/escudeiro/escudeiro-0.2.4-cp312-cp312-musllinux_1_1_x86_64.whl/escudeiro/ds/__init__.py
@@ -0,0 +1,22 @@
+from .circuit import CircuitBreaker, with_circuit_breaker
+from .filetree.filetree import FileTree
+from .filetree.virtual import VirtualFileTree
+from .manager import TaskManager
+from .pools.asyncio import AsyncPool
+from .pools.thread import ThreadPool
+from .scheduler import CronInfo, Task, TaskScheduler
+from .worker import WorkerQueue
+
+__all__ = [
+    "AsyncPool",
+    "CircuitBreaker",
+    "CronInfo",
+    "Task",
+    "TaskManager",
+    "TaskScheduler",
+    "ThreadPool",
+    "WorkerQueue",
+    "with_circuit_breaker",
+    "FileTree",
+    "VirtualFileTree",
+]
