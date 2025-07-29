9d149babfaeb9b9dca58fc0768e94277fc74f995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:22 +0000

    Add package runtime-provisioner/1.0.0/runtime_provisioner-1.0.0.tar.gz

---
+++ packages/runtime-provisioner/runtime_provisioner-1.0.0.tar.gz/runtime_provisioner-1.0.0/runtime_provisioner/runtime_provisioner.py
@@ -0,0 +1,24 @@
+import wget
+import zipfile
+import os
+import urllib.request
+
+class Config:
+    # 统一放在用户目录下
+    RUNTIME_PROVISION_DIR = os.path.join(os.path.expanduser('~'), 'runtime_provisioner')
+
+def get_chrome_109_exe() -> str:
+    # 设置User-Agent避免403错误
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    
+    url = 'https://pub-d7ccffce644a4b24b641510c3a9e6cfb.r2.dev/Chrome109.zip'
+    wget.download(url, os.path.join(Config.RUNTIME_PROVISION_DIR, 'Chrome109.zip'))
+    # 解压
+    with zipfile.ZipFile(os.path.join(Config.RUNTIME_PROVISION_DIR, 'Chrome109.zip'), 'r') as zip_ref:
+        zip_ref.extractall(Config.RUNTIME_PROVISION_DIR)
+
+    return os.path.join(Config.RUNTIME_PROVISION_DIR, 'chrome.exe')
+
+os.makedirs(Config.RUNTIME_PROVISION_DIR, exist_ok=True)
