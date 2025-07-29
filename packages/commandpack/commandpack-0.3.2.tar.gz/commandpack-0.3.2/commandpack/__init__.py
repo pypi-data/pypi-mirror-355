5de7b4a43c64d6e6a954369f746a1984fd2dd0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:31 +0000

    Add package commandpack/0.3.2/commandpack-0.3.2.tar.gz

---
+++ packages/commandpack/commandpack-0.3.2.tar.gz/commandpack-0.3.2/commandpack/__init__.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2025, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""
+Command executors' library.
+
+- A cross-platform library for creation, storage, management
+of commands and command packages. Execution of commands,
+parsing of files with command packages.
+
+"""
+__version__ = '0.3.2'
