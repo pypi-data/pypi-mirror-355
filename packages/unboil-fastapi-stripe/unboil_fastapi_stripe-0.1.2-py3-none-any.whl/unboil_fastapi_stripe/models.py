7dd3e9a5415b8f4ef3966316509fe1d96a7734bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:52 +0000

    Add package unboil-fastapi-stripe/0.1.2/unboil_fastapi_stripe-0.1.2-py3-none-any.whl

---
+++ packages/unboil-fastapi-stripe/unboil_fastapi_stripe-0.1.2-py3-none-any.whl/unboil_fastapi_stripe/models.py
@@ -0,0 +1,118 @@
+from typing import Any, Protocol, runtime_checkable
+import uuid
+from datetime import datetime
+from sqlalchemy import (
+    UUID,
+    TypeDecorator,
+    Uuid,
+    DateTime,
+    ForeignKey,
+    Index,
+    String,
+    func,
+    MetaData,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+    DeclarativeBase,
+)
+
+__all__ = [
+    "Models",
+    "UserLike",
+    "HasName",
+    "HasEmail",
+]
+
+class Models:
+
+    def __init__(
+        self,
+        metadata: MetaData,
+        user_foreign_key: str,
+    ):
+
+        metadata_ = metadata
+
+        class Base(DeclarativeBase):
+            metadata = metadata_
+
+        class Identifiable:
+            id: Mapped[uuid.UUID] = mapped_column(
+                Uuid(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4()
+            )
+
+        class Timestamped:
+            created_at: Mapped[datetime] = mapped_column(
+                DateTime(timezone=True),
+                default=func.now(),
+                server_default=func.now(),
+            )
+            last_updated_at: Mapped[datetime] = mapped_column(
+                DateTime(timezone=True),
+                default=func.now(),
+                onupdate=func.now(),
+                server_default=func.now(),
+            )
+
+        class Customer(Base, Identifiable, Timestamped):
+            __tablename__ = "stripe_customers"
+            __table_args__ = (
+                Index("ix_stripe_customers_stripe_customer_id", "stripe_customer_id"),
+                Index("ix_stripe_customers_user_id", "user_id"),
+            )
+            stripe_customer_id: Mapped[str] = mapped_column(String(255), unique=True)
+            user_id: Mapped[Any] = mapped_column(ForeignKey(user_foreign_key), unique=True)
+            subscriptions: Mapped[list["Subscription"]] = relationship()
+
+            def __init__(self, stripe_customer_id: str, user_id: Any):
+                self.stripe_customer_id = stripe_customer_id
+                self.user_id = user_id
+
+        class Subscription(Base, Identifiable, Timestamped):
+            __tablename__ = "stripe_subscriptions"
+            __table_args__ = (
+                Index("ix_stripe_subscriptions_stripe_product_id", "stripe_product_id"),
+            )
+            stripe_subscription_item_id: Mapped[str] = mapped_column(
+                String, unique=True
+            )
+            stripe_product_id: Mapped[str] = mapped_column(String)
+            customer_id: Mapped[uuid.UUID] = mapped_column(
+                ForeignKey(f"{Customer.__tablename__}.{Customer.id.key}")
+            )
+            customer: Mapped["Customer"] = relationship()
+            current_period_end: Mapped[datetime] = mapped_column(
+                DateTime(timezone=True)
+            )
+
+            def __init__(
+                self,
+                customer_id: uuid.UUID,
+                current_period_end: datetime,
+                stripe_product_id: str,
+                stripe_subscription_item_id: str,
+            ):
+                self.customer_id = customer_id
+                self.current_period_end = current_period_end
+                self.stripe_product_id = stripe_product_id
+                self.stripe_subscription_item_id = stripe_subscription_item_id
+
+        self.Customer = Customer
+        self.Subscription = Subscription
+
+
+class UserLike(Protocol):
+    id: Mapped[Any]
+
+
+@runtime_checkable
+class HasName(Protocol):
+    name: Mapped[str]
+
+
+@runtime_checkable
+class HasEmail(Protocol):
+    email: Mapped[str]
---
+++ packages/unboil-fastapi-stripe/unboil_fastapi_stripe-0.1.2-py3-none-any.whl/unboil_fastapi_stripe/routes.py
@@ -0,0 +1,80 @@
+import stripe
+import stripe.webhook
+from typing import Annotated, Awaitable, Callable
+from fastapi import APIRouter, Body, Depends, HTTPException, Header, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from unboil_fastapi_stripe.config import Config
+from unboil_fastapi_stripe.dependencies import Dependencies
+from unboil_fastapi_stripe.models import HasEmail, HasName, UserLike
+from unboil_fastapi_stripe.schemas import CheckoutSessionResponse, CheckoutSession
+from unboil_fastapi_stripe.service import Service
+
+__all__ = ["create_router"]
+
+def create_router(
+    config: Config,
+    service: Service,
+    dependencies: Dependencies,
+):
+        
+    router = APIRouter(prefix="/stripe", tags=["Stripe"])
+
+    @router.post("/checkout")
+    async def checkout_session(
+        request: Annotated[CheckoutSession, Body()],
+        db: Annotated[AsyncSession, Depends(dependencies.get_db)],
+        user: Annotated[UserLike, Depends(dependencies.require_user)],
+    ) -> CheckoutSessionResponse:
+        customer = await service.ensure_customer(
+            db=db,
+            user_id=user.id,
+            name=user.name if isinstance(user, HasName) else None,
+            email=user.email if isinstance(user, HasEmail) else None,
+        )
+        checkout_session = stripe.checkout.Session.create(
+            success_url=request.success_url,
+            cancel_url=request.cancel_url,
+            customer=customer.stripe_customer_id,
+            mode=request.type,
+            line_items=[
+                {
+                    "quantity": 1,
+                    "price": price_id,
+                }
+                for price_id in request.price_ids
+            ],
+        )
+        assert checkout_session.url is not None
+        return CheckoutSessionResponse(checkout_session_url=checkout_session.url)
+
+
+    @router.post("/webhook")
+    async def webhook(
+        request: Request,
+        stripe_signature: Annotated[str, Header(alias="stripe-signature")],
+        db: Annotated[AsyncSession, Depends(dependencies.get_db)],
+    ):
+        payload = await request.body()
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=payload,
+                sig_header=stripe_signature,
+                secret=config.stripe_webhook_secret,
+            )
+        except (ValueError, stripe.SignatureVerificationError) as e:
+            raise HTTPException(status_code=400, detail="Invalid Stripe webhook")
+
+        if event.type == "customer.subscription.created" or \
+            event.type == "customer.subscription.updated":
+            stripe_subscription = stripe.Subscription(**event.data.object)
+            await service.create_or_update_subscriptions_from_stripe_subscription(
+                db=db,
+                stripe_subscription=stripe_subscription
+            )
+        elif event.type == "customer.subscription.deleted":
+            stripe_subscription = stripe.Subscription(**event.data.object)
+            await service.delete_subscriptions_from_stripe_subscription(
+                db=db, stripe_subscription=stripe_subscription
+            )
+            
+    return router
