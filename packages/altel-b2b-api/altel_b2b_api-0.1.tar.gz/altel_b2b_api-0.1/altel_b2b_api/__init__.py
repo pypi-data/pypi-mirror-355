42a341923b6f14f98ade74bccbe5075ff800295e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:27 +0000

    Add package altel-b2b-api/0.1/altel_b2b_api-0.1.tar.gz

---
+++ packages/altel-b2b-api/altel_b2b_api-0.1.tar.gz/altel_b2b_api-0.1/altel_b2b_api/__init__.py
@@ -0,0 +1,14 @@
+"""
+:authors: @litemat
+:license: The MIT License (MIT), see LICENSE file
+:copyright: (c) 2025 @litemat
+"""
+
+from .api import AltelB2B
+from .settings import AltelConfig
+
+__all__ = ["AltelB2B", "AltelConfig"]
+
+__author__ = '@litemat'
+__version__ = '0.1'
+__email__ = 'legentea22@gmail.com'
