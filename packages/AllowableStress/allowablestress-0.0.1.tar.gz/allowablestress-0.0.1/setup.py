7aa797eaa70963c882b4ec1a6cc7a0662cb5a093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:00 +0000

    Add package AllowableStress/0.0.1/allowablestress-0.0.1.tar.gz

---
+++ packages/AllowableStress/allowablestress-0.0.1.tar.gz/allowablestress-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="AllowableStress",
+    version="0.0.1",
+    description="Probabilistic method to determine allowable stress",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shinsuke Sakai",
+    author_email='sakaishin0321@gmail.com',
+    url='https://github.com/ShinsukeSakai0321/AllowableStress',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+)
