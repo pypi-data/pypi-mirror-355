71cc27d079bc1968f88892221a1b4ee3efb4c212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:33 +0000

    Add package terra_ui_components/0.0.47/terra_ui_components-0.0.47.tar.gz

---
+++ packages/terra_ui_components/terra_ui_components-0.0.47.tar.gz/terra_ui_components-0.0.47/src/utilities/date.ts
@@ -0,0 +1,30 @@
+import { isValid } from 'date-fns'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone.js'
+import utc from 'dayjs/plugin/utc.js'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Etc/GMT')
+
+type MaybeDate = string | number | Date
+
+export function isValidDate(date: any): boolean {
+    const parsedDate = Date.parse(date)
+    return !isNaN(parsedDate) && isValid(parsedDate)
+}
+
+export function getUTCDate(date: MaybeDate) {
+    return dayjs.utc(date).toDate()
+}
+
+/**
+ * formats a date, see https://day.js.org/docs/en/display/format for available formatting options
+ */
+export function formatDate(date: dayjs.Dayjs | MaybeDate, format?: string) {
+    if (!dayjs.isDayjs(date)) {
+        date = dayjs.utc(date)
+    }
+
+    return date.format(format)
+}
