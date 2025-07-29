50447c21380064021627fb7639563143433e84e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:54 +0000

    Add package TelegramManager/0.1.18/telegrammanager-0.1.18.tar.gz

---
+++ packages/TelegramManager/telegrammanager-0.1.18.tar.gz/telegrammanager-0.1.18/telegram_manager/__init__.py
@@ -0,0 +1,24 @@
+from .config import Config
+from .controller import (
+    TelegramManager as _TelegramManager,
+    AsyncTelegramManager as _AsyncTelegramManager,
+)
+
+
+class TelegramManager(_TelegramManager):
+    def __init__(self):
+        super().__init__(Config.TELEGRAM_API_ID, Config.TELEGRAM_API_HASH, Config.TELEGRAM_PHONE_NUMBER)
+
+class AsyncTelegramManager(_AsyncTelegramManager):
+    def __init__(self):
+        super().__init__(
+            Config.TELEGRAM_API_ID,
+            Config.TELEGRAM_API_HASH,
+            Config.TELEGRAM_PHONE_NUMBER,
+        )
+
+
+__all__ = [
+    "TelegramManager",
+    "AsyncTelegramManager",
+]
