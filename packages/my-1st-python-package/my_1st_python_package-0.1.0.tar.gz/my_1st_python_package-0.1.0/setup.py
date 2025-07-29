4dbed2955d2e003bf5fbc7da0cd29928a638fe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:09 +0000

    Add package my-1st-python-package/0.1.0/my_1st_python_package-0.1.0.tar.gz

---
+++ packages/my-1st-python-package/my_1st_python_package-0.1.0.tar.gz/my_1st_python_package-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "my_1st_python_package",
+    version = "0.1.0",
+    packages=find_packages(),
+    install_requires = [
+        
+    ]
+)
