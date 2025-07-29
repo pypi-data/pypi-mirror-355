180ef01016eb2c0e602b987fcb084eef5de2608e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:28 +0000

    Add package yap.helper/0.0.1/yap.helper-0.0.1.tar.gz

---
+++ packages/yap.helper/yap.helper-0.0.1.tar.gz/yap.helper-0.0.1/noteTaking/function_arguments.py
@@ -0,0 +1,8 @@
+import time
+from datetime import datetime
+from typing import Any, Optional
+
+# DO NOT USE mutable default arguments
+# Mutable default arguments are evaluated only once when the module is loaded
+# This means that the default argument is shared between all calls to the funciton
+
