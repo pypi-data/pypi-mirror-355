7738783e65c614ee96b01cc1176c623cc9ae9654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:51 +0000

    Add package appserver-sdk-python-i18n-pt-br/0.1.3/appserver_sdk_python_i18n_pt_br-0.1.3.tar.gz

---
+++ packages/appserver-sdk-python-i18n-pt-br/appserver_sdk_python_i18n_pt_br-0.1.3.tar.gz/appserver_sdk_python_i18n_pt_br-0.1.3/src/appserver_sdk_python_i18n_pt_br/__init__.py
@@ -0,0 +1,13 @@
+"""
+Appserver SDK
+
+Pacote de artefatos referentes ao Brasil.
+"""
+
+__version__ = "0.1.1"
+__author__ = "Appserver SDK"
+__email__ = "suporte@appserver.com.br"
+
+from . import validation
+
+__all__ = ["validation"]
