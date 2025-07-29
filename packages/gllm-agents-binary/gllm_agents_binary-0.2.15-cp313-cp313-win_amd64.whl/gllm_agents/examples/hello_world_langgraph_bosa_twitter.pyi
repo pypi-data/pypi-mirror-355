92d2b5d463d7ef2bdf44ffba61c629fe39ed177a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:40 +0000

    Add package gllm-agents-binary/0.2.15/gllm_agents_binary-0.2.15-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-agents-binary/gllm_agents_binary-0.2.15-cp313-cp313-win_amd64.whl/gllm_agents/examples/hello_world_langgraph_bosa_twitter.pyi
@@ -0,0 +1,5 @@
+from gllm_agents.agent.langgraph_agent import LangGraphAgent as LangGraphAgent
+from gllm_agents.examples.tools.twitter_bosa_tool import twitter_get_user_tool as twitter_get_user_tool
+
+async def langgraph_bosa_example() -> None:
+    """Demonstrates the LangGraphAgent's arun method."""
