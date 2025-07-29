235fe8e387803c0023a31c5b7f32d234a84cb957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:57 +0000

    Add package ticlust/1.0.8/ticlust-1.0.8-py3-none-any.whl

---
+++ packages/ticlust/ticlust-1.0.8-py3-none-any.whl/ticlust/__main__.py
@@ -0,0 +1,22 @@
+from .ticlust import TICAnalysis
+from .core_api import parse_arguments
+
+
+def main():
+    args = parse_arguments()
+    simple_tic = TICAnalysis(
+        args.fasta_file,
+        args.zotu_table
+        )
+    threshold = {
+        "species": args.species_thr,
+        "genus": args.genera_thr,
+        "family": args.family_thr
+    }
+    simple_tic.run(
+        threads = args.threads,
+        cluster_thresholds_d = threshold
+    )
+
+if __name__ == "__main__":
+    main()
