15995771683989ffee5ce4bcec2d6ebfc5dff723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package pdap-access-manager/0.3.6/pdap_access_manager-0.3.6-py3-none-any.whl

---
+++ packages/pdap-access-manager/pdap_access_manager-0.3.6-py3-none-any.whl/pdap_access_manager/__init__.py
@@ -0,0 +1,11 @@
+from pdap_access_manager.access_manager import AccessManager, RequestInfo, RequestType, DataSourcesNamespaces, \
+    SourceCollectorNamespaces, ResponseInfo
+
+__all__ = [
+    "AccessManager",
+    "RequestInfo",
+    "RequestType",
+    "DataSourcesNamespaces",
+    "SourceCollectorNamespaces",
+    "ResponseInfo"
+]
