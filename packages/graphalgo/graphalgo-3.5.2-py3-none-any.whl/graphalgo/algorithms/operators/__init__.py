83883ad378fe0af573db83d6a928380d31bbac9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:09 +0000

    Add package graphalgo/3.5.2/graphalgo-3.5.2-py3-none-any.whl

---
+++ packages/graphalgo/graphalgo-3.5.2-py3-none-any.whl/graphalgo/algorithms/operators/__init__.py
@@ -0,0 +1,4 @@
+from graphalgo.algorithms.operators.all import *
+from graphalgo.algorithms.operators.binary import *
+from graphalgo.algorithms.operators.product import *
+from graphalgo.algorithms.operators.unary import *
