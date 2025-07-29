73e650ca4031d4a2b18db7801c35ee0048fb6cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:52 +0000

    Add package awsjavakit-cfn-rules/0.0.28/awsjavakit_cfn_rules-0.0.28-py3-none-any.whl

---
+++ packages/awsjavakit-cfn-rules/awsjavakit_cfn_rules-0.0.28-py3-none-any.whl/awsjavakit_cfn_rules/__init__.py
@@ -0,0 +1,15 @@
+"""
+awsjavakit_cfn_rules package
+"""
+import os
+from pathlib import Path
+
+from awsjavakit_cfn_rules.rules.sqs_long_polling_rule import SqsLongPollingRule
+from awsjavakit_cfn_rules.rules.tags_checker import TagsChecker
+
+PROJECT_FOLDER = Path(os.path.abspath(__file__)).parent
+
+__all__ = [
+    "TagsChecker",
+    "SqsLongPollingRule"
+]
