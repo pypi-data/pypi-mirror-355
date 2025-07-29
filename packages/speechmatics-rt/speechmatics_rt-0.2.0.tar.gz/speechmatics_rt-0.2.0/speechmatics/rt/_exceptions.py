163716b0ad9b8de72c0eb359a5a4a656adf5599f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:16 +0000

    Add package speechmatics-rt/0.2.0/speechmatics_rt-0.2.0.tar.gz

---
+++ packages/speechmatics-rt/speechmatics_rt-0.2.0.tar.gz/speechmatics_rt-0.2.0/speechmatics/rt/_exceptions.py
@@ -0,0 +1,63 @@
+class ConfigurationError(Exception):
+    """Raised when there's an error in configuration."""
+
+    pass
+
+
+class AuthenticationError(Exception):
+    """Raised when authentication fails."""
+
+    pass
+
+
+class ConnectionError(Exception):
+    """Raised when connection to the service fails."""
+
+    pass
+
+
+class TransportError(Exception):
+    """Raised when there's an error in the transport layer."""
+
+    pass
+
+
+class TranscriptionError(Exception):
+    """Raised when transcription fails."""
+
+    pass
+
+
+class AudioError(Exception):
+    """Raised when there's an issue with audio data."""
+
+    pass
+
+
+class SessionError(Exception):
+    """Raised when there's an error with the session state."""
+
+    pass
+
+
+class TimeoutError(Exception):
+    """Raised when an operation times out."""
+
+    pass
+
+
+class EndOfTranscriptError(Exception):
+    """Raised when the transcript has ended."""
+
+    pass
+
+
+class ForceEndSession(Exception):
+    """
+    Exception that can be raised by user handlers to force session termination.
+
+    This is a special exception that users can raise from event handlers
+    or middleware to cleanly terminate a transcription session early.
+    """
+
+    pass
