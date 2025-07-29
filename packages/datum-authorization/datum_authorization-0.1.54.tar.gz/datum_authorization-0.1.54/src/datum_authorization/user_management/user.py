96ec47cfb526271608bb6599a17170fbdf160e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:19 +0000

    Add package datum-authorization/0.1.54/datum_authorization-0.1.54.tar.gz

---
+++ packages/datum-authorization/datum_authorization-0.1.54.tar.gz/datum_authorization-0.1.54/src/datum_authorization/user_management/user.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+# Abstract base class
+class User(ABC):
+
+    @abstractmethod
+    def create_user(self, username, email, password, roles=[]):
+        pass  # This method must be overridden by subclasses
+
+    @abstractmethod
+    def update_user(self, payload):
+        pass  # This method must be overridden by subclasses
+
+    @abstractmethod
+    def get_user_id(self, username):
+        pass  # This method must be overridden by subclasses
+
+    @abstractmethod
+    def assign_roles(self, user_id, roles):
+        pass
+
+    @abstractmethod
+    def delete_user(self, user_id):
+        pass
+
+    @abstractmethod
+    def get_all(self, page, size):
+        pass
