2104cb38351d1b5609be44faa48ac7f97c12e49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:08 +0000

    Add package everysk-mcp/0.1.70/everysk_mcp-0.1.70.tar.gz

---
+++ packages/everysk-mcp/everysk_mcp-0.1.70.tar.gz/everysk_mcp-0.1.70/everysk_mcp/__init__.py
@@ -0,0 +1,15 @@
+###############################################################################
+#
+# (C) Copyright 2025 EVERYSK TECHNOLOGIES
+#
+# This is an unpublished work containing confidential and proprietary
+# information of EVERYSK TECHNOLOGIES. Disclosure, use, or reproduction
+# without authorization of EVERYSK TECHNOLOGIES is prohibited.
+#
+###############################################################################
+__all__ = ["main", "server"]
+from . import server
+
+
+def main() -> None:
+    server.mcp.run(transport="stdio")
