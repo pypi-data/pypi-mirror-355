85f9acca33e5da9de9beb7e737737ea6c2555442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:50 +0000

    Add package schlange/0.1.2/schlange-0.1.2.tar.gz

---
+++ packages/schlange/schlange-0.1.2.tar.gz/schlange-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="schlange",
+    version="0.1.0",
+    author="Konja Rehm",
+    description="Python auf Deutsch - Deutsche Schlüsselwörter für Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/schlange",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: German",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "schlange=schlange.cli:main",
+        ],
+    },
+)
