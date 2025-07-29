5d4c032ab6d06cd32f31900b571b2281f24f4909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:13 +0000

    Add package detect-row/1.0.2/detect-row-1.0.2.tar.gz

---
+++ packages/detect-row/detect-row-1.0.2.tar.gz/detect-row-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="detect-row",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python>=4.8.0",
+        "numpy>=1.24.0",
+        "pytesseract>=0.3.10"
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A library for detecting rows in images",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/detect-row",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+) 
