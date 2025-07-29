93979d75325aee66fbc287ae6e957f9e76bf0d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:23 +0000

    Add package structifyai/1.121.0/structifyai-1.121.0.tar.gz

---
+++ packages/structifyai/structifyai-1.121.0.tar.gz/structifyai-1.121.0/src/structify/types/structure_enhance_property_params.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["StructureEnhancePropertyParams"]
+
+
+class StructureEnhancePropertyParams(TypedDict, total=False):
+    entity_id: Required[str]
+
+    property_name: Required[str]
+
+    allow_extra_entities: bool
+
+    max_steps_without_save: int
+
+    special_job_type: Optional[Literal["HumanLLM"]]
+
+    starting_searches: List[str]
+
+    starting_urls: List[str]
