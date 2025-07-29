2d2489ca756799f649bd7a69922e336209a04164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:09:12 +0000

    Add package trd-utils/0.0.18/trd_utils-0.0.18.tar.gz

---
+++ packages/trd-utils/trd_utils-0.0.18.tar.gz/trd_utils-0.0.18/trd_utils/date_utils/datetime_helpers.py
@@ -0,0 +1,16 @@
+
+
+
+
+from datetime import datetime, timezone
+
+
+def dt_from_ts(timestamp: float) -> datetime:
+    """
+    Return a datetime from a timestamp.
+    :param timestamp: timestamp in seconds or milliseconds
+    """
+    if timestamp > 1e10:
+        # Timezone in ms - convert to seconds
+        timestamp /= 1000
+    return datetime.fromtimestamp(timestamp, tz=timezone.utc)
