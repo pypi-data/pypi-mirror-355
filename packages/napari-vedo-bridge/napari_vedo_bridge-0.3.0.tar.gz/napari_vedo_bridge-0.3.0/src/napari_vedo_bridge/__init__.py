75d24b03f6a40ab1044426cbdb78fc2ec72fa048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:31 +0000

    Add package napari-vedo-bridge/0.3.0/napari_vedo_bridge-0.3.0.tar.gz

---
+++ packages/napari-vedo-bridge/napari_vedo_bridge-0.3.0.tar.gz/napari_vedo_bridge-0.3.0/src/napari_vedo_bridge/__init__.py
@@ -0,0 +1,38 @@
+from ._mesh import (
+    compute_normals,
+    shrink,
+    subdivide,
+    decimate,
+    decimate_pro,
+    decimate_binned,
+    smooth,
+    fill_holes,
+    split,
+    extract_largest_region,
+    binarize,
+)
+
+from ._points import (
+    smooth_mls_1d,
+    remove_outliers,
+)
+
+from . import _widgets as widgets
+
+__all__ = [
+    "compute_normals",
+    "shrink",
+    "subdivide",
+    "decimate",
+    "decimate_pro",
+    "decimate_binned",
+    "smooth",
+    "fill_holes",
+    "split",
+    "extract_largest_region",
+    "binarize",
+    "smooth_points",
+    "remove_outliers"
+    "smooth_mls_1d",
+    "widgets",
+]
