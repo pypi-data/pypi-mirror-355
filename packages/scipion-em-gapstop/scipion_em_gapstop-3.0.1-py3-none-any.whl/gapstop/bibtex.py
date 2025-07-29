29656fa026026d8fe9c98d4f1e3d032351128d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:57 +0000

    Add package scipion-em-gapstop/3.0.1/scipion_em_gapstop-3.0.1-py3-none-any.whl

---
+++ packages/scipion-em-gapstop/scipion_em_gapstop-3.0.1-py3-none-any.whl/gapstop/bibtex.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+@article{Wan2024,
+title = "STOPGAP: an open-source package for template matching, subtomogram alignment and classification",
+author = "Wan, William et al.",
+journal = "Acta Crystallographica Section D",
+year = "2024",
+volume = "80",
+number = "5",
+pages = "336 - 349",
+doi = "http://doi.org/10.1107/S205979832400295X",
+url = "http://doi.org/10.1107/S205979832400295X",
+}
+
+
+@article{CruzLeon2024,
+title = "High-confidence 3D template matching for cryo-electron tomography.",
+journal = "Nature Communications",
+volume = "15",
+number = "1",
+pages = "209 - 216",
+year = "2024",
+doi = "http://doi.org/10.1038/s41467-024-47839-8",
+url = "http://www.nature.com/articles/s41467-024-47839-8",
+author = "Cruz-León, Sergio. et al.",
+}
+
+"""
