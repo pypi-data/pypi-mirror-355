6089fcd7a302857c67f6299d932932798178752b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:56 +0000

    Add package aws_pylambda_sam_builder/0.2.4/aws_pylambda_sam_builder-0.2.4-py3-none-any.whl

---
+++ packages/aws_pylambda_sam_builder/aws_pylambda_sam_builder-0.2.4-py3-none-any.whl/aws_pylambda_sam_builder/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Entry point for running the module with python -m aws_pylambda_sam_builder
+"""
+
+from aws_pylambda_sam_builder import main
+
+if __name__ == "__main__":
+    main()
