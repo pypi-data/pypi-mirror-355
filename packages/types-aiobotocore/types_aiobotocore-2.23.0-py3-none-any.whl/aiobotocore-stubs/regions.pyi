2e92c3e5f35766c8eab63446daad8731e9d22dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:41 +0000

    Add package types-aiobotocore/2.23.0/types_aiobotocore-2.23.0-py3-none-any.whl

---
+++ packages/types-aiobotocore/types_aiobotocore-2.23.0-py3-none-any.whl/aiobotocore-stubs/regions.pyi
@@ -0,0 +1,22 @@
+"""
+Type annotations for aiobotocore.regions module.
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from logging import Logger
+from typing import Any, Mapping
+
+from botocore.endpoint_provider import RuleSetEndpoint
+from botocore.model import OperationModel
+from botocore.regions import EndpointRulesetResolver
+
+LOG: Logger
+
+class AioEndpointRulesetResolver(EndpointRulesetResolver):
+    async def construct_endpoint(  # type: ignore[override]
+        self,
+        operation_model: OperationModel,
+        call_args: Mapping[str, Any] | None,
+        request_context: Mapping[str, Any] | None,
+    ) -> RuleSetEndpoint: ...
