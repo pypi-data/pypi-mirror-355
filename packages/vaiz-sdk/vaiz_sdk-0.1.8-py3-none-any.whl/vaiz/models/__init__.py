310e928db9ca7e93937475fc9c067ecc936aff01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:17 +0000

    Add package vaiz-sdk/0.1.8/vaiz_sdk-0.1.8-py3-none-any.whl

---
+++ packages/vaiz-sdk/vaiz_sdk-0.1.8-py3-none-any.whl/vaiz/models/__init__.py
@@ -0,0 +1,45 @@
+from .base import TaskFollower, TaskPriority, CustomField
+from .tasks import Task, TaskResponse, CreateTaskRequest, EditTaskRequest
+from vaiz.models.boards import (
+    Board, 
+    BoardsResponse, 
+    BoardResponse, 
+    CreateBoardTypeRequest, 
+    CreateBoardTypeResponse,
+    EditBoardTypeRequest,
+    EditBoardTypeResponse,
+    CreateBoardCustomFieldRequest,
+    CreateBoardCustomFieldResponse,
+    EditBoardCustomFieldRequest,
+    EditBoardCustomFieldResponse,
+    CustomFieldType
+)
+from vaiz.models.profile import Profile, ProfileResponse
+from vaiz.models.projects import Project, ProjectsResponse, ProjectResponse
+
+__all__ = [
+    'TaskFollower',
+    'TaskPriority',
+    'CustomField',
+    'CreateTaskRequest',
+    'EditTaskRequest',
+    'Task',
+    'TaskResponse',
+    'Board',
+    'BoardsResponse',
+    'BoardResponse',
+    'CreateBoardTypeRequest',
+    'CreateBoardTypeResponse',
+    'EditBoardTypeRequest',
+    'EditBoardTypeResponse',
+    'CreateBoardCustomFieldRequest',
+    'CreateBoardCustomFieldResponse',
+    'EditBoardCustomFieldRequest',
+    'EditBoardCustomFieldResponse',
+    'CustomFieldType',
+    'Profile',
+    'ProfileResponse',
+    'Project',
+    'ProjectsResponse',
+    'ProjectResponse'
+] 
