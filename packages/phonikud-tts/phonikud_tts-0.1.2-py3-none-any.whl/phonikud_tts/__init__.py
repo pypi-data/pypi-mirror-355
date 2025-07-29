d94acf680f155382ab0f187d7e9496d0b79fc827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:33 +0000

    Add package phonikud-tts/0.1.2/phonikud_tts-0.1.2-py3-none-any.whl

---
+++ packages/phonikud-tts/phonikud_tts-0.1.2-py3-none-any.whl/phonikud_tts/__init__.py
@@ -0,0 +1,10 @@
+from piper_onnx import Piper
+from phonikud import phonemize
+from phonikud_onnx import Phonikud
+
+class StyleTTS2:
+    def __init__(self):
+        ...
+
+    def create(text: str, is_phonemes = False):
+        ...
