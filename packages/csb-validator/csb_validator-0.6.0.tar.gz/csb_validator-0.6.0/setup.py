eab43d0a29ae3381567dc88762159e3d83acc91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:33 +0000

    Add package csb-validator/0.6.0/csb_validator-0.6.0.tar.gz

---
+++ packages/csb-validator/csb_validator-0.6.0.tar.gz/csb_validator-0.6.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="csb_validator",
+    version="0.6.0",
+    description="Validate CSB (Crowdsourced Bathymetry) GeoJSON and other formats",
+    author="Clinton Campbell",
+    author_email="clint.campbell@colorado.edu",
+    url="https://github.com/CI-CMG/csb-validator",
+    packages=find_packages(),
+    install_requires=[
+    "aiofiles"
+    ],
+    entry_points={
+        "console_scripts": [
+            "csb-validator=csb_validator.validator:main", 
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
