4771af097abcfbd513ea52070cbbb7bc41f5fa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:54 +0000

    Add package novel-downloader/1.4.2/novel_downloader-1.4.2-py3-none-any.whl

---
+++ packages/novel-downloader/novel_downloader-1.4.2-py3-none-any.whl/novel_downloader/core/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+novel_downloader.core
+---------------------
+
+This package serves as the core layer of the novel_downloader system.
+
+It provides factory methods for constructing key components required for
+downloading and processing online novel content, including:
+
+- Downloader: Handles the full download lifecycle of a book or a batch of books.
+- Parser: Extracts structured data from HTML or SSR content.
+- Fetcher: Sends HTTP requests and manages sessions, including login if required.
+- Exporter: Responsible for exporting downloaded data into various output formats.
+"""
+
+from .factory import get_parser
+
+__all__ = [
+    "get_parser",
+]
