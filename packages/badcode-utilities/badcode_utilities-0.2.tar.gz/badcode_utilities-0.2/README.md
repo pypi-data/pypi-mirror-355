3d0a0eb9e6ecb27fc09e9aa0f865820d0d2254c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:32 +0000

    Add package badcode-utilities/0.2/badcode_utilities-0.2.tar.gz

---
+++ packages/badcode-utilities/badcode_utilities-0.2.tar.gz/badcode_utilities-0.2/README.md
@@ -0,0 +1,15 @@
+# badcode_utilities
+
+A small Python utility library by cosmo-the-deer, providing simple helpers for string-to-int conversion, yes/no prompts, and project info display.
+
+- docs: [badcode utilities docs...](https://cosmo-the-deer.github.io/badcode_utilities/)
+
+## License
+
+MIT License © 2025 cosmo-the-deer
+
+## Socials
+
+- Discord: cosmothedeer12
+- YouTube: [@cosmo-the-deer](https://youtube.com/@cosmo-the-deer)
+- Itch.io: [cosmothedeer](https://cosmothedeer.itch.io)
---
+++ packages/badcode-utilities/badcode_utilities-0.2.tar.gz/badcode_utilities-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='badcode_utilities',
+    version='0.2',  # or higher, must be unique and not used before
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'badcode_utilities': ['badwords.txt'],
+    },
+    install_requires=[
+        # none
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
