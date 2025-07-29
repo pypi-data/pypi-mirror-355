8f0f036f5d0ca0d07725b7752308bd5e3a61f7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:37 +0000

    Add package python-data-augmenter/0.0.3/python_data_augmenter-0.0.3.tar.gz

---
+++ packages/python-data-augmenter/python_data_augmenter-0.0.3.tar.gz/python_data_augmenter-0.0.3/tests/test_augmentation.py
@@ -0,0 +1,63 @@
+import pytest
+from augmenter.augmentation import TGIAugmenter, OllamaAugmenter, OpenAIAugmenter, DatasetAugmenter
+
+# ------------------------
+# TGIAugmenter Tests
+# ------------------------
+
+def test_tgi_generate_queries_from_document():
+    pass
+
+def test_tgi_call_tgi():
+    pass
+
+# ------------------------
+# OllamaAugmenter Tests
+# ------------------------
+
+def test_ollama_generate_queries_from_document():
+    pass
+
+def test_ollama_call_ollama():
+    pass
+
+# ------------------------
+# OpenAIAugmenter Tests
+# ------------------------
+
+def test_openai_generate_queries_from_document():
+    pass
+
+def test_openai_call_openai():
+    pass
+
+# ------------------------
+# DatasetAugmenter Tests
+# ------------------------
+
+def test_dataset_augmenter_split_and_augment():
+    pass
+
+def test_dataset_augmenter_get_embeddings():
+    pass
+
+def test_dataset_augmenter_get_cosine_similarity():
+    pass
+
+def test_dataset_augmenter_get_cross_cosine_similarity():
+    pass
+
+def test_dataset_augmenter_filter_cosine_similarity():
+    pass
+
+def test_dataset_augmenter_filter_cross_cosine_similarity():
+    pass
+
+def test_dataset_augmenter_filter_dataset():
+    pass
+
+def test_dataset_augmenter_load_augmented_dataset():
+    pass
+
+def test_dataset_augmenter_load_augmented_dataset_embeddings():
+    pass
