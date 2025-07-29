9b368b058ee3c8a77adb17c85bca845822c2e5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:55 +0000

    Add package ontolutils/0.17.0/ontolutils-0.17.0-py3-none-any.whl

---
+++ packages/ontolutils/ontolutils-0.17.0-py3-none-any.whl/ontolutils/classes/__init__.py
@@ -0,0 +1,7 @@
+"""Classes for working with ontology data"""
+from .decorator import namespaces, urirefs
+from .query_util import query, dquery
+from .thing import Thing, get_urirefs, get_namespaces, build, Property
+from .utils import as_id
+
+__all__ = ['namespaces', 'urirefs', 'Thing', 'query', 'dquery', 'get_urirefs', 'get_namespaces', 'as_id', 'build', 'Property']
