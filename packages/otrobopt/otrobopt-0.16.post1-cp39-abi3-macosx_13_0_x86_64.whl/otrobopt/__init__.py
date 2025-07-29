2a8f2e515d68c6c91d335f696c598f4cff35154c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:23 +0000

    Add package otrobopt/0.16.post1/otrobopt-0.16.post1-cp39-abi3-macosx_13_0_x86_64.whl

---
+++ packages/otrobopt/otrobopt-0.16.post1-cp39-abi3-macosx_13_0_x86_64.whl/otrobopt/__init__.py
@@ -0,0 +1,18 @@
+"""
+    otrobopt --- An OpenTURNS module
+    ==================================
+
+    Contents
+    --------
+      'otrobopt' is a module for OpenTURNS
+
+"""
+
+# flake8: noqa
+
+# ensures swig type tables order & dll load
+import openturns as _ot
+
+from .otrobopt import *
+
+__version__ = '0.16.post1'
