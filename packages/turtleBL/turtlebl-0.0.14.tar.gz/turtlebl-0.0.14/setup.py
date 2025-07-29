93e868c8773b59814620916716108a343e550436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:33 +0000

    Add package turtleBL/0.0.14/turtlebl-0.0.14.tar.gz

---
+++ packages/turtleBL/turtlebl-0.0.14.tar.gz/turtlebl-0.0.14/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+root = r"E:\vs code\files\TurtleBL"
+
+setup(
+    name="turtleBL",
+    version=open(os.path.join(root, "tests\\verstion.txt")).read().strip(),
+    author="Back",
+    description="A Python library for running small bots in the roblox game Build Logic",
+    long_description=open(os.path.join(root, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    license="GPL-3.0-or-later",
+    license_files=["LICENSE"],
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
