ef05b7ada575a73ee2b89e1fb5c15be233b6cfcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:51 +0000

    Add package enable/6.1.0/enable-6.1.0-cp311-cp311-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/enable/enable-6.1.0-cp311-cp311-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/enable/trait_defs/api.py
@@ -0,0 +1,17 @@
+# (C) Copyright 2005-2023 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+""" API for enable.trait_defs subpackage.
+
+- :attr:`~.KivaFont`
+- :attr:`~.RGBAColor`
+"""
+
+from .kiva_font_trait import KivaFont
+from .rgba_color_trait import RGBAColor
