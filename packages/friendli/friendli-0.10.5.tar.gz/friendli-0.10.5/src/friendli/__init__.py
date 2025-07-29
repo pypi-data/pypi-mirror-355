264463ae6b3d142b684f93d3f09e5cc062fc12c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:25 +0000

    Add package friendli/0.10.5/friendli-0.10.5.tar.gz

---
+++ packages/friendli/friendli-0.10.5.tar.gz/friendli-0.10.5/src/friendli/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025-present, FriendliAI Inc. All rights reserved.
+
+"""Friendli Python SDK."""
+
+from __future__ import annotations
+
+from friendli_core import VERSION
+
+from .friendli import AsyncFriendli, SyncFriendli
