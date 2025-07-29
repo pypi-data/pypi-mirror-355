1118f04272c3b3f0ed241bc2da648eb14e5d0316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:50 +0000

    Add package evalassist/0.1.0/evalassist-0.1.0.tar.gz

---
+++ packages/evalassist/evalassist-0.1.0.tar.gz/evalassist-0.1.0/evalassist/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+import litellm
+
+litellm.drop_params = True
+
+
+root_pkg_logger = logging.getLogger(__name__)
+root_pkg_logger.propagate = False
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s"
+)
+handler.setFormatter(formatter)
+
+root_pkg_logger.addHandler(handler)
+
+root_pkg_logger.setLevel(logging.DEBUG)
