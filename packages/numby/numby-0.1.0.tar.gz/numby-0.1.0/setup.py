c357e67a62cc8005597fd9d9a90853c46fd7f668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:34 +0000

    Add package numby/0.1.0/numby-0.1.0.tar.gz

---
+++ packages/numby/numby-0.1.0.tar.gz/numby-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numby',
+    version='0.1.0',
+    author='Andew Sitalo',
+    author_email='andreysitalo09@gmail.com',
+    description='Short description',
+    url='https://github.com/yourusername/my_package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
