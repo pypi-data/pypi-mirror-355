fa0fffc4ef0e6a9ad8a8fadf1649a97985ca87b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:07 +0000

    Add package grabba/0.0.9/grabba-0.0.9.tar.gz

---
+++ packages/grabba/grabba-0.0.9.tar.gz/grabba-0.0.9/src/grabba/types/__init__.py
@@ -0,0 +1,100 @@
+# Re-export all types from individual files
+from .enums import (
+    JobStatus,
+    JobExecutionStatus,
+    JobTaskType,
+    SpecificDataExtractionType,
+    StructuredDataExtractionAttribute,
+    SpecificDataExtractionOutputFormat,
+    WebScreenCaptureFormat,
+    PdfPaperFormat,
+    PdfOrientation,
+    JobSchedulePolicy,
+    JobNavigationType,
+    PuppetDeviceType,
+    PuppetRegion,
+    WebDataSelectorType
+)
+
+from .job_schedule_types import (
+    JobSchedule
+)
+
+from .job_navigation_types import (
+    JobNavigation
+)
+
+from .job_types import (
+    Job,
+    JobDetail,
+    JobResult,
+    WebAgentConfig
+)
+
+from .job_task_types import (
+    JobTask,
+    JobTaskOutput,
+    SpecificDataExtractionOptions,
+    WebScreenCaptureOptions,
+    WebpageAsMarkdownOptions,
+    StructuredDataExtractionOptions,
+    StructuredDataExtractionParameter,
+    FormDataExtractionOptions,
+    TableDataExtractionOptions,
+    PresetDataExtractionOptions,
+    CustomDataExtractionOptions,
+)
+
+from .client_response_types import (
+    BaseResponse,
+    GetJobResponse,
+    GetJobsResponse,
+    GetJobResultResponse,
+    JobExecutionResponse,
+    JobCreationResponse,
+    JobEstimatedCostResponse,
+    JobStatsResponse
+)
+
+# Optional: Define __all__ to explicitly list what should be exported
+__all__ = [
+    "JobStatus",
+    "JobExecutionStatus",
+    "JobTaskType",
+    "SpecificDataExtractionType",
+    "StructuredDataExtractionAttribute",
+    "SpecificDataExtractionOutputFormat",
+    "WebScreenCaptureFormat",
+    "PdfPaperFormat",
+    "PdfOrientation",
+    "JobSchedulePolicy",
+    "JobNavigationType",
+    "PuppetDeviceType",
+    "PuppetRegion",
+    "WebDataSelectorType",
+    "Job",
+    "JobSchedule",
+    "JobNavigation",
+    "JobDetail",
+    "JobResult",
+    "JobTask",
+    "JobTaskOutput",
+    "SpecificDataExtractionOptions",
+    "WebScreenCaptureOptions",
+    "WebpageAsMarkdownOptions",
+    "StructuredDataExtractionOptions",
+    "StructuredDataExtractionParameter",
+    "FormDataExtractionOptions",
+    "TableDataExtractionOptions",
+    "PresetDataExtractionOptions",
+    "CustomDataExtractionOptions",
+    "BaseResponse",
+    "GetJobResponse",
+    "GetJobsResponse",
+    "GetJobResultResponse",
+    "JobCreationResponse",
+    "JobExecutionResponse",
+    "JobEstimatedCostResponse",
+    "JobStatsResponse",
+    "WebAgentConfig"
+]
