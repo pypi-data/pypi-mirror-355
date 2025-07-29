eab5b92c742ab1331eff83febcd90b9cbdf02741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:45 +0000

    Add package gdmec23120351/0.1.1/gdmec23120351-0.1.1-py3-none-any.whl

---
+++ packages/gdmec23120351/gdmec23120351-0.1.1-py3-none-any.whl/gdmec23120351/__init__.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP 
+mcpserver = FastMCP('gdmec23120351')
+@mcpserver.tool
+def sayHello(name):
+    """
+    打招呼，问候，寒暄
+    :param name:对方的名字
+    :return:打招呼的回复
+    """
+    return f'{name}您好，23120351张仕增和高兴认识您'
+
+def main():
+    mcpserver.run(transport ='stdio')
