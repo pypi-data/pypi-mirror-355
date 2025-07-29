4bfd1a21326f84553da0bf585d5afd0ffa322c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    Add package napari-tapenade-processing/0.0.14/napari_tapenade_processing-0.0.14.tar.gz

---
+++ packages/napari-tapenade-processing/napari_tapenade_processing-0.0.14.tar.gz/napari_tapenade_processing-0.0.14/src/napari_tapenade_processing/_custom_widgets.py
@@ -0,0 +1,130 @@
+from qtpy.QtCore import QPoint, Qt, QRect
+from qtpy.QtWidgets import (
+    QApplication,
+    QLabel,
+    QPushButton,
+)
+from qtpy import QtGui
+
+
+class HoverTooltipButton(QPushButton):
+    def __init__(self, text, parent=None):
+        super().__init__(text, parent)
+        self.setFixedSize(20, 20)  # Small square button
+        self.setText("?")  # Add interrogation mark inside the button
+        self.setCheckable(True)  # Toggle button to show/hide the tooltip
+
+        # Create the custom tooltip as a top-level widget (not a child of the button)
+        self.text_box = QLabel()
+        self.text_box.setText(text)
+        self.text_box.setStyleSheet(
+            "background-color: yellow; border: 1px solid black; padding: 5px;"
+        )
+        self.text_box.setWindowFlags(Qt.ToolTip)  # Make it look like a tooltip
+        self.text_box.hide()
+
+        # Enable mouse tracking to track mouse movement inside the button
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            if self.isChecked():
+                self.setChecked(False)  # Uncheck to hide the tooltip
+                self.text_box.hide()
+            else:
+                self.setChecked(True)  # Check to show the tooltip
+                # move the tooltip to the mouse position
+                self.adjust_tooltip_position(event.globalPos())
+                self.text_box.show()  # Immediately show tooltip on click
+        super().mousePressEvent(event)
+
+    def leaveEvent(self, event):
+        # Hide the text box when mouse leaves the button
+        self.text_box.hide()
+        self.setChecked(False)  # Uncheck to hide the tooltip
+        super().leaveEvent(event)
+
+    def mouseMoveEvent(self, event):
+        # Update tooltip position to follow the mouse using global coordinates
+        if self.isChecked():
+            # self.text_box.move(event.globalPos() + QPoint(10, 10))  # Offset for better visibility
+            self.adjust_tooltip_position(event.globalPos())
+        super().mouseMoveEvent(event)
+
+    def adjust_tooltip_position(self, cursor_pos):
+        screen = QApplication.screenAt(cursor_pos)
+
+        if screen is not None:
+            screen_rect = (
+                screen.availableGeometry()
+            )  # Get the geometry of the screen with the cursor
+        else:
+            screen_rect = (
+                QApplication.desktop().availableGeometry()
+            )  # Fallback to primary screen
+
+        # Get the size of the tooltip
+        tooltip_size = self.text_box.sizeHint()
+
+        # Calculate the desired position of the tooltip
+        new_x = cursor_pos.x() + 10  # Offset for better visibility
+        new_y = cursor_pos.y() + 10
+
+        # Adjust the position if the tooltip goes beyond the screen's right edge
+        if new_x + tooltip_size.width() > screen_rect.right():
+            new_x = (
+                screen_rect.right() - tooltip_size.width() - 10
+            )  # Shift left
+
+        # Adjust the position if the tooltip goes beyond the screen's bottom edge
+        if new_y + tooltip_size.height() > screen_rect.bottom():
+            new_y = (
+                screen_rect.bottom() - tooltip_size.height() - 10
+            )  # Shift up
+
+        # Adjust the position if the tooltip goes beyond the screen's left edge
+        if new_x < screen_rect.left():
+            new_x = screen_rect.left() + 10  # Shift right
+
+        # Adjust the position if the tooltip goes beyond the screen's top edge
+        if new_y < screen_rect.top():
+            new_y = screen_rect.top() + 10  # Shift down
+
+        # Move the tooltip to the new adjusted position
+        self.text_box.move(QPoint(new_x, new_y))
+
+
+class SwitchButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setCheckable(True)
+        self.setChecked(True)
+        self.setMinimumWidth(66)
+        self.setMinimumHeight(22)
+
+    def paintEvent(self, event):
+        label = "ON" if self.isChecked() else "OFF"
+        bg_color = Qt.blue if self.isChecked() else Qt.blue
+
+        radius = 10
+        width = 32
+        center = self.rect().center()
+
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.translate(center)
+        painter.setBrush(QtGui.QColor(0, 0, 0))
+
+        pen = QtGui.QPen(Qt.black)
+        pen.setWidth(2)
+        painter.setPen(pen)
+
+        painter.drawRoundedRect(
+            QRect(-width, -radius, 2 * width, 2 * radius), radius, radius
+        )
+        painter.setBrush(QtGui.QBrush(bg_color))
+        sw_rect = QRect(-radius, -radius, width + radius, 2 * radius)
+        if not self.isChecked():
+            sw_rect.moveLeft(-width)
+        painter.drawRoundedRect(sw_rect, radius, radius)
+        painter.drawText(sw_rect, Qt.AlignCenter, label)
---
+++ packages/napari-tapenade-processing/napari_tapenade_processing-0.0.14.tar.gz/napari_tapenade_processing-0.0.14/src/napari_tapenade_processing/_macro_recorder.py
@@ -0,0 +1,122 @@
+import json
+import os
+from collections import OrderedDict
+from datetime import datetime
+
+"""
+### TODO:
+    - change the macro recorded params structure to have
+      INPUTS ? (i) variable parameters (i.e params that are supposed to
+          change when you run the macro)
+      FUNC PARAMS ? (ii) fixed parameters that remain the same across runs
+"""
+
+
+class MacroRecorder:
+    def __init__(self):
+        self._reset_recording()
+
+    def _reset_recording(self):
+        self._recorded_functions_calls_list = []
+        self._layer_unique_id = -1
+        self._name_to_layer_unique_id_dict = {}
+
+    def dump_recorded_parameters(self, path: str):
+        date = (
+            str(datetime.now())
+            .split(".")[:-1][0]
+            .replace(" ", "_")
+            .replace(":", "-")
+        )
+        filename = f"recorded_parameters_{date}.json"
+
+        with open(os.path.join(path, filename), "w") as f:
+            json.dump(self._recorded_functions_calls_list, f, indent=4)
+
+        self._reset_recording()
+
+    def record(
+        self,
+        function_name: str,
+        input_params_to_layer_names_and_types_dict: dict,
+        output_params_to_layer_names_and_types_dict: OrderedDict,
+        func_params: dict,
+        main_input_param_name: str,
+    ):
+        """
+        Parameters
+        ----------
+        function_name : str
+            The name of the function that is being recorded
+
+        func_params : dict
+            A dictionary that maps the name of the parameters of the
+            function to their values
+
+        Creates a dictionary with structure:
+        {
+            "func_name",
+            "func_params",
+            "main_input_param_name",
+            "input_params_to_layer_ids_and_types_dict",
+            "output_params_to_layer_ids_and_types_dict",
+        }
+        """
+
+        input_params_to_layer_ids_and_types_dict = {}
+
+        for input_param, (
+            name,
+            layer_type,
+        ) in input_params_to_layer_names_and_types_dict.items():
+
+            if name is None:
+                layer_unique_id = None
+            elif name in self._name_to_layer_unique_id_dict:
+                layer_unique_id = self._name_to_layer_unique_id_dict[name]
+            else:
+                self._layer_unique_id += 1
+                self._name_to_layer_unique_id_dict[name] = (
+                    self._layer_unique_id
+                )
+
+                layer_unique_id = self._layer_unique_id
+
+            input_params_to_layer_ids_and_types_dict[input_param] = (
+                layer_unique_id,
+                layer_type,
+            )
+
+        output_params_to_layer_ids_and_types_dict = OrderedDict()
+
+        for output_param, (
+            name,
+            layer_type,
+        ) in output_params_to_layer_names_and_types_dict.items():
+
+            if name is None:
+                layer_unique_id = None
+            elif name in self._name_to_layer_unique_id_dict:
+                layer_unique_id = self._name_to_layer_unique_id_dict[name]
+            else:
+                self._layer_unique_id += 1
+                self._name_to_layer_unique_id_dict[name] = (
+                    self._layer_unique_id
+                )
+
+                layer_unique_id = self._layer_unique_id
+
+            output_params_to_layer_ids_and_types_dict[output_param] = (
+                layer_unique_id,
+                layer_type,
+            )
+
+        params = {
+            "func_name": function_name,
+            "func_params": func_params,
+            "main_input_param_name": main_input_param_name,
+            "input_params_to_layer_ids_and_types_dict": input_params_to_layer_ids_and_types_dict,
+            "output_params_to_layer_ids_and_types_dict": output_params_to_layer_ids_and_types_dict,
+        }
+
+        self._recorded_functions_calls_list.append(params)
