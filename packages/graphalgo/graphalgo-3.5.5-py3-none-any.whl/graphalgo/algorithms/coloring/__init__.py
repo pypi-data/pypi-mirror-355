3f2dca2aac77d44313d132d284cdc9b6dbb00324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:49 +0000

    Add package graphalgo/3.5.5/graphalgo-3.5.5-py3-none-any.whl

---
+++ packages/graphalgo/graphalgo-3.5.5-py3-none-any.whl/graphalgo/algorithms/coloring/__init__.py
@@ -0,0 +1,4 @@
+from graphalgo.algorithms.coloring.greedy_coloring import *
+from graphalgo.algorithms.coloring.equitable_coloring import equitable_color
+
+__all__ = ["greedy_color", "equitable_color"]
