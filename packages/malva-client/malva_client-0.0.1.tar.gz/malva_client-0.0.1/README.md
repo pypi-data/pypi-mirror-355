7526f6deb6ac7c55c73826255dca60e5bbad66fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:24 +0000

    Add package malva-client/0.0.1/malva_client-0.0.1.tar.gz

---
+++ packages/malva-client/malva_client-0.0.1.tar.gz/malva_client-0.0.1/README.md
@@ -0,0 +1,19 @@
+# Malva Client
+
+🚧 **This package is currently under development** 🚧
+
+Malva Client will be here very soon
+
+## Installation
+
+```bash
+pip install malva_client
+```
+
+## Contact
+
+For questions or suggestions, please contact [daniel.leonperinan@mdc-berlin.de](mailto:daniel.leonperinan@mdc-berlin.de).
+
+## License
+
+Copyright 2025 Malva
---
+++ packages/malva-client/malva_client-0.0.1.tar.gz/malva_client-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="malva-client",
+    version="0.0.1",
+    author="Daniel León-Periñán",
+    author_email="daniel.leonperinan@mdc-berlin.de",
+    description="Malva Client for the Malva Index",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/malva-bio/malva-client",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],
+)
