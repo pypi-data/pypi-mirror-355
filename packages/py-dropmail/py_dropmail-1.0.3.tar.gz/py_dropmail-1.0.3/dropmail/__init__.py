f96c950c32cd2135475710c6818883e5b1c399e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:29 +0000

    Add package py-dropmail/1.0.3/py_dropmail-1.0.3.tar.gz

---
+++ packages/py-dropmail/py_dropmail-1.0.3.tar.gz/py_dropmail-1.0.3/dropmail/__init__.py
@@ -0,0 +1,15 @@
+from .__version__ import __version__, __version_info__
+from .client import DropMail, AsyncDropMail, dropmail, async_dropmail
+from .exceptions import DropMailError, SessionExpiredError, NetworkError
+
+__all__ = [
+    'dropmailplus', 
+    'AsyncDropMail',
+    'dropmail', 
+    'async_dropmail',
+    'DropMailError', 
+    'SessionExpiredError',
+    'NetworkError'
+]
+
+DropMail = dropmailplus
