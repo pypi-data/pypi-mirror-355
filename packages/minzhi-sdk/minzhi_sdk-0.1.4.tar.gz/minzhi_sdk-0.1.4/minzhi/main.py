762f94e1beeaee3a1120631a3ac7c5428fbb82a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:40 +0000

    Add package minzhi-sdk/0.1.4/minzhi_sdk-0.1.4.tar.gz

---
+++ packages/minzhi-sdk/minzhi_sdk-0.1.4.tar.gz/minzhi_sdk-0.1.4/minzhi/main.py
@@ -0,0 +1,68 @@
+from typing import Any, Dict, List
+from loguru import logger
+from minzhi import res, invoke, invoke_enum
+
+
+
+
+class AE:
+    """AE CLient"""
+
+    def __init__(self, businessId: str, table_name: str):
+        self.businessId = businessId
+        self.table_name = table_name
+
+    @logger.catch
+    def selectPage(self, current: int, pageSize: int)-> Dict[str, Any]:
+        """
+        分页查询
+        Args:
+            current: 当前页码
+            pageSize: 每页条数
+        Returns:
+            Dict[str, Any]: 查询结果
+        """
+        return invoke(
+            self.businessId,
+            "selectPage",
+            {
+                "current": current,
+                "pageSize": pageSize,
+            },
+            self.businessId
+        )
+
+    @logger.catch
+    def selectAll(self)-> Dict[str, Any]:
+        """
+        查询所有
+        Returns:
+            Dict[str, Any]: 查询结果
+        """
+        return invoke(
+            self.table_name,
+            "selectAll",
+            {},
+            self.businessId
+        )
+
+    @logger.catch
+    def selectOne(self, query: Dict[str, Any], fields: List[str])-> Dict[str, Any]:
+        """
+        根据id查询
+        Args:
+            query: 查询条件
+        Returns:
+            Dict[str, Any]: 查询结果
+        """
+        query_str = " and ".join([f"{k} = '{v}'" for k, v in query.items()])
+        data = {
+            "query": query_str,
+            "show": fields
+        }
+        return invoke(
+            self.table_name,
+            "selectOne",
+            data,
+            self.businessId
+        )
