2a05b288279d90785cc7dd38ad938bd8bbc8f5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:52 +0000

    Add package xcu/1.0.1a0/xcu-1.0.1a0.tar.gz

---
+++ packages/xcu/xcu-1.0.1a0.tar.gz/xcu-1.0.1a0/plantillas/telegram-bot/python-telegram-bot/simple/bot/utils.py
@@ -0,0 +1,4 @@
+# Aquí puedes poner funciones de utilidad, como validaciones, formateos, etc.
+
+def ejemplo_utilidad():
+    return "Esto es una función de utilidad."
---
+++ packages/xcu/xcu-1.0.1a0.tar.gz/xcu-1.0.1a0/xcu/__init__.py
@@ -0,0 +1,3 @@
+"""
+xpip - Un wrapper de pip para instalar paquetes desde el repositorio PyPI de Cuba
+"""
