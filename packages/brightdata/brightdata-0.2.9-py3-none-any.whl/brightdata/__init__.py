a4b7837ef8b652e0e11a0d7451eb1cd323ff6ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:42 +0000

    Add package brightdata/0.2.9/brightdata-0.2.9-py3-none-any.whl

---
+++ packages/brightdata/brightdata-0.2.9-py3-none-any.whl/brightdata/__init__.py
@@ -0,0 +1,7 @@
+# this is brightdata/__init__.py
+
+from .brightdata_web_unlocker import BrightdataWebUnlocker
+from .base_specialized_scraper import BrightdataBaseSpecializedScraper
+from .auto import scrape_url, trigger_scrape_url, trigger_scrape_url_with_fallback
+from .browser_api import BrowserAPI
+
