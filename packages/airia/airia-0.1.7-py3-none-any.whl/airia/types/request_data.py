9b61fd960de181cc80599c0df65e14f8383f805b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:35 +0000

    Add package airia/0.1.7/airia-0.1.7-py3-none-any.whl

---
+++ packages/airia/airia-0.1.7-py3-none-any.whl/airia/types/request_data.py
@@ -0,0 +1,10 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel
+
+
+class RequestData(BaseModel):
+    url: str
+    payload: Optional[Dict[str, Any]]
+    headers: Dict[str, Any]
+    correlation_id: str
