8bf8b810392a22025a0373150ba5432eba5d43bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:01 +0000

    Add package boltz-vsynthes/1.0.11/boltz_vsynthes-1.0.11-py3-none-any.whl

---
+++ packages/boltz-vsynthes/boltz_vsynthes-1.0.11-py3-none-any.whl/boltz/data/parse/pdb.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+from typing import Optional
+
+from Bio import PDB
+from Bio.PDB.Polypeptide import three_to_one
+from Bio.Data.IUPACData import protein_letters_3to1
+from rdkit import Chem
+from rdkit.Chem.rdchem import Mol
+
+from boltz.data.types import Target
+from boltz.data.parse.yaml import parse_boltz_schema
+
+
+def parse_pdb(
+    path: Path,
+    ccd: dict[str, Mol],
+    mol_dir: Path,
+    boltz2: bool = False,
+) -> Target:
+    """Parse a PDB file.
+
+    Parameters
+    ----------
+    path : Path
+        Path to the PDB file.
+    ccd : Dict
+        Dictionary of CCD components.
+    mol_dir : Path
+        Path to the directory containing the molecules.
+    boltz2 : bool
+        Whether to parse the input for Boltz2.
+
+    Returns
+    -------
+    Target
+        The parsed target.
+    """
+    # Read PDB file
+    parser = PDB.PDBParser(QUIET=True)
+    structure = parser.get_structure("protein", str(path))
+
+    # Convert to yaml format
+    sequences = []
+    for model in structure:
+        for chain in model:
+            # Get chain sequence
+            seq = ""
+            for residue in chain:
+                if residue.id[0] == " ":  # Only standard residues
+                    try:
+                        seq += protein_letters_3to1[residue.resname]
+                    except KeyError:
+                        continue
+
+            if seq:  # Only add if sequence is not empty
+                molecule = {
+                    "protein": {
+                        "id": chain.id,
+                        "sequence": seq,
+                        "modifications": [],
+                    },
+                }
+                sequences.append(molecule)
+
+    data = {
+        "sequences": sequences,
+        "bonds": [],
+        "version": 1,
+    }
+
+    name = path.stem
+    return parse_boltz_schema(name, data, ccd, mol_dir, boltz2) 
