a86488fff1056673fbdd09ced975078a761adb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:11 +0000

    Add package image-processing-diogodevtest/0.0.1/image-processing-diogodevtest-0.0.1.tar.gz

---
+++ packages/image-processing-diogodevtest/image-processing-diogodevtest-0.0.1.tar.gz/image-processing-diogodevtest-0.0.1/README.md
@@ -0,0 +1,30 @@
+# Image Processing
+
+Description. 
+The package image_processing is used to:
+
+	Processing:
+	    - Histogram matching
+        - Structural similarity
+        - Resize image
+    
+    Utils:
+        - Read image
+        - Save image
+        - Plot image
+        - Plot result
+        - plot histogram
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install image_processing
+```
+
+## Author
+Diogo Carodso
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/image-processing-diogodevtest/image-processing-diogodevtest-0.0.1.tar.gz/image-processing-diogodevtest-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image-processing-diogodevtest",
+    version="0.0.1",
+    author="Diogo_Cardoso",
+    author_email="diocardosodev@gmail.com",
+    description="Pacote de teste para processamento de imagens",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diogo-tcardoso/image-processing",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
