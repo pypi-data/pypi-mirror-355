b7fe3af055b075a4298ba7a79f03a74f40967342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:50 +0000

    Add package apek/0.0.2b1/apek-0.0.2b1.tar.gz

---
+++ packages/apek/apek-0.0.2b1.tar.gz/apek-0.0.2b1/apek/_text.py
@@ -0,0 +1,37 @@
+def _mergestr(*s):
+    return "\n".join(s)
+
+text = {
+    "en": {
+        "helps.function.setLang.languageNotSupport": "Unsupported language: ",
+        "helps.function.setLang.languageNotSupport2": "Supported languages: ",
+        "helps.function.updateLog.versionNotFound": "Version not found: ",
+        "helps.function.updateLog.versionFormatError": "The version code format is incorrect: ",
+        "helps.upgradeLogs.0_0_1": "The first version, this module was just created."
+    },
+    "zh": {
+        "helps.function.setLang.languageNotSupport": "不支持的语言：",
+        "helps.function.setLang.languageNotSupport2": "支持以下语言：",
+        "helps.function.updateLog.versionNotFound": "无法找到版本号：",
+        "helps.function.updateLog.versionFormatError": "版本号格式错误：",
+        "helps.upgradeLogs.0_0_1": "第一个版本，这个模块刚被创建。"
+    },
+    "zh": {
+        "helps.function.setLang.languageNotSupport": "不支持之语言：",
+        "helps.function.setLang.languageNotSupport2": "支持之语言：",
+        "helps.function.updateLog.versionNotFound": "未得版本号：",
+        "helps.function.updateLog.versionFormatError": "版本号格式有误：",
+        "helps.upgradeLogs.0_0_1": "初版，此模块方始创设。"
+    },
+    "fr": {
+        "helps.function.setLang.languageNotSupport": "Les langues non prises en charge: ",
+        "helps.function.setLang.languageNotSupport2": "Prend en charge les langues suivantes: ",
+        "helps.function.updateLog.versionNotFound": "Impossible de trouver le numéro de version: ",
+        "helps.function.updateLog.versionFormatError": "Le format du numéro de version est incorrect: ",
+        "helps.upgradeLogs.0_0_1": "Première version, ce module vient d'être créé."
+    },
+    "default": {
+        "meta.version": "0.0.2b1",
+        "meta.lastVersion": "0.0.2a3"
+    }
+}
