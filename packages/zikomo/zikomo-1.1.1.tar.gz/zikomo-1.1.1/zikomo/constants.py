055f605b093e5812e24abe1966c54dcad423d6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:33 +0000

    Add package zikomo/1.1.1/zikomo-1.1.1.tar.gz

---
+++ packages/zikomo/zikomo-1.1.1.tar.gz/zikomo-1.1.1/zikomo/constants.py
@@ -0,0 +1,24 @@
+FLIGHTBITE_DIR = "E:\\ZikomoSolutions\\Projects\\FlightBiteBlazor"
+BACKOFFICE_DIR ="E:\\ZikomoSolutions\\Projects\\ZikomoMini\\Zikomo"
+WEBSITES_DIR ="E:\\ZikomoSolutions\\Projects\\ZikomoMini\\MiniWebsitesV3"
+
+RELEASE_DOCS_FILE = "E:\\ZikomoSolutions\\Images\\ServerUpdatePics\\staging-2025.md"
+
+FLIGHTBITE_REGISTRY = "zikomo.azurecr.io/flightbite"
+BACKOFFICE_REGISTRY = "zikomo.azurecr.io/mini"
+WEBSITES_REGISTRY = "zikomo.azurecr.io/mini-websites-v3"
+
+SOLUTION_ROOT = "E:/ZikomoSolutions/Projects/ZikomoMini/Zikomo"
+STARTUP_PROJECT = "Zikomo.Main.BackendHost"    
+PROJECT_DIR = r"E:\ZikomoSolutions\ProjectResources\ZikomoMini"
+OUTPUT_DIR = r"E:\ZikomoSolutions\ProjectResources\ZikomoMini\Scripts"
+
+EMAIL_SETTINGS = {
+    "sender": "info@zikomo.io",
+    "receiver": ["imran.shah@zikomosolutions.com"],
+    "smtp_server": "mail.zikomosolutions.net",
+    "smtp_port": 587,
+    "username": "info@zikomo.io",
+    "password": "Developer#1"
+}
+
