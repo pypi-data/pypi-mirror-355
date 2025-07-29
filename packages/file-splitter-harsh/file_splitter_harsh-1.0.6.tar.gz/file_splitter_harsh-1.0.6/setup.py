49175bf863dbf404425a397ee271c6124ffff9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:56 +0000

    Add package file-splitter-harsh/1.0.6/file_splitter_harsh-1.0.6.tar.gz

---
+++ packages/file-splitter-harsh/file_splitter_harsh-1.0.6.tar.gz/file_splitter_harsh-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.6"
+DESCRIPTION = "A simple Python library to split large text files into smaller parts."
+LONG_DESCRIPTION = "A Python package to split large text files into smaller parts easily."
+
+# Setting up
+setup(
+    name="file_splitter_harsh",
+    version=VERSION,
+    author="Harsh Mendapara",
+    author_email="mendapara.harsh47@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    install_requires=[],
+    url="https://github.com/harshh351998/filesplitter.git",
+    download_url="https://github.com/harshh351998/filesplitter/archive/refs/tags/1.0.0.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
