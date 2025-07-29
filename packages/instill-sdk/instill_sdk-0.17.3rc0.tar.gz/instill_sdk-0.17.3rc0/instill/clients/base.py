37a0bb2dacbaacd21c85280cf91d061aa6196072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:37 +0000

    Add package instill-sdk/0.17.3rc0/instill_sdk-0.17.3rc0.tar.gz

---
+++ packages/instill-sdk/instill_sdk-0.17.3rc0.tar.gz/instill_sdk-0.17.3rc0/instill/clients/base.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+import google.protobuf.message
+import grpc
+
+
+class Client(ABC):
+    """Base interface class for creating mgmt/pipeline/model clients.
+
+    Args:
+        ABC (abc.ABCMeta): std abstract class
+    """
+
+    @property
+    @abstractmethod
+    def host(self):
+        pass
+
+    @host.setter
+    @abstractmethod
+    def host(self):
+        pass
+
+    @property
+    @abstractmethod
+    def metadata(self):
+        pass
+
+    @metadata.setter
+    @abstractmethod
+    def metadata(self):
+        pass
+
+    @abstractmethod
+    def liveness(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def readiness(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_serving(self):
+        raise NotImplementedError
+
+
+class RequestFactory:
+    def __init__(
+        self,
+        method: Union[grpc.UnaryUnaryMultiCallable, grpc.StreamUnaryMultiCallable],
+        request: google.protobuf.message.Message,
+        metadata,
+    ) -> None:
+        self.method = method
+        self.request = request
+        self.metadata = metadata
+
+    def send_sync(self):
+        return self.method(request=self.request, metadata=self.metadata)
+
+    def send_stream(self):
+        return self.method(
+            request_iterator=iter([self.request]),
+            metadata=self.metadata,
+        )
+
+    async def send_async(self):
+        return await self.method(request=self.request, metadata=self.metadata)
