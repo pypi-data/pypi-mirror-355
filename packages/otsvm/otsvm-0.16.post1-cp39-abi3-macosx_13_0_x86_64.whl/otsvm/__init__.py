9f326e497279c22ac485945829583f31acf06f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:23 +0000

    Add package otsvm/0.16.post1/otsvm-0.16.post1-cp39-abi3-macosx_13_0_x86_64.whl

---
+++ packages/otsvm/otsvm-0.16.post1-cp39-abi3-macosx_13_0_x86_64.whl/otsvm/__init__.py
@@ -0,0 +1,25 @@
+"""
+    otsvm
+    =====
+
+    Documentation is available online at http://www.openturns.org.
+
+    Contents
+    --------
+    'otsvm' is python module for Open TURNS, which enables to perform support vector regression
+    and classification using LibSVM and to manipulate results with OpenTURNS.
+
+    Classes
+    --------
+    SVMRegression                                The class that supports the regression.
+
+"""
+
+# flake8: noqa
+
+# ensures swig type tables order & dll load
+import openturns as _ot
+
+from .otsvm import *
+
+__version__ = '0.16.post1'
