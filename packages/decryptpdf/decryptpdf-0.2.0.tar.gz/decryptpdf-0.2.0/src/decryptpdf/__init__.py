783fbbed6213ddbb46ddbae3c8fbf2bc42c3e00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:36 +0000

    Add package decryptpdf/0.2.0/decryptpdf-0.2.0.tar.gz

---
+++ packages/decryptpdf/decryptpdf-0.2.0.tar.gz/decryptpdf-0.2.0/src/decryptpdf/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.2.0"
+from ._main import decrypt_pdf
+
+__all__ = ["decrypt_pdf"]
---
+++ packages/decryptpdf/decryptpdf-0.2.0.tar.gz/decryptpdf-0.2.0/src/decryptpdf/__main__.py
@@ -0,0 +1,5 @@
+"""Make the CLI runnable using python -m decryptpdf."""
+
+from .cli import app
+
+app(prog_name="decryptpdf")
