95a79390836eeef26748084a00bf094318700d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:02 +0000

    Add package aptamerforge/0.1.7.1/aptamerforge-0.1.7.1.tar.gz

---
+++ packages/aptamerforge/aptamerforge-0.1.7.1.tar.gz/aptamerforge-0.1.7.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aptamerforge',
+    version='0.1.7.1',
+    description='A DNA aptamer screening tool for mismatch analysis and hairpin detection & drawing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='William Asamoah',
+    author_email='cephaswills@gmail.com',
+    url='https://github.com/Feicheiel/aptamerforge',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'viennarna',  # if using pip version of ViennaRNA
+        'pycairo>=1.20.0b'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
