e63dbbaee3c4aad7829c5fc11d8cf3fff97a4885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:00 +0000

    Add package aedev-git-repo-manager/0.3.98/aedev_git_repo_manager-0.3.98.tar.gz

---
+++ packages/aedev-git-repo-manager/aedev_git_repo_manager-0.3.98.tar.gz/aedev_git_repo_manager-0.3.98/setup.py
@@ -0,0 +1,17 @@
+""" temporary disabled setup.py template to publish new grm version. """
+import pprint
+import sys
+
+import setuptools
+
+from aedev.setup_project import project_env_vars    # type: ignore
+
+pev = project_env_vars(from_setup=True)
+
+if __name__ == "__main__":
+    print("#  EXECUTING SETUPTOOLS SETUP: argv, kwargs  ###################")
+    print(pprint.pformat(sys.argv, indent=3, width=75, compact=True))
+    setup_kwargs = pev['setup_kwargs']
+    print(pprint.pformat(setup_kwargs, indent=3, width=75, compact=True))
+    setuptools.setup(**setup_kwargs)
+    print("#  FINISHED SETUPTOOLS SETUP  ##################################")
