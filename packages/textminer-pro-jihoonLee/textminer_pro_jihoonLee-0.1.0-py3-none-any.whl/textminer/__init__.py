588461934cbc7caafbf34050568e7f3bd1c3b0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:55 +0000

    Add package textminer-pro-jihoonLee/0.1.0/textminer_pro_jihoonLee-0.1.0-py3-none-any.whl

---
+++ packages/textminer-pro-jihoonLee/textminer_pro_jihoonLee-0.1.0-py3-none-any.whl/textminer/__init__.py
@@ -0,0 +1,3 @@
+from .cleaner import remove_stopwords, extract_keywords
+from .summarizer import summarize_text
+from .detector import detect_language
---
+++ packages/textminer-pro-jihoonLee/textminer_pro_jihoonLee-0.1.0-py3-none-any.whl/textminer/summarizer.py
@@ -0,0 +1,9 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+def summarize_text(text: str, num_sentences=2) -> str:
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, num_sentences)
+    return " ".join(str(sentence) for sentence in summary)
