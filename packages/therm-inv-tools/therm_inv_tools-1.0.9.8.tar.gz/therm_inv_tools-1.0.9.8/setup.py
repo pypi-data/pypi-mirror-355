0fb8ee799e153bebccfe5e3168c2b195cbbc8381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:22 +0000

    Add package therm-inv-tools/1.0.9.8/therm_inv_tools-1.0.9.8.tar.gz

---
+++ packages/therm-inv-tools/therm_inv_tools-1.0.9.8.tar.gz/therm_inv_tools-1.0.9.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools 
+  
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="therm_inv_tools", 
+    version="1.0.9.8", 
+    author="Anthony R. Osborne", 
+    author_email="anthony.r.osborne019@pm.me", 
+    packages=["tools"], 
+    description="A package containing all my tools for thermal inversions", 
+    long_description=description, 
+    long_description_content_type="text/markdown", 
+    url="https://github.com/Anthony904175/therm_inv_tools.git", 
+    license='MIT', 
+    python_requires='>=3.8', 
+    install_requires=[] 
+) 
