341d571e825d94ee6c7a51f2231e821c2f40e0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:25 +0000

    Add package malva/0.0.1/malva-0.0.1.tar.gz

---
+++ packages/malva/malva-0.0.1.tar.gz/malva-0.0.1/README.md
@@ -0,0 +1,19 @@
+# Malva
+
+🚧 **This package is currently under development** 🚧
+
+Malva will be here very soon
+
+## Installation
+
+```bash
+pip install malva
+```
+
+## Contact
+
+For questions or suggestions, please contact [daniel.leonperinan@mdc-berlin.de](mailto:daniel.leonperinan@mdc-berlin.de).
+
+## License
+
+Copyright 2025 Malva
---
+++ packages/malva/malva-0.0.1.tar.gz/malva-0.0.1/malva/__init__.py
@@ -0,0 +1,18 @@
+"""
+Generate the Malva Index
+"""
+
+__version__ = "0.0.1"
+__author__ = "Daniel León-Periñán"
+__email__ = "daniel.leonperinan@mdc-berlin.de"
+
+def run_malva():
+    """
+    A function to say we will be there soon
+    
+    Returns:
+        str: A message
+    """
+    return "Malva will be here soon!"
+
+__all__ = ["run_malva"]
