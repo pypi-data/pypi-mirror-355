ef1bf0fce711f59d19ddc84d37c9fe3116d552f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:15 +0000

    Add package py-ballisticcalc.exts/2.2.0b1/py_ballisticcalc_exts-2.2.0b1.tar.gz

---
+++ packages/py-ballisticcalc.exts/py_ballisticcalc_exts-2.2.0b1.tar.gz/py_ballisticcalc_exts-2.2.0b1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""setup.py script for py_ballisticcalc library"""
+from setuptools import setup, Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import sys
+    # use this command to skip building wheel and compiling modules on unsupported platforms
+    # pip install --no-build-isolation --no-binary :all: py-ballisticcalc.exts
+    setup()
+    sys.exit(0)  # Stop installation
+
+
+compiler_directives = {
+    "language_level": 3,
+    "embedsignature": True,
+    "cdivision": True,
+    "binding": False,
+    "c_api_binop_methods": True,
+    "warn.undeclared": True,
+    "warn.unreachable": True,
+    "warn.maybe_uninitialized": True,
+    "warn.unused": True,
+    "warn.unused_arg": True,
+    "warn.multiple_declarators": True,
+    "show_performance_hints": True,
+}
+
+extension_names = [
+    "vector",
+    "cy_bindings",
+    "base_engine",
+    "euler_engine",
+    "rk4_engine",
+    "trajectory_data",
+]
+
+extensions = [
+    Extension('py_ballisticcalc_exts.' + name,
+              ['py_ballisticcalc_exts/' + name + '.pyx'])
+    for name in extension_names
+]
+
+extensions = cythonize(extensions,
+                       compiler_directives=compiler_directives,
+                       annotate=True)
+
+setup(ext_modules=extensions)
