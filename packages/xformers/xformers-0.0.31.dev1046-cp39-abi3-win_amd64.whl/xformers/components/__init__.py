05f2581c8e54826cfe9c12ffb02f14159b8abce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:46 +0000

    Add package xformers/0.0.31.dev1046/xformers-0.0.31.dev1046-cp39-abi3-win_amd64.whl

---
+++ packages/xformers/xformers-0.0.31.dev1046-cp39-abi3-win_amd64.whl/xformers/components/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import warnings
+from pathlib import Path
+
+from xformers.utils import import_all_modules
+
+from .attention import Attention, build_attention  # noqa
+from .input_projection import InputProjection, InputProjectionConfig  # noqa
+from .residual import NormalizationType  # noqa
+from .residual import PostNorm  # noqa
+from .residual import PreNorm  # noqa
+from .residual import RequiresWrappedInputs  # noqa
+from .residual import Residual  # noqa
+from .residual import ResidualNormStyle  # noqa
+
+warnings.warn(
+    "xformers.components is deprecated and is not maintained anymore. "
+    "It might be removed in a future version of xFormers ",
+    FutureWarning,
+    stacklevel=2,
+)
+
+
+# automatically import any Python files in the directory
+import_all_modules(str(Path(__file__).parent), "xformers.components")
