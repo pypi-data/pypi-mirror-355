c7b99e2524d5aeb250ea763c9fa1853b3365ec98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:25 +0000

    Add package mflux/0.8.0/mflux-0.8.0-py3-none-any.whl

---
+++ packages/mflux/mflux-0.8.0-py3-none-any.whl/mflux/ui/defaults.py
@@ -0,0 +1,16 @@
+BATTERY_PERCENTAGE_STOP_LIMIT = 5
+CONTROLNET_STRENGTH = 0.4
+DEFAULT_DEV_FILL_GUIDANCE = 30
+DEFAULT_DEPTH_GUIDANCE = 10
+GUIDANCE_SCALE = 3.5
+HEIGHT, WIDTH = 1024, 1024
+IMAGE_STRENGTH = 0.4
+MODEL_CHOICES = ["dev", "dev-fill", "schnell"]
+MODEL_INFERENCE_STEPS = {
+    "dev": 14,
+    "dev-fill": 14,
+    "dev-depth": 14,
+    "dev-redux": 14,
+    "schnell": 4,
+}
+QUANTIZE_CHOICES = [3, 4, 6, 8]
