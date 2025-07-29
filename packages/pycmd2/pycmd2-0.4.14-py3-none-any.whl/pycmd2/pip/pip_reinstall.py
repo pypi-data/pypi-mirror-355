3a92989dbd0ebc3a177225c14cbfe71619b95f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:09 +0000

    Add package pycmd2/0.4.14/pycmd2-0.4.14-py3-none-any.whl

---
+++ packages/pycmd2/pycmd2-0.4.14-py3-none-any.whl/pycmd2/pip/pip_reinstall.py
@@ -0,0 +1,36 @@
+"""功能: 重新安装库."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from typer import Argument
+from typing_extensions import Annotated
+
+from pycmd2.common.cli import get_client
+from pycmd2.pip.conf import conf
+from pycmd2.pip.pip_uninstall import pip_uninstall
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+cli = get_client()
+
+
+def pip_reinstall(libname: str) -> None:
+    pip_uninstall(libname)
+    cli.run_cmd(
+        [
+            "pip",
+            "install",
+            libname,
+            *conf.TRUSTED_PIP_URL,
+        ],
+    )
+
+
+@cli.app.command()
+def main(
+    libnames: Annotated[list[Path], Argument(help="待下载库清单")],
+) -> None:
+    cli.run(pip_reinstall, libnames)
