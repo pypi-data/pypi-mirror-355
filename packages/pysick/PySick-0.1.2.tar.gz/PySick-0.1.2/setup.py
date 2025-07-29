74ed5437a4e92a988fee53edcfdb6daae968e552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:47 +0000

    Add package pysick/0.1.2/PySick-0.1.2.tar.gz

---
+++ packages/pysick/PySick-0.1.2.tar.gz/PySick-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PySick",
+    version="0.1.2",
+    py_modules= ['pysick'],
+    install_requires=[],
+    author="CouSik",
+    author_email="your@email.com",
+    description="A lightweight 2D game framework using Tkinter",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/pysick",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
