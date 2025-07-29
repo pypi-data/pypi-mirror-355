deec992b4ba4c597353a73410a7cc43552e33735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:51 +0000

    Add package triggersms/0.1.1/triggersms-0.1.1.tar.gz

---
+++ packages/triggersms/triggersms-0.1.1.tar.gz/triggersms-0.1.1/README.md
@@ -0,0 +1,21 @@
+# TriggerSMS
+
+A Python package for sending data to the TriggerSMS API.
+
+## Installation
+
+```bash
+pip install triggersms
+```
+
+## Usage
+
+```python
+import triggersms
+
+# Your API usage code here
+```
+
+## License
+
+MIT License 
---
+++ packages/triggersms/triggersms-0.1.1.tar.gz/triggersms-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+# Read the contents of README.md
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='triggersms',
+    version='0.1.1',
+    description='TriggerSMS is a Python package for sending data to the TriggerSMS API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['triggersms'],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=2.6.6',
+)
