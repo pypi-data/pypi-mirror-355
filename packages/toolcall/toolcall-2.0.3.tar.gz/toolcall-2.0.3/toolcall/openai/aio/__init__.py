865a6c7f7c18aa0e008b74ebde4ca4a4460d4eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:38 +0000

    Add package toolcall/2.0.3/toolcall-2.0.3.tar.gz

---
+++ packages/toolcall/toolcall-2.0.3.tar.gz/toolcall-2.0.3/toolcall/openai/aio/__init__.py
@@ -0,0 +1,38 @@
+from ..common import (
+    ErrorForLLMToSee,
+    ToolErrorMessageForLLMToSee,
+    ToolHandlerResult,
+    ToolCallFailReason,
+    ToolCallSuccess,
+    ToolCallFailure,
+    ToolCallResult,
+    ToolMessageContent,
+    StandardToolCall,
+    standardize_tool_call,
+    tool_def_for_chat_completions_api,
+    tool_def_for_responses_api,
+)
+from .tool import (
+    LLMFunctionTool,
+)
+from .tool_group import (
+    LLMFunctionToolGroup,
+)
+
+
+__all__ = [
+    "ErrorForLLMToSee",
+    "ToolErrorMessageForLLMToSee",
+    "ToolHandlerResult",
+    "ToolCallFailReason",
+    "ToolCallSuccess",
+    "ToolCallFailure",
+    "ToolCallResult",
+    "ToolMessageContent",
+    "StandardToolCall",
+    "standardize_tool_call",
+    "tool_def_for_chat_completions_api",
+    "tool_def_for_responses_api",
+    "LLMFunctionTool",
+    "LLMFunctionToolGroup",
+]
