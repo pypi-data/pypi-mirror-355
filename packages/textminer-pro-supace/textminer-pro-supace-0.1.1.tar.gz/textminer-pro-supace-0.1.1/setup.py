b0b122f883946f767c2308a54c11d55d32f4c80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:41 +0000

    Add package textminer-pro-supace/0.1.1/textminer-pro-supace-0.1.1.tar.gz

---
+++ packages/textminer-pro-supace/textminer-pro-supace-0.1.1.tar.gz/textminer-pro-supace-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textminer-pro-supace",  # PyPI 등록 시 중복 확인 필요
+    version="0.1.1",
+    author="최수한",
+    author_email="wagkster@naver.com",
+    description="불용어 제거, 키워드 추출, 요약, 언어 감지를 제공하는 텍스트 분석 도구",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/project/Hansupace/textminer-pro",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "nltk>=3.8.1",
+        "scikit-learn>=1.3.0",
+        "langdetect>=1.0.9",
+        "sumy>=0.11.0",
+    ]
+)
