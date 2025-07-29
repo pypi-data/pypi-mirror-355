f7c650c64988a18f00a9e864cc6a177103126712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:56 +0000

    Add package reykit/1.0.1/reykit-1.0.1-py3-none-any.whl

---
+++ packages/reykit/reykit-1.0.1-py3-none-any.whl/reyworm/__init__.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19 18:59:26
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's worm method set.
+
+Modules
+-------
+rall : All methods.
+rbrowser : Browser methods.
+rcalendar : Calendar methods.
+rnews : News methods.
+rsecurity : Security methods.
+rtranslate : Translate methods.
+"""
+
+
+from typing import Final
+
+
+__version__: Final[str] = '1.0.0'
