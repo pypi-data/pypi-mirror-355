84c14bc034b8d0ce33436688837995fb41f3cbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:56 +0000

    Add package anticor-features/0.2.3/anticor_features-0.2.3-py3-none-any.whl

---
+++ packages/anticor-features/anticor_features-0.2.3-py3-none-any.whl/anticor_features/__init__.py
@@ -0,0 +1,12 @@
+"""
+Top-level imports for anticor_features package.
+
+This allows:
+    from anticor_features import get_anti_cor_genes
+"""
+
+from .anticor_features import get_anti_cor_genes
+
+__all__ = [
+    "get_anti_cor_genes",
+]
