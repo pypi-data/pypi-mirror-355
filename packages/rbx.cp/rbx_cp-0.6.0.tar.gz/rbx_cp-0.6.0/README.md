921690fabd7a1ecb2e17c6420e8869fb1d333b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:52 +0000

    Add package rbx.cp/0.6.0/rbx_cp-0.6.0.tar.gz

---
+++ packages/rbx.cp/rbx_cp-0.6.0.tar.gz/rbx_cp-0.6.0/README.md
@@ -0,0 +1,52 @@
+<p align="center">
+    <em>The go-to CLI tool for competitive programmers and setters.</em>
+</p>
+<p align="center">
+	<!-- loscal repository, no metadata badges. -->
+<p>
+<p align="center">
+	<img src="https://img.shields.io/badge/Poetry-60A5FA.svg?style=default&logo=Poetry&logoColor=white" alt="Poetry">
+	<img src="https://img.shields.io/badge/Python-3776AB.svg?style=default&logo=Python&logoColor=white" alt="Python">
+	<img src="https://img.shields.io/badge/FastAPI-009688.svg?style=default&logo=FastAPI&logoColor=white" alt="FastAPI">
+	<img src="https://img.shields.io/badge/JSON-000000.svg?style=default&logo=JSON&logoColor=white" alt="JSON">
+</p>
+
+<br><!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary><br>
+
+- [Overview](#overview)
+- [Documentation](#documentation)
+- [License](#license)
+</details>
+<hr>
+
+##  Overview
+
+<!--[![Usage video]](https://github.com/rsalesc/rbx/assets/4999965/111de01e-6cbd-495e-b8c2-4293921e49b3)-->
+
+[![GitHub license](https://img.shields.io/github/license/rsalesc/rbx.svg)](https://github.com/rsalesc/rbx/blob/master/LICENSE)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/rbx.svg)](https://pypi.python.org/pypi/rbx/)
+[![PyPI version shields.io](https://img.shields.io/pypi/v/rbx.svg)](https://pypi.python.org/pypi/rbx/)
+
+rbx is a CLI tool and library that aims to help contestants and setters from the competitive programming community to have an easier time.
+
+Although trying to solve the issues of these two audiences seems to be too much for a single tool to handle, we exploit the fact that the journeys of contestants and setters have a lot in common.
+
+---
+
+##  Documentation
+
+You can read the docs [here](https://rsalesc.github.io/rbx/).
+
+---
+
+##  License
+
+This project is protected under the [Apache License 2.0](http://www.apache.org/licenses/) License. For more details, refer to the [LICENSE](LICENSE) file.
+
+---
+
+[**Return**](#-overview)
+
+---
---
+++ packages/rbx.cp/rbx_cp-0.6.0.tar.gz/rbx_cp-0.6.0/rbx/box/contest/main.py
@@ -0,0 +1,258 @@
+import pathlib
+import shutil
+import subprocess
+from typing import Annotated, Optional
+
+import rich.prompt
+import typer
+
+from rbx import annotations, console, utils
+from rbx.box import cd, creation, presets
+from rbx.box.contest import contest_package, contest_utils, statements
+from rbx.box.contest.contest_package import (
+    find_contest,
+    find_contest_package_or_die,
+    find_contest_yaml,
+    within_contest,
+)
+from rbx.box.contest.schema import ContestProblem
+from rbx.box.packaging import contest_main as packaging
+from rbx.box.presets.fetch import get_preset_fetch_info
+from rbx.box.schema import Package
+from rbx.config import open_editor
+
+app = typer.Typer(no_args_is_help=True, cls=annotations.AliasGroup)
+app.add_typer(
+    statements.app,
+    name='statements, st',
+    cls=annotations.AliasGroup,
+    help='Manage contest-level statements.',
+)
+app.add_typer(
+    packaging.app,
+    name='package, pkg',
+    cls=annotations.AliasGroup,
+    help='Build contest-level packages.',
+)
+
+
+@app.command('create, c', help='Create a new contest package.')
+def create(
+    path: str,
+    preset: Annotated[
+        str,
+        typer.Option(
+            '--preset',
+            '-p',
+            help='Which preset to use to create this package. Can be a named of an already installed preset, or an URI, in which case the preset will be downloaded.',
+        ),
+    ] = 'default',
+    local: bool = typer.Option(
+        False,
+        '--local',
+        '-l',
+        help='Whether to inline the installed preset within the contest folder.',
+    ),
+):
+    console.console.print(f'Creating new contest at [item]{path}[/item]...')
+
+    fetch_info = get_preset_fetch_info(preset)
+    if fetch_info is None:
+        console.console.print(
+            f'[error]Invalid preset name/URI [item]{preset}[/item][/error]'
+        )
+        raise typer.Exit(1)
+
+    if fetch_info.is_remote():
+        preset = presets.install_from_remote(fetch_info)
+    elif fetch_info.is_local_dir():
+        preset = presets.install_from_local_dir(fetch_info)
+
+    preset_cfg = presets.get_installed_preset(preset)
+    preset_path = (
+        presets.get_preset_installation_path(preset)
+        if preset_cfg.contest is not None
+        else presets.get_preset_installation_path('default')
+    )
+
+    contest_path = (
+        presets.get_preset_installation_path(preset) / preset_cfg.contest
+        if preset_cfg.contest is not None
+        else presets.get_preset_installation_path('default') / 'contest'
+    )
+
+    if not contest_path.is_dir():
+        console.console.print(
+            f'[error]Contest template [item]{contest_path}[/item] does not exist.[/error]'
+        )
+        raise typer.Exit(1)
+
+    dest_path = pathlib.Path(path)
+
+    if dest_path.exists():
+        if not rich.prompt.Confirm.ask(
+            f'Directory [item]{dest_path}[/item] already exists. Create contest in it? This might be destructive.',
+            show_default=False,
+            console=console.console,
+        ):
+            console.console.print(
+                f'[error]Directory [item]{dest_path}[/item] already exists.[/error]'
+            )
+            raise typer.Exit(1)
+
+    dest_path.mkdir(parents=True, exist_ok=True)
+    shutil.copytree(str(contest_path), str(dest_path), dirs_exist_ok=True)
+    shutil.rmtree(str(dest_path / 'build'), ignore_errors=True)
+    shutil.rmtree(str(dest_path / '.box'), ignore_errors=True)
+    shutil.rmtree(str(dest_path / '.local.rbx'), ignore_errors=True)
+    # TODO: consider clearing build and .box recursively for nested problem directories
+    for lock in dest_path.rglob('.preset-lock.yml'):
+        lock.unlink(missing_ok=True)
+
+    if local:
+        presets.copy_local_preset(
+            preset_path, dest_path, remote_uri=fetch_info.uri or preset_cfg.uri
+        )
+
+    with cd.new_package_cd(dest_path):
+        contest_utils.clear_all_caches()
+        presets.generate_lock(preset if not local else presets.LOCAL)
+
+
+@app.command('edit, e', help='Open contest.rbx.yml in your default editor.')
+@within_contest
+def edit():
+    console.console.print('Opening contest definition in editor...')
+    # Call this function just to raise exception in case we're no in
+    # a problem package.
+    find_contest()
+    open_editor(find_contest_yaml() or pathlib.Path())
+
+
+@app.command('add, a', help='Add new problem to contest.')
+@within_contest
+def add(path: str, short_name: str, preset: Optional[str] = None):
+    problem_path = pathlib.Path(path)
+    name = problem_path.stem
+    utils.validate_field(ContestProblem, 'short_name', short_name)
+    utils.validate_field(Package, 'name', name)
+
+    if short_name in [p.short_name for p in find_contest_package_or_die().problems]:
+        console.console.print(
+            f'[error]Problem [item]{short_name}[/item] already exists in contest.[/error]',
+        )
+        raise typer.Exit(1)
+
+    preset_lock = presets.get_preset_lock()
+    if preset is None and preset_lock is not None:
+        preset = preset_lock.preset_name
+    creation.create(name, preset=preset, path=pathlib.Path(path))
+
+    contest = find_contest_package_or_die()
+
+    ru, contest = contest_package.get_ruyaml()
+
+    item = {
+        'short_name': short_name,
+        'path': path,
+    }
+    if 'problems' not in contest:
+        contest['problems'] = [item]
+    else:
+        contest['problems'].append(item)
+
+    dest = find_contest_yaml()
+    assert dest is not None
+    utils.save_ruyaml(dest, ru, contest)
+
+    console.console.print(
+        f'Problem [item]{name} ({short_name})[/item] added to contest at [item]{path}[/item].'
+    )
+
+
+@app.command('remove, r', help='Remove problem from contest.')
+@within_contest
+def remove(path_or_short_name: str):
+    contest = find_contest_package_or_die()
+
+    removed_problem_idx = None
+    removed_problem = None
+    for i, problem in enumerate(contest.problems):
+        if (
+            problem.path == pathlib.Path(path_or_short_name)
+            or problem.short_name == path_or_short_name
+        ):
+            removed_problem_idx = i
+            removed_problem = problem
+            break
+
+    if removed_problem_idx is None or removed_problem is None:
+        console.console.print(
+            f'[error]Problem [item]{path_or_short_name}[/item] not found in contest.[/error]'
+        )
+        raise typer.Exit(1)
+
+    ru, contest = contest_package.get_ruyaml()
+
+    del contest['problems'][removed_problem_idx]
+    dest = find_contest_yaml()
+    assert dest is not None
+    utils.save_ruyaml(dest, ru, contest)
+
+    shutil.rmtree(str(removed_problem.path), ignore_errors=True)
+    console.console.print(
+        f'Problem [item]{removed_problem.short_name}[/item] removed from contest at [item]{removed_problem.path}[/item].'
+    )
+
+
+@app.command(
+    'each',
+    help='Run a command for each problem in the contest.',
+    context_settings={'allow_extra_args': True, 'ignore_unknown_options': True},
+)
+@within_contest
+def each(ctx: typer.Context) -> None:
+    command = ' '.join(['rbx'] + ctx.args)
+    contest = find_contest_package_or_die()
+    ok = True
+    for problem in contest.problems:
+        console.console.print(
+            f'[status]Running [item]{command}[/item] for [item]{problem.short_name}[/item]...[/status]'
+        )
+
+        retcode = subprocess.call(
+            command,
+            cwd=problem.get_path(),
+            shell=True,
+        )
+        ok = ok and retcode == 0
+        console.console.print()
+
+    if not ok:
+        console.console.print(
+            '[error]One of the commands above failed. Check the output![/error]'
+        )
+
+
+@app.command(
+    'on',
+    help='Run a command in the problem (or in a set of problems) of a context.',
+    context_settings={'allow_extra_args': True, 'ignore_unknown_options': True},
+)
+@within_contest
+def on(ctx: typer.Context, problems: str) -> None:
+    command = ' '.join(['rbx'] + ctx.args)
+    problems_of_interest = contest_utils.get_problems_of_interest(problems)
+
+    if not problems_of_interest:
+        console.console.print(
+            f'[error]No problems found in contest matching [item]{problems}[/item].[/error]'
+        )
+        raise typer.Exit(1)
+
+    for p in problems_of_interest:
+        console.console.print(
+            f'[status]Running [item]{command}[/item] for [item]{p.short_name}[/item]...[/status]'
+        )
+        subprocess.call(command, cwd=p.get_path(), shell=True)
+        console.console.print()
