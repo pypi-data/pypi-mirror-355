a7d8658c0c4e4e63999c332bd3997569fe8f0809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:08 +0000

    Add package base-converter-pedromagno/0.1.1/base_converter_pedromagno-0.1.1.tar.gz

---
+++ packages/base-converter-pedromagno/base_converter_pedromagno-0.1.1.tar.gz/base_converter_pedromagno-0.1.1/base_converter/__init__.py
@@ -0,0 +1,4 @@
+from .base_converter import (
+    convert_from_base_n_to_decimal,
+    convert_from_decimal_to_base_n
+)
---
+++ packages/base-converter-pedromagno/base_converter_pedromagno-0.1.1.tar.gz/base_converter_pedromagno-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from tkinter.font import names
+
+from setuptools import setup, find_packages
+
+setup(
+    name="base_converter_pedromagno",
+    version="0.1.1",
+    description="Base conversion library (base 2 to 16) with decimal and fractional support.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Pedro Magno",
+    author_email="pedromagnopro@gmail.com",
+    url="https://github.com/PedroMagno11/base_converter",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
