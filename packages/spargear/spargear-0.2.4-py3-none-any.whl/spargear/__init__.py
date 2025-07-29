68b8fc9c37bc0064840bc571485935643120514f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:50 +0000

    Add package spargear/0.2.4/spargear-0.2.4-py3-none-any.whl

---
+++ packages/spargear/spargear-0.2.4-py3-none-any.whl/spargear/__init__.py
@@ -0,0 +1,18 @@
+from ._typing import SUPPRESS, Action, FileProtocol, TypedFileType
+from .argument_spec import ArgumentSpec
+from .argument_spec_type import ArgumentSpecType
+from .arguments import RunnableArguments
+from .base_arguments import BaseArguments
+from .subcommand_spec import SubcommandSpec
+
+__all__ = [
+    "SUPPRESS",
+    "Action",
+    "FileProtocol",
+    "TypedFileType",
+    "ArgumentSpec",
+    "ArgumentSpecType",
+    "RunnableArguments",
+    "BaseArguments",
+    "SubcommandSpec",
+]
