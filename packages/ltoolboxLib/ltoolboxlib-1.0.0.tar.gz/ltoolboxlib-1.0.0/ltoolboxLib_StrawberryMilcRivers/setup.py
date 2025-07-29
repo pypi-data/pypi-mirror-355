8a1aff4642452e0b0e3a0c204eac02124eb92903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:45 +0000

    Add package ltoolboxLib/1.0.0/ltoolboxlib-1.0.0.tar.gz

---
+++ packages/ltoolboxLib/ltoolboxlib-1.0.0.tar.gz/ltoolboxlib-1.0.0/ltoolboxLib_StrawberryMilcRivers/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ltools',
+    version='0.1',
+    description='Lumi’s legendary little library ☆',
+    author='Lumi',
+    packages=find_packages(),  # ← ONLY ONE `packages=` line
+)
