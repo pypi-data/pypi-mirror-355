3525384c4abb71021b2b83e3e22d9b28544bd128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:43 +0000

    Add package yap.helper/0.0.2/yap.helper-0.0.2.tar.gz

---
+++ packages/yap.helper/yap.helper-0.0.2.tar.gz/yap.helper-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='yap.helper',
+    version='0.0.2',
+    description='A helper python library to assist a system engineer',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Graham "yap.athy" Lasseigne',
+    author_email='grahamlasseigne@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='yap',
+    packages=find_packages(),
+    install_requires=['']
+)
