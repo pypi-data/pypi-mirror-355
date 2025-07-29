c6a97cdc20220793c98e3a4c9f63416b2e249ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:23 +0000

    Add package masto-cli/0.1.7/masto_cli-0.1.7.tar.gz

---
+++ packages/masto-cli/masto_cli-0.1.7.tar.gz/masto_cli-0.1.7/masto_cli/profile.py
@@ -0,0 +1,68 @@
+from masto_cli import http_client, api
+import json, re
+
+login = json.load(open('login.json', 'r'))
+
+class NotFound(Exception):
+    ...
+
+def get_id_info(id: str) -> dict:
+    """ get user id info, example
+        .get_id_info('1')
+        -> {"id":"1","username":"Gargron","acct":"Gargron","display_name":"Eugen Rochko",..... }
+    """
+    url_id = f'{api}/v1/accounts/{id}'
+    response = http_client.rq_get(url= url_id, headers={
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+    })
+    if 'error' in response.text:
+        raise NotFound(
+            f"User {id} not found!"
+        )
+    return (json.loads(response.text))
+
+def get_username_info(username: str) -> dict:
+    """ get username information """
+    url = f'{api}/v1/accounts/lookup?acct={username}'
+    response = http_client.rq_get(
+        url= url, headers= login
+    )
+    if 'error' in response.text:
+        raise NotFound(
+            f"{username} not found!"
+        )
+    return (get_id_info(json.loads(response.text)['id']))
+
+def get_following(id: str, limit: str) -> dict:
+    """ get following by limit
+    .get_following('1', limit= 10)
+    """
+    url = f'{api}/v1/accounts/{id}/following?limit={limit}'
+    response = http_client.rq_get(
+        url= url, headers= login
+    )
+    return json.loads(response.text)
+
+def get_followers(id: str, limit: str) -> dict:
+    """ get followers by limit
+    .get_followers('1', limit= 10)
+    """
+    url = f'{api}/v1/accounts/{id}/followers?limit={limit}'
+    response = http_client.rq_get(
+        url= url, headers= login
+    )
+    return json.loads(response.text)
+
+def follow(id: str) -> list:
+    url = f'{api}/v1/accounts/{id}/follow'
+    response = http_client.rq_post(
+        url= url, headers= login, data={'reblogs': True}
+    )
+    return json.loads(response.text)
+
+def unfollow(id: str) -> list:
+    url = f'{api}/v1/accounts/{id}/follow'
+    response = http_client.rq_post(
+        url= url, headers= login
+    )
+    return json.loads(response.text)
---
+++ packages/masto-cli/masto_cli-0.1.7.tar.gz/masto_cli-0.1.7/masto_cli/timeline.py
@@ -0,0 +1,18 @@
+from masto_cli import http_client, login, api
+import json
+
+def get_timeline() -> dict:
+    """ get home time line """
+    url = f"{api}/v1/timelines/home"
+    response = http_client.rq_get(
+        url= url, headers= login
+    )
+    return (json.loads(response.text))
+
+def get_post_info(post_id: str) -> dict:
+    """ get post information """
+    url = f'{api}/v1/statuses/{post_id}/context'
+    response = http_client.rq_get(
+        url= url, headers= login
+    )
+    return (json.loads(response.text))
