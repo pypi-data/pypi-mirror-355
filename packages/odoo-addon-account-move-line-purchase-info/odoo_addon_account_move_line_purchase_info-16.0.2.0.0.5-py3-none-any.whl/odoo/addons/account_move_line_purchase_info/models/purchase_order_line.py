fca15b342d5ed2ef4a1e4a2d64eab3ec58479aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:07 +0000

    Add package odoo-addon-account-move-line-purchase-info/16.0.2.0.0.5/odoo_addon_account_move_line_purchase_info-16.0.2.0.0.5-py3-none-any.whl

---
+++ packages/odoo-addon-account-move-line-purchase-info/odoo_addon_account_move_line_purchase_info-16.0.2.0.0.5-py3-none-any.whl/odoo/addons/account_move_line_purchase_info/models/purchase_order_line.py
@@ -0,0 +1,24 @@
+# Copyright 2019-2020 ForgeFlow S.L.
+#   (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    stock_invoice_lines = fields.One2many(
+        "account.move.line", "oca_purchase_line_id", readonly=True, copy=False
+    )
+
+    def name_get(self):
+        result = []
+        orig_name = dict(super(PurchaseOrderLine, self).name_get())
+        for line in self:
+            name = orig_name[line.id]
+            if self.env.context.get("po_line_info", False):
+                name = "[{}] {} ({})".format(
+                    line.order_id.name, name, line.order_id.state
+                )
+            result.append((line.id, name))
+        return result
