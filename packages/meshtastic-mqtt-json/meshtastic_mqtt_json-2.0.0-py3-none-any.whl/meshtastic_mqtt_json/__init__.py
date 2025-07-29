18ace7ef3ef453b7997014cf6bb90153b5cfecbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:40 +0000

    Add package meshtastic-mqtt-json/2.0.0/meshtastic_mqtt_json-2.0.0-py3-none-any.whl

---
+++ packages/meshtastic-mqtt-json/meshtastic_mqtt_json-2.0.0-py3-none-any.whl/meshtastic_mqtt_json/__init__.py
@@ -0,0 +1,9 @@
+''' 
+Meshtastic MQTT Interface - A lightweight Python library for parsing Meshtastic MQTT messages
+'''
+
+from .client import MeshtasticMQTT
+
+__version__ = '2.0.0'
+__author__  = 'acidvegas'
+__license__ = 'ISC'
