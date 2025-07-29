692f88f05b1af973edfc60c29880baffa3e18af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:57 +0000

    Add package wagtail-enap-designsystem/1.2.0.11/wagtail_enap_designsystem-1.2.0.11.tar.gz

---
+++ packages/wagtail-enap-designsystem/wagtail_enap_designsystem-1.2.0.11.tar.gz/wagtail_enap_designsystem-1.2.0.11/enap_designsystem/templatetags/math_tags.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def mul(value, arg):
+	try:
+		return int(value) * int(arg)
+	except:
+		return ''
+
+@register.filter
+def classname(obj):
+	return obj.__class__.__name__
+
