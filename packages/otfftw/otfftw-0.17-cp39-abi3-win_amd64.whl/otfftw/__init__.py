1a3da320098ded00ee545d1f76acc758b1dea3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package otfftw/0.17/otfftw-0.17-cp39-abi3-win_amd64.whl

---
+++ packages/otfftw/otfftw-0.17-cp39-abi3-win_amd64.whl/otfftw/__init__.py
@@ -0,0 +1,18 @@
+"""
+    otfftw --- An OpenTURNS module
+    ===================================================
+
+    Contents
+    --------
+      'otfftw' is python module for OpenTURNS
+
+"""
+
+# flake8: noqa
+
+# ensures swig type tables order & dll load
+import openturns as _ot
+
+from .otfftw import *
+
+__version__ = '0.17'
