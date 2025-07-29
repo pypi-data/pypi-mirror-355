8a5a7c0bd03baa1074a6fffba998bfca986cba0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:34 +0000

    Add package apek/0.0.2a2/apek-0.0.2a2.tar.gz

---
+++ packages/apek/apek-0.0.2a2.tar.gz/apek-0.0.2a2/apek/meta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+This module defines the package's metadata.
+
+Constants:
+    cst_version (str): The package version number.
+    cst_lastVersion (str): The package last version number.
+"""
+
+
+
+from ._text import text
+
+cst_version = text["default"]["meta.version"]
+cst_lastVersion = text["default"]["meta.lastVersion"]
