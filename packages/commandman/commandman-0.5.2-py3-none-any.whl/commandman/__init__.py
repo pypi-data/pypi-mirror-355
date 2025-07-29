dd41c71d016f85baa6a8e2cd412c5592d49d8918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:47 +0000

    Add package commandman/0.5.2/commandman-0.5.2-py3-none-any.whl

---
+++ packages/commandman/commandman-0.5.2-py3-none-any.whl/commandman/__init__.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2025, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+"""Cross-platform console command manager."""
+__version__ = '0.5.2'
