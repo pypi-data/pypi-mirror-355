286e49e248687d700be345bac5f239ccd14764d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:52 +0000

    Add package aait/1.0.4.2/aait-1.0.4.2.tar.gz

---
+++ packages/aait/aait-1.0.4.2.tar.gz/aait-1.0.4.2/orangecontrib/AAIT/llm/test.py
@@ -0,0 +1,18 @@
+print("\033[31m") # Couleur rouge pour les cheveux rouges
+print(" /\\_/\\ ")
+print(" ( o.o ) ")
+print(" > ^ < ")
+print("\033[0m") # Réinitialisation de la couleur
+
+print(" || ") # Lunettes
+print(" || ")
+print(" || ")
+
+print(" /\\_/\\ ")
+print(" ( o.o ) ")
+print(" > ^ < ")
+
+print("\nCamille - 1m50 | Énergique & Curieuse!")
+print(" | ") # Pieds
+print(" | ")
+print(" |______") # Sol
