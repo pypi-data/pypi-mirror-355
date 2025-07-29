67124f1d4e27691182112c3be28fffe414c673f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:09:02 +0000

    Add package malva-api/0.0.1/malva_api-0.0.1-py3-none-any.whl

---
+++ packages/malva-api/malva_api-0.0.1-py3-none-any.whl/malva_api/__init__.py
@@ -0,0 +1,18 @@
+"""
+Malva API for the Malva Index
+"""
+
+__version__ = "0.0.1"
+__author__ = "Daniel León-Periñán"
+__email__ = "daniel.leonperinan@mdc-berlin.de"
+
+def run_malva_api():
+    """
+    A function to say we will be there soon
+    
+    Returns:
+        str: A message
+    """
+    return "Malva API will be here soon!"
+
+__all__ = ["run_malva_api"]
