7b036c79bb72ee470f413197e316e5d038f66760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:25 +0000

    Add package embedl-hub/2025.6.0.dev1/embedl_hub-2025.6.0.dev1.tar.gz

---
+++ packages/embedl-hub/embedl_hub-2025.6.0.dev1.tar.gz/embedl_hub-2025.6.0.dev1/tests/test_package.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2025 Embedl AB
+"""
+This module contains unit tests for the embedl-hub package.
+"""
+
+import embedl_hub
+
+
+def test_version():
+    """Test that the package version is correct."""
+    assert embedl_hub.__version__ == "2025.6.0.dev0"
