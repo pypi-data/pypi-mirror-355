e6bad3a1ca8f15c73ee6799885fc4eeeb6d67986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:55 +0000

    Add package albert/0.13.2/albert-0.13.2-py3-none-any.whl

---
+++ packages/albert/albert-0.13.2-py3-none-any.whl/albert/resources/btdataset.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from pydantic import Field
+
+from albert.resources.base import BaseResource, EntityLink
+from albert.resources.identifiers import BTDatasetId
+from albert.utils.types import BaseAlbertModel
+
+
+class BTDatasetReferences(BaseAlbertModel):
+    project_ids: list[str]
+    data_column_ids: list[str]
+    filters: list[dict[str, Any]] = Field(..., default_factory=list)
+
+
+class BTDataset(BaseResource):
+    name: str
+    id: BTDatasetId | None = Field(default=None, alias="albertId")
+    key: str | None = Field(default=None)
+    file_name: str | None = Field(default=None, alias="fileName")
+    report: EntityLink | None = Field(default=None, alias="Report")
+    references: BTDatasetReferences | None = Field(default=None, alias="References")
