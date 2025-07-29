d668a47d3d6e4ef5ec0a41b46e18e83d55daeaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:52 +0000

    Add package kago-utils/0.1.3/kago_utils-0.1.3-cp312-cp312-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/kago-utils/kago_utils-0.1.3-cp312-cp312-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl/kago_utils/resolvers/results/__init__.py
@@ -0,0 +1,27 @@
+from .ankan_result import AnkanResult
+from .chii_result import ChiiResult
+from .dahai_result import DahaiResult
+from .daiminkan_result import DaiminkanResult
+from .kakan_result import KakanResult
+from .kyuushu_kyuuhai_result import KyuushuKyuuhaiResult
+from .pending import Pending
+from .pon_result import PonResult
+from .riichi_result import RiichiResult
+from .ronho_result import RonhoResult
+from .skip_result import SkipResult
+from .tsumoho_result import TsumohoResult
+
+__all__ = (
+    "AnkanResult",
+    "ChiiResult",
+    "DahaiResult",
+    "DaiminkanResult",
+    "KakanResult",
+    "KyuushuKyuuhaiResult",
+    "PonResult",
+    "RiichiResult",
+    "RonhoResult",
+    "SkipResult",
+    "TsumohoResult",
+    "Pending",
+)
