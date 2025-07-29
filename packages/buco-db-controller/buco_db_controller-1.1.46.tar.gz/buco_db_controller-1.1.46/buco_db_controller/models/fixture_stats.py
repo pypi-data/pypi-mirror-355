b4c1195cbfaadee9b4ae1ca5593bd346441f9d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:08:19 +0000

    Add package buco-db-controller/1.1.46/buco_db_controller-1.1.46.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-1.1.46.tar.gz/buco_db_controller-1.1.46/buco_db_controller/models/fixture_stats.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class FixtureStats:
+    def __init__(self, fixture_id: int, home: Optional[dict], away: Optional[dict]):
+        self.fixture_id: int = fixture_id
+        self.home: dict = home
+        self.away: dict = away
+
+    @classmethod
+    def from_dict(cls, response: dict) -> 'FixtureStats':
+        fixture_id = response['parameters']['fixture']
+
+        if not response['data']:
+            return cls(fixture_id=fixture_id, home=None, away=None)
+
+        home = response['data']['home']
+        away = response['data']['away']
+
+        return cls(fixture_id=fixture_id, home=home, away=away)
