80e70570af4236a07eb701ca45e837f0ff7dcf30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:44 +0000

    Add package summa-testing-framework-summasolutions/0.1.88/summa-testing-framework-summasolutions-0.1.88.tar.gz

---
+++ packages/summa-testing-framework-summasolutions/summa-testing-framework-summasolutions-0.1.88.tar.gz/summa-testing-framework-summasolutions-0.1.88/stf/command/config.py
@@ -0,0 +1,39 @@
+import click
+from click_help_colors import HelpColorsCommand
+
+import stf.common.config as common_config
+import stf.common.filesystem as common_filesystem
+import stf.common.docker as common_docker
+import os
+import sys
+import shutil
+
+sys.path.insert(1, os.getcwd())
+
+
+@click.command(
+    cls=HelpColorsCommand,
+    help_headers_color='yellow',
+    help_options_color='green',
+)
+def init():
+    click.secho("Initializing project config...", fg='blue')
+
+    base_project_config_file = "%s/config.base.yml" % (common_config.get_assets_dir())
+    target = "%s/config.yml" % os.getcwd()
+
+    shutil.copyfile(base_project_config_file, target)
+
+    click.secho("Initializing project xpath file...", fg='blue')
+
+    base_project_xpath_file = "%s/xpath.yml" % (common_config.get_assets_dir())
+    target_xpath = "%s/xpath.yml" % os.getcwd()
+
+    shutil.copyfile(base_project_xpath_file, target_xpath)
+
+    click.secho("Generating gitignore file...", fg='blue')
+
+    base_gitignore_file = "%s/gitignore.base" % (common_config.get_assets_dir())
+    target_gitignore = "%s/.gitignore" % os.getcwd()
+
+    shutil.copyfile(base_gitignore_file, target_gitignore)
