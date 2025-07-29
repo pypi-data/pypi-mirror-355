e59e8e4db0e57d5e37929a63d6bc253b26b69d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:23 +0000

    Add package ipyscan/1.0.30/ipyscan-1.0.30-py3-none-any.whl

---
+++ packages/ipyscan/ipyscan-1.0.30-py3-none-any.whl/utils/banner.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import time
+
+from colorama import (
+    init,
+    Fore,
+)  # Usando o colorama para inserir cor ao texto do terminal
+
+init()
+
+
+def clean_screen():
+    print("\033c", end="") # Limpa a tela do terminal
+    time.sleep(1)
+    
+
+def banner():
+    time.sleep(2)  # Aguardar dois segundos para iniciar a aplicação
+    clean_screen()  # Limpa a tela
+    data = datetime.now()
+
+    print(
+        Fore.GREEN
+        + """
+Welcome to iPyScan v. 1.0.29. Date: """
+        + data.strftime("%d/%m/%Y")
+        + """   
+Developed by: gh05tb0y (Werdeles Soares). E-Mail: gh05tb0y@disroot.org                          
+"""
+        + Fore.RESET
+    )
