bb97ff7908693fd34ed530115acd7d2da60b4930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:00 +0000

    Add package fishaudio/0.0.1/fishaudio-0.0.1.tar.gz

---
+++ packages/fishaudio/fishaudio-0.0.1.tar.gz/fishaudio-0.0.1/src/fishaudio/webui/variables.py
@@ -0,0 +1,7 @@
+from fish_speech.i18n import i18n
+
+HEADER_MD = f"""# text-to-speech synthesis"""
+
+TEXTBOX_PLACEHOLDER = i18n("Put your text here.")
+# example (see below):
+# Hey Connector, why your appearance looks so stupid? Oh, really? maybe I ate too much smart beans. Wow. Amazing (laughing). Let's go to get some more smart beans and you will become stupid as well.
