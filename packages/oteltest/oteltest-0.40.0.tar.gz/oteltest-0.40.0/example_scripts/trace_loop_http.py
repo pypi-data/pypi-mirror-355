1304a3a9dd1ebac76b6ba94f2b9e34545e2e09d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:26 +0000

    Add package oteltest/0.40.0/oteltest-0.40.0.tar.gz

---
+++ packages/oteltest/oteltest-0.40.0.tar.gz/oteltest-0.40.0/example_scripts/trace_loop_http.py
@@ -0,0 +1,33 @@
+from opentelemetry import trace
+
+SERVICE_NAME = "my-otel-test"
+
+if __name__ == "__main__":
+    tracer = trace.get_tracer("my-tracer")
+    with tracer.start_as_current_span("aaa"):
+        with tracer.start_as_current_span("bbb"):
+            with tracer.start_as_current_span("ccc"):
+                print('hola mundo')
+
+
+class MyOtelTest:
+    def requirements(self):
+        return "opentelemetry-distro", "opentelemetry-exporter-otlp-proto-http"
+
+    def environment_variables(self):
+        return {
+            "OTEL_SERVICE_NAME": SERVICE_NAME,
+            "OTEL_EXPORTER_OTLP_PROTOCOL": "http/protobuf",
+        }
+
+    def wrapper_command(self):
+        return "opentelemetry-instrument"
+
+    def on_start(self):
+        return None
+
+    def on_stop(self, telemetry, stdout: str, stderr: str, returncode: int) -> None:
+        pass
+
+    def is_http(self):
+        return True
