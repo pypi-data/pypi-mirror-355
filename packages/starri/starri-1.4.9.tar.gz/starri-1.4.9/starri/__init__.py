4e8b031b9a2a446c64d3227bdd681d9a6e6a0522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:21 +0000

    Add package starri/1.4.9/starri-1.4.9.tar.gz

---
+++ packages/starri/starri-1.4.9.tar.gz/starri-1.4.9/starri/__init__.py
@@ -0,0 +1,19 @@
+from .menu import (
+    starri,
+    spacer,
+    text,
+    option,
+    sidebyside,
+    checkbox,
+    textinput,
+)
+
+__all__ = [
+    "starri",
+    "spacer",
+    "text",
+    "option",
+    "sidebyside",
+    "checkbox",
+    "textinput"
+]
