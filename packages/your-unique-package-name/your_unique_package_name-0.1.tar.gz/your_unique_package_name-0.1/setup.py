96ea46d5bf6c3942301632244cab8b77adc56c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:07 +0000

    Add package your-unique-package-name/0.1/your_unique_package_name-0.1.tar.gz

---
+++ packages/your-unique-package-name/your_unique_package_name-0.1.tar.gz/your_unique_package_name-0.1/setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="your-unique-package-name",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="A beginner test package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python :: 3"],
+)
