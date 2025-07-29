8bb7e8297fd3824ec4ff7936da2a4d50cad1edab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:22 +0000

    Add package clusterscope/0.0.5/clusterscope-0.0.5-py3-none-any.whl

---
+++ packages/clusterscope/clusterscope-0.0.5-py3-none-any.whl/clusterscope/lib.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+from clusterscope.cluster_info import ClusterInfo
+
+cluster_info = ClusterInfo()
+
+
+def cluster() -> str:
+    return cluster_info.get_cluster_name()
+
+
+def slurm_version() -> str:
+    return cluster_info.get_slurm_version()
