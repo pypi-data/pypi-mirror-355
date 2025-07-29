94b048f5b1a34d820a5d3066d66d1dad9a39f174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:21 +0000

    Add package vembrane/2.1.0/vembrane-2.1.0-py3-none-any.whl

---
+++ packages/vembrane/vembrane-2.1.0-py3-none-any.whl/vembrane/cli.py
@@ -0,0 +1,43 @@
+import argparse
+
+from vembrane.modules import fhir
+
+from . import __version__
+from .modules import annotate, filter, structured, table, tag
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+    subparsers = parser.add_subparsers(
+        dest="command",
+        description="valid subcommands",
+        required=True,
+    )
+    filter.add_subcommmand(subparsers)
+    table.add_subcommmand(subparsers)
+    annotate.add_subcommmand(subparsers)
+    tag.add_subcommand(subparsers)
+    structured.add_subcommand(subparsers)
+    fhir.add_subcommand(subparsers)
+
+    args = parser.parse_args()
+    if args.command == "filter":
+        filter.execute(args)
+    elif args.command == "table":
+        table.execute(args)
+    elif args.command == "annotate":
+        annotate.execute(args)
+    elif args.command == "tag":
+        tag.execute(args)
+    elif args.command == "structured":
+        structured.execute(args)
+    elif args.command == "fhir":
+        fhir.execute(args)
+    else:
+        raise ValueError(f"Unknown subcommand {args.command}")
