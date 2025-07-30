c1036f51b21289aabfe954c410343c4d12c41e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:13:22 +0000

    Add package mzhtool/1.0.0/mzhtool-1.0.0-py3-none-any.whl

---
+++ packages/mzhtool/mzhtool-1.0.0-py3-none-any.whl/mzhtool/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+r"""
+Created on 2024/6/2 0:55
+rd /S /Q "d:\pybuild"&&mkdir "d:\pybuild"&&pyinstaller D:/python/函数/pip_Mzhtools/mzhtool\__init__.py.py --workpath d:\pybuild  --distpath d:\pybuild\dist
+"""
