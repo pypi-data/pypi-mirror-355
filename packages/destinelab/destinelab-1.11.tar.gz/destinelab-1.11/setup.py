9e61d2810d948fe58d502fbc06c57245dc2c56c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:55 +0000

    Add package destinelab/1.11/destinelab-1.11.tar.gz

---
+++ packages/destinelab/destinelab-1.11.tar.gz/destinelab-1.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='destinelab',
+    version='1.11',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'lxml',
+        'PyJWT',
+    ],
+)
