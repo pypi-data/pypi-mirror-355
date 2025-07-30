d30e426b2b3816d571b065ef00292e30a5ca19e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:09 +0000

    Add package simdlib/0.1.0/simdlib-0.1.0.tar.gz

---
+++ packages/simdlib/simdlib-0.1.0.tar.gz/simdlib-0.1.0/simdlib/__init__.py
@@ -0,0 +1,20 @@
+from simdlib_module import (sum_list, sum_list_float, multiply_list, min_list, max_list, any_list, all_list,
+                            subtract_each_list, add_each_list, multiply_each_list, square_each_list,
+                            subtract_and_square_each_list
+                            )
+
+__all__ = [
+          "sum_list",
+          "sum_list_float",
+          "multiply_list",
+          "min_list", 
+          "max_list", 
+          "any_list", 
+          "all_list", 
+          "subtract",
+          "subtract_each_list",
+          "add_each_list",
+          "multiply_each_list",
+          "square_each_list",
+          "subtract_and_square_each_list"
+        ]
