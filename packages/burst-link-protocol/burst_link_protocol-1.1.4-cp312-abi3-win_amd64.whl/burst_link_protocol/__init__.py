ec02f7bc17bfcdea8d369c6d3c3afcc623085819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:48 +0000

    Add package burst-link-protocol/1.1.4/burst_link_protocol-1.1.4-cp312-abi3-win_amd64.whl

---
+++ packages/burst-link-protocol/burst_link_protocol-1.1.4-cp312-abi3-win_amd64.whl/burst_link_protocol/__init__.py
@@ -0,0 +1,5 @@
+from burst_interface_c import BurstInterfaceC
+from .main import BurstInterfacePy
+from .serial_burst_interface import SerialBurstInterface
+
+__all__ = ["BurstInterfaceC", "BurstInterfacePy", "SerialBurstInterface"]
