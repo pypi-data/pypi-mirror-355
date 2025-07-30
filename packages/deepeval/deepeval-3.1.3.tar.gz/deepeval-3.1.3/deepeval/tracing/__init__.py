4b594a4cfb1ee89bb78e928fa2a71f949b0f05b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:11:41 +0000

    Add package deepeval/3.1.3/deepeval-3.1.3.tar.gz

---
+++ packages/deepeval/deepeval-3.1.3.tar.gz/deepeval-3.1.3/deepeval/tracing/__init__.py
@@ -0,0 +1,9 @@
+from .context import update_current_span, update_current_trace
+from .attributes import (
+    LlmAttributes,
+    RetrieverAttributes,
+    ToolAttributes,
+    AgentAttributes,
+)
+from .types import BaseSpan, Trace, Feedback
+from .tracing import observe, trace_manager
