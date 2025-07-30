72b5476b98978a47934b8a5c59e769faca5d3bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:32 +0000

    Add package apsimNGpy/0.39.3.1/apsimNGpy-0.39.3.1-py3-none-any.whl

---
+++ packages/apsimNGpy/apsimNGpy-0.39.3.1-py3-none-any.whl/apsimNGpy/__init__.py
@@ -0,0 +1,20 @@
+import os.path
+import sys
+
+sys.path.append(os.path.realpath(".."))
+
+__all__= []
+try:
+    from apsimNGpy import core, manager, core_utils
+    from apsimNGpy.core.core import CoreModel
+    from apsimNGpy.core.apsim import ApsimModel
+
+    __all__.extend(['core', 'replacements', 'manager',
+                       'ApsimModel',
+                       'core_utils', 'config', 'CoreModel'])
+
+except Exception as e: # expected errors if pythonnet fails to get initialized
+
+    pass
+
+
