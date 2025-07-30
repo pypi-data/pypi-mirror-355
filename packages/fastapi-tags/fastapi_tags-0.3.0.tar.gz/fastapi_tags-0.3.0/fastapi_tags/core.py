2152653e2e108206389fd5cc0487b642b5f7290c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:17 +0000

    Add package fastapi-tags/0.3.0/fastapi_tags-0.3.0.tar.gz

---
+++ packages/fastapi-tags/fastapi_tags-0.3.0.tar.gz/fastapi_tags-0.3.0/fastapi_tags/core.py
@@ -0,0 +1,28 @@
+import importlib
+from typing import Any
+from fastapi import Response
+
+
+def dict_to_ft_component(d):
+    children_raw = d.get("_children", ())
+    if isinstance(children_raw, str):
+        children_raw = (children_raw,)
+    children = tuple(
+        dict_to_ft_component(c) if isinstance(c, dict) else c for c in children_raw
+    )
+    # TODO: cache this somehow
+    module = importlib.import_module(d["_module"])
+    obj = getattr(module, d["_name"])
+    return obj(*children, **d.get("_attrs", {}))
+
+
+class TagResponse(Response):
+    """Custom response class to handle fastapi_tags.tags.Tags."""
+
+    media_type = "text/html; charset=utf-8"
+
+    def render(self, content: Any) -> bytes:
+        """Render Tag elements to bytes of HTML."""
+        if isinstance(content, dict):
+            content = dict_to_ft_component(content)
+        return content.render().encode("utf-8")
