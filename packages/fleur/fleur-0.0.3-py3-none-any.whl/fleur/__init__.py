a399d47062b7726197cb478e295b39f711fe3299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:18 +0000

    Add package fleur/0.0.3/fleur-0.0.3-py3-none-any.whl

---
+++ packages/fleur/fleur-0.0.3-py3-none-any.whl/fleur/__init__.py
@@ -0,0 +1,7 @@
+from .scatterstats import ScatterStats
+from .betweenstats import BetweenStats
+from .utils.theme import themify
+from .input_data_handling import InputDataHandler
+
+__version__ = "0.0.3"
+__all__ = ["ScatterStats", "BetweenStats", "themify", "InputDataHandler"]
