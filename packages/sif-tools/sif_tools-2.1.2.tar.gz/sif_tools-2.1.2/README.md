fbcdcb7c97c76591593ecdbd901f84641eb91e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:53 +0000

    Add package sif-tools/2.1.2/sif_tools-2.1.2.tar.gz

---
+++ packages/sif-tools/sif_tools-2.1.2.tar.gz/sif_tools-2.1.2/README.md
@@ -0,0 +1,40 @@
+# SIF-Tools
+
+SIF-Tools is a light-weight Python toolkit designed to read `.sif` data files from an Andor Solis spectrometer, and convert them into easy-to-access numpy arrays.
+
+## Requirements
+
+- Python >= 3.6
+- NumPy
+
+## Installation
+
+* Pip install
+```bash
+pip install sif-toolkit
+```
+
+* Clone the repository
+```bash
+git clone https://github.com/BjornFS/SIF-Toolkit.git
+```
+
+## Usage
+
+### Importing SIF-Tools in a Python Script
+
+```python 
+from sif_tools import SIFconvert
+
+data = SIFconvert('tests/UnitTest.sif')
+wavelength, count = data[:, 0], data[:, 1]
+```
+
+## Support
+
+If you encounter any issues, have suggestions for add-ons, or have questions, feel free to open an issue on the [GitHub repository](https://github.com/yourusername/SIF-Toolkit/issues).
+
+## Authors
+
+- Bjorn Schroder Nielsen
+- Bjorn@SchroderNielsen.com
---
+++ packages/sif-tools/sif_tools-2.1.2.tar.gz/sif_tools-2.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+# Read the contents of the README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='sif_tools',  
+    version='2.1.2', 
+    author='Bjorn F. Schroder Nielsen', 
+    author_email='Bjorn@SchroderNielsen.com',  
+    description='A light-weight package for SIF file conversion', 
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/BjornFS/SIF-Tools', 
+    packages=find_packages(), 
+    classifiers=[
+        'Programming Language :: Python :: 3', 
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+    install_requires=[
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sif-tools=sif_tools.__main__:main',
+        ],
+    },
+)
+
+#new version:
+## change version number in setup.py Done
+## python setup.py sdist bdist_wheel
+## twine upload dist/* 
+## pip install --upgrade --force-reinstall sif-tools
