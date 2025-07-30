c390eeb468fd93c1277fe7192704267e56c600fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:49 +0000

    Add package nixt/331/nixt-331.tar.gz

---
+++ packages/nixt/nixt-331.tar.gz/nixt-331/nixt/object.py
@@ -0,0 +1,135 @@
+# This file is placed in the Public Domain.
+
+
+"a clean namespace"
+
+
+import json
+
+
+class Object:
+
+    def __contains__(self, key):
+        return key in dir(self)
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class Default(Object):
+
+    def __getattr__(self, key):
+        if key not in self:
+            setattr(self, key, "")
+        return self.__dict__.get(key, "")
+
+
+def construct(obj, *args, **kwargs):
+    if args:
+        val = args[0]
+        if isinstance(val, zip):
+            update(obj, dict(val))
+        elif isinstance(val, dict):
+            update(obj, val)
+        elif isinstance(val, Object):
+            update(obj, vars(val))
+    if kwargs:
+        update(obj, kwargs)
+
+
+def fqn(obj):
+    kin = str(type(obj)).split()[-1][1:-2]
+    if kin == "type":
+        kin = f"{obj.__module__}.{obj.__name__}"
+    return kin
+
+
+def items(obj):
+    if isinstance(obj, dict):
+        return obj.items()
+    return obj.__dict__.items()
+
+
+def keys(obj):
+    if isinstance(obj, dict):
+        return obj.keys()
+    return obj.__dict__.keys()
+
+
+def update(obj, data):
+    if isinstance(data, dict):
+        return obj.__dict__.update(data)
+    obj.__dict__.update(vars(data))
+
+
+def values(obj):
+    if isinstance(obj, dict):
+        return obj.values()
+    return obj.__dict__.values()
+
+
+class Encoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, dict):
+            return o.items()
+        if issubclass(type(o), Object):
+            return vars(o)
+        if isinstance(o, list):
+            return iter(o)
+        try:
+            return json.JSONEncoder.default(self, o)
+        except TypeError:
+            try:
+                return vars(o)
+            except TypeError:
+                return repr(o)
+
+
+def dump(obj, fp, *args, **kw):
+    kw["cls"] = Encoder
+    json.dump(obj, fp, *args, **kw)
+
+
+def dumps(obj, *args, **kw):
+    kw["cls"] = Encoder
+    return json.dumps(obj, *args, **kw)
+
+
+def hook(objdict):
+    obj = Object()
+    construct(obj, objdict)
+    return obj
+
+
+def load(fp, *args, **kw):
+    kw["object_hook"] = hook
+    return json.load(fp, *args, **kw)
+
+
+def loads(s, *args, **kw):
+    kw["object_hook"] = hook
+    return json.loads(s, *args, **kw)
+
+
+def __dir__():
+    return (
+        'Default',
+        'Object',
+        'construct',
+        'dump',
+        'dumps',
+        'fqn',
+        'items',
+        'keys',
+        'load',
+        'loads',
+        'update',
+        'values'
+    )
---
+++ packages/nixt/nixt-331.tar.gz/nixt-331/nixt/path.py
@@ -0,0 +1,62 @@
+# This file is placed in the Public Domain.
+
+
+"parhs"
+
+
+import os
+import pathlib
+
+
+class Workdir:
+
+    name = __file__.rsplit(os.sep, maxsplit=2)[-2]
+    wdr = ""
+
+
+def long(name):
+    split = name.split(".")[-1].lower()
+    res = name
+    for names in types():
+        if split == names.split(".")[-1].lower():
+            res = names
+            break
+    return res
+
+
+def pidname(name):
+    return os.path.join(Workdir.wdr, f"{name}.pid")
+
+
+def skel():
+    pth = pathlib.Path(store())
+    pth.mkdir(parents=True, exist_ok=True)
+    return str(pth)
+
+
+def store(pth=""):
+    return os.path.join(Workdir.wdr, "store", pth)
+
+
+def strip(pth, nmr=2):
+    return os.path.join(pth.split(os.sep)[-nmr:])
+
+
+def types():
+    return os.listdir(store())
+
+
+def wdr(pth):
+    return os.path.join(Workdir.wdr, pth)
+
+
+def __dir__():
+    return (
+        'Workdir',
+        'long',
+        'ident',
+        'pidname',
+        'skel',
+        'store',
+        'wdr'
+    )
