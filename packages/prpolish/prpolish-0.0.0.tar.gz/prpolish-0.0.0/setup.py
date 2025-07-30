ca337a07c5f86f3f8d0ac59a62775758ed3395ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:05 +0000

    Add package prpolish/0.0.0/prpolish-0.0.0.tar.gz

---
+++ packages/prpolish/prpolish-0.0.0.tar.gz/prpolish-0.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prpolish",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "GitPython",
+        "pyperclip"
+    ],
+    entry_points={
+        "console_scripts": [
+            "prpolish=prpolish.cli:main"
+        ]
+    },
+    author="Yash Gupta",
+    author_email="ysgupta@wisc.edu",
+    description="A CLI tool to generate PR titles and descriptions for Git repositories",
+    url="https://github.com/yashg4509/prpolish",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
