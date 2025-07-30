a9b9c9b63137c83dd585fbdcd8e0e339debc1869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:50 +0000

    Add package ecodev-front/0.0.87/ecodev_front-0.0.87.tar.gz

---
+++ packages/ecodev-front/ecodev_front-0.0.87.tar.gz/ecodev_front-0.0.87/ecodev_front/login.py
@@ -0,0 +1,39 @@
+"""
+Module implementing the navbar login / logout components
+"""
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+from ecodev_front.ids import LOGIN_BTN_ID
+from ecodev_front.ids import LOGIN_PASSWORD_INPUT_ID
+from ecodev_front.ids import LOGIN_USERNAME_INPUT_ID
+
+
+def login(username_placeholder: str = 'Username',
+          password_placeholder: str = 'Password',
+          button_label: str = 'Login',
+          button_color: str = 'white'
+          ) -> dmc.Group:
+    """
+    Login bar component, shown when no user-tokens have been set.
+    """
+    return dmc.Group([
+        dmc.TextInput(id=LOGIN_USERNAME_INPUT_ID,
+                      placeholder=username_placeholder,
+                      radius='xl',
+                      style={'width': '15vw'},
+                      leftSection=DashIconify(icon='material-symbols:supervised-user-circle')),
+        dmc.PasswordInput(id=LOGIN_PASSWORD_INPUT_ID,
+                          placeholder=password_placeholder,
+                          radius='xl',
+                          style={'width': '15vw'},
+                          leftSection=DashIconify(icon='bi:shield-lock')),
+        dmc.Button(button_label,
+                   id=LOGIN_BTN_ID,
+                   radius='xl',
+                   variant=button_color,
+                   style={'margin': '5px', 'width': '5vw', 'color': 'gray'})
+    ], style={'display': 'flex',
+              'justifyContent': 'center',
+              'alignItems': 'center',
+              'textAlign': 'center'})
