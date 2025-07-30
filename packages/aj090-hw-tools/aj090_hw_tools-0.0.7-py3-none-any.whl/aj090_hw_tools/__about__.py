f27b05b6a634d7b7170a71c5d6f4c7ca879001ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:14 +0000

    Add package aj090-hw-tools/0.0.7/aj090_hw_tools-0.0.7-py3-none-any.whl

---
+++ packages/aj090-hw-tools/aj090_hw_tools-0.0.7-py3-none-any.whl/aj090_hw_tools/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Vasencheg <roman.vasilev@nobitlost.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.7"
---
+++ packages/aj090-hw-tools/aj090_hw_tools-0.0.7-py3-none-any.whl/aj090_hw_tools/tools/__init__.py
@@ -0,0 +1,4 @@
+from .firmware import firmware
+from .test import test
+from .serial_number import serial
+from .info import info
