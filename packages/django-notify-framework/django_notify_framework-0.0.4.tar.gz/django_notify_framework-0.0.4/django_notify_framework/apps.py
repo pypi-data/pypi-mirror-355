e638e5269005e9d39482a111e76dbe7b1592c6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:21 +0000

    Add package django-notify-framework/0.0.4/django_notify_framework-0.0.4.tar.gz

---
+++ packages/django-notify-framework/django_notify_framework-0.0.4.tar.gz/django_notify_framework-0.0.4/django_notify_framework/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class DjangoNotifyAppConfig(AppConfig):
+
+    def ready(self):
+        default_auto_field = 'django.db.models.AutoField'
+        name = 'django_notify_framework'
+        verbose_name = 'Django Notify Framework'
