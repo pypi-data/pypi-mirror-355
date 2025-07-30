2b4e5a2b1f53ec256583a54820a8e83dfe49b8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:13:14 +0000

    Add package deepset-cloud-sdk/1.7.0/deepset_cloud_sdk-1.7.0-py3-none-any.whl

---
+++ packages/deepset-cloud-sdk/deepset_cloud_sdk-1.7.0-py3-none-any.whl/deepset_cloud_sdk/__about__.py
@@ -0,0 +1,4 @@
+"""This file defines the package version."""
+# Will be automatically overridden during the release process
+# It's okay if this is outdated in the repo. We will use the tag from the release as the version.
+__version__ = "1.7.0"
