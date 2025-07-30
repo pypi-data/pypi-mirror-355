e43df6103b1f4812ae4d56c455a8da86c01f3ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:04 +0000

    Add package chenxizhang-pyhello/0.1.0/chenxizhang_pyhello-0.1.0.tar.gz

---
+++ packages/chenxizhang-pyhello/chenxizhang_pyhello-0.1.0.tar.gz/chenxizhang_pyhello-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="chenxizhang-pyhello",
+    version="0.1.0",
+    description="A simple hello world Python module",
+    py_modules=["pyhello"],
+    python_requires=">=3.6",
+)
