fab99f8e85ef617485249339da20e197ac86730d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:13 +0000

    Add package pytest-infrahouse/0.9.1/pytest_infrahouse-0.9.1.tar.gz

---
+++ packages/pytest-infrahouse/pytest_infrahouse-0.9.1.tar.gz/pytest_infrahouse-0.9.1/src/pytest_infrahouse/data/jumphost/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+}
+variable "role_arn" {
+  default = null
+}
+
+variable "environment" {
+  default = "development"
+}
+
+variable "subnet_public_ids" {}
+variable "subnet_private_ids" {}
+variable "test_zone" {}
