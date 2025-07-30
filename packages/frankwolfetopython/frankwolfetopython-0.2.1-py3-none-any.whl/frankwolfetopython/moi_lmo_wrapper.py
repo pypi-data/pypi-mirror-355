45970b605de7db1486e5adf7fbfd9cfc42562224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:48 +0000

    Add package frankwolfetopython/0.2.1/frankwolfetopython-0.2.1-py3-none-any.whl

---
+++ packages/frankwolfetopython/frankwolfetopython-0.2.1-py3-none-any.whl/frankwolfetopython/moi_lmo_wrapper.py
@@ -0,0 +1,7 @@
+def wrap_moi(A, b):
+    """
+    Will allow user to set custom linear constraints for solver using the MathOptInterface (MOI) in Julia
+
+    Returns a MOI model with the given linear constraints.
+    """
+    pass
