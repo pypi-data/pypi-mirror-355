1a09bab7a44f7f31a233b4d290ffa840093a1e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:11:12 +0000

    Add package dishpy/1.0.1/dishpy-1.0.1.tar.gz

---
+++ packages/dishpy/dishpy-1.0.1.tar.gz/dishpy-1.0.1/examples/Add Two Numbers/src/main.py
@@ -0,0 +1,9 @@
+"""
+Template for the main.py file.
+
+Eventually, DishPy will add better templates to achieve feature parity with VEXcode, but for now you're stuck with this empty file!
+"""
+
+from add_two_nums import add_two_numbers
+
+print(add_two_numbers(6, 7))
