5e310b2e25dd7e4ffa84af217baf4eecdb0541ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:08 +0000

    Add package CTkFileDialog/0.1.1/ctkfiledialog-0.1.1.tar.gz

---
+++ packages/CTkFileDialog/ctkfiledialog-0.1.1.tar.gz/ctkfiledialog-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CTkFileDialog',
+    version='0.1.1',
+    author='FlickGMD',
+    author_email='salvadorfabiansulcacuba@gmail.com',
+    description='Un elegante diálogo de archivos para CustomTkinter',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'customtkinter>=5.0.0',
+        'Pillow',
+        'opencv-python',
+        'CTkMessagebox',
+        'CTkToolTip'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    python_requires='>=3.7',
+)
