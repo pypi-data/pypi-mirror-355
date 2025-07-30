9a1bbd2e853ed31c733c64db7fc3690c2cae9441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:58 +0000

    Add package napari-spotiflow/0.4.2/napari_spotiflow-0.4.2.tar.gz

---
+++ packages/napari-spotiflow/napari_spotiflow-0.4.2.tar.gz/napari_spotiflow-0.4.2/tests/test_reader.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from napari_spotiflow._io_hooks import reader_function
+
+
+def test_reader():
+    paths = sorted(Path('data').glob('*.csv'))
+    layers = tuple(reader_function(path) for path in paths)
+
+    assert not any([lay is None for lay in layers])
+
+
+
+if __name__ == '__main__':
+
+    layers = test_reader()
