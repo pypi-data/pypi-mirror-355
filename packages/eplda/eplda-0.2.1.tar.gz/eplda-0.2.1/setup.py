9c426effeec514379d5810dc284e839c80c50bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:05:55 +0000

    Add package eplda/0.2.1/eplda-0.2.1.tar.gz

---
+++ packages/eplda/eplda-0.2.1.tar.gz/eplda-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eplda",
+    version="0.2.1",
+    description="An unofficial API handler to footballapi.pulselive.com. Permitted for study purposes only.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Zhou Yuyang",
+    author_email="joeyjoeyjoemail@163.com",
+    url="https://github.com/zhouyuyang-joey/English-Premier-League-Data-API",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pandas"
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
