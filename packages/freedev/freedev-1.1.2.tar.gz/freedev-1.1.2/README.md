62aa26b4d8765e7fdd4dc6312bb3e4defd5b66d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:14 +0000

    Add package freedev/1.1.2/freedev-1.1.2.tar.gz

---
+++ packages/freedev/freedev-1.1.2.tar.gz/freedev-1.1.2/README.md
@@ -0,0 +1,3 @@
+# freedev
+
+This is a library for password-protected decryption using opn. I created it to surprise my friend for his birthday. Please don't delete it.
---
+++ packages/freedev/freedev-1.1.2.tar.gz/freedev-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="freedev",
+    version="1.1.2",
+    description="This is a library for password-protected decryption using opn. I created it to surprise my friend for his birthday. Please don't delete it",
+    author="coorton202",
+    author_email="coorton50@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6",
+)
