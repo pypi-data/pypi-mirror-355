c77bdb9cf681d2382f75f7ece3106c133c9ce82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:32 +0000

    Add package ultradetector/0.1.14/ultradetector-0.1.14.tar.gz

---
+++ packages/ultradetector/ultradetector-0.1.14.tar.gz/ultradetector-0.1.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ultradetector",  # PyPI 上的包名（必须唯一）
+    py_modules=["ultradetector"],  # 模块名
+    version="0.1.14",  # 版本号
+    author="weki",
+    # 限定Python 3.8,3.9,3.10
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "nibabel",
+        "matplotlib",
+        "opencv-python",
+        "psutil",
+        "pyyaml",
+        "requests"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ultrasegmentator=ultrasegmentator.python_api:main'
+        ]
+    },
+)
