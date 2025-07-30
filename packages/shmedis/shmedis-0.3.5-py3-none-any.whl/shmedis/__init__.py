9598541072bb35e584b99016406738f81f7394b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:04 +0000

    Add package shmedis/0.3.5/shmedis-0.3.5-py3-none-any.whl

---
+++ packages/shmedis/shmedis-0.3.5-py3-none-any.whl/shmedis/__init__.py
@@ -0,0 +1,26 @@
+from .core import Shmedis
+
+try:
+    from multiprocessing import resource_tracker
+
+
+    def disable_shm_tracking():
+        """
+        In python3.13- ,shared memory is released before the process exits.
+        This function is used to disable resource tracker for shared memory.
+        """
+        original_register = resource_tracker.register
+
+        def patched_register(name, rtype):
+            if rtype == "shared_memory":
+                return
+            return original_register(name, rtype)
+
+        resource_tracker.register = patched_register
+
+
+    disable_shm_tracking()
+except ImportError:
+    pass
+
+__all__ = ["Shmedis"]
