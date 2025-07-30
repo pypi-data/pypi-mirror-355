ff9778aad0b5cf809209135849a31bc3e9d054c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:09:27 +0000

    Add package rquote/0.2.5/rquote-0.2.5.tar.gz

---
+++ packages/rquote/rquote-0.2.5.tar.gz/rquote-0.2.5/rquote/__init__.py
@@ -0,0 +1,13 @@
+'''
+rquote
+
+A stock history data api and related tools
+
+Copyright (c) 2021 Roi ZHAO
+
+'''
+
+from .main import get_price, get_stock_concepts, get_concept_stocks
+from .main import get_all_concepts, get_all_industries
+from .utils import WebUtils, BasicFactors
+from .plots import PlotUtils
