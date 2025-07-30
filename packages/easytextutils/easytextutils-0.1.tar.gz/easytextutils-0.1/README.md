8e7d58086a8797890d65246ba68f40ad57ab9f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:41 +0000

    Add package easytextutils/0.1/easytextutils-0.1.tar.gz

---
+++ packages/easytextutils/easytextutils-0.1.tar.gz/easytextutils-0.1/README.md
@@ -0,0 +1,10 @@
+# easytextutils
+
+A beginner-friendly Python package with simple text utilities.
+
+## Features
+- Count vowels
+- Check palindromes
+- Remove punctuation
+- Word frequency
+- Capitalize each word
---
+++ packages/easytextutils/easytextutils-0.1.tar.gz/easytextutils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easytextutils',
+    version='0.1',
+    description='A simple text utility library',
+    author='Troop',
+    packages=find_packages(),
+    install_requires=[],
+)
