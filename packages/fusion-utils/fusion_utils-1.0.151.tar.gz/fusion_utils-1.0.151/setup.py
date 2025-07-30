e8e753db9106ea246547db5a33e369911e69dc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:08 +0000

    Add package fusion-utils/1.0.151/fusion_utils-1.0.151.tar.gz

---
+++ packages/fusion-utils/fusion_utils-1.0.151.tar.gz/fusion_utils-1.0.151/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+#
+setup(
+    name='fusion_utils',
+    version='1.0.151',
+    install_requires=[
+        'pandas',
+        'google-cloud-bigquery',
+        'numpy',
+        'slack_sdk'
+        # Add other dependencies here
+    ],
+    author='Dylan D',
+    author_email='dylan.doyle@jdpa.com',
+    description='A utility package Fusion 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dylandoyle11/fusion_utils',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
+
