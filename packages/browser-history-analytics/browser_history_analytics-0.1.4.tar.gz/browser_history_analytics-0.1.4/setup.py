cac8867310701945aabdc220b40ef723a8ebb4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:32 +0000

    Add package browser-history-analytics/0.1.4/browser_history_analytics-0.1.4.tar.gz

---
+++ packages/browser-history-analytics/browser_history_analytics-0.1.4.tar.gz/browser_history_analytics-0.1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="browser-history-analytics",
+    version="0.1.4",
+    author="Arpit Sengar (arpy8)",
+    description="A package to visualize your browser history :D",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'browser_history_analytics': ['*.py'],
+    },
+    install_requires=[
+        "streamlit", 
+        "plotly", 
+        "pandas", 
+        "browser-history", 
+        "numpy", 
+        "seaborn", 
+        "matplotlib", 
+        "urllib3",
+        "setuptools>=66.1.1",
+        "rich",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "browser-history-analytics=browser_history_analytics.main:main",
+        ],
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
