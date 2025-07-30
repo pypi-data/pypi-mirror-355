63b45eca48e1500a54cf3ab96f17eca24be7d131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:02 +0000

    Add package data-ingestion-tool/1.2.3/data_ingestion_tool-1.2.3.tar.gz

---
+++ packages/data-ingestion-tool/data_ingestion_tool-1.2.3.tar.gz/data_ingestion_tool-1.2.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_ingestion_tool',
+    version='1.2.3',
+    author='Bug Hunter',
+    author_email='sayhi@thekingof.cool',
+    description='A toolset for ingesting data from Box into Delta tables, and exporting data to Box.',
+    packages=find_packages(),
+    install_requires=[
+        'boxsdk',
+        'pandas',
+        'cerberus',
+        'cerberus-python-client',
+        'openpyxl',
+        'pyspark',
+        'delta-spark'
+    ],
+    python_requires='>=3.6',
+)
