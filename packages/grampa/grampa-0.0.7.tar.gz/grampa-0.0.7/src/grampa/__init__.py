d3fe238aba7a9b50284c08435950a45a082c2edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:32 +0000

    Add package grampa/0.0.7/grampa-0.0.7.tar.gz

---
+++ packages/grampa/grampa-0.0.7.tar.gz/grampa-0.0.7/src/grampa/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from .__version import version as __version__
+from .__version import version_tuple as __version_tuple__
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+]
+
+"""
+GRAMPA: A package for analyzing magnetic fields.
+
+This package provides tools for magnetic field generation as Gaussian Random Fields see magneticfieldmodel.py
+- Model generation (magneticfieldmodel.py)
+- Visualization (todo)
+
+Modules:
+- magneticfieldmodel.py
+- visualization (todo)
+"""
---
+++ packages/grampa/grampa-0.0.7.tar.gz/grampa-0.0.7/src/grampa/__version.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+version: str
+version_tuple: tuple[int, int, int] | tuple[int, int, int, str, str]
+
+__version__ = version = "0.0.7"
+__version_tuple__ = version_tuple = (0, 0, 6, "", "")
