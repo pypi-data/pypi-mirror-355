06256f11dffebd9340f9d239a99bd56fd23c37cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:58 +0000

    Add package kap_sdk/0.1.24/kap_sdk-0.1.24-py3-none-any.whl

---
+++ packages/kap_sdk/kap_sdk-0.1.24-py3-none-any.whl/kap_sdk/models/company_info.py
@@ -0,0 +1,79 @@
+from dataclasses import asdict, dataclass
+from asyncio.log import logger
+from pyppeteer import launch
+from bs4 import BeautifulSoup
+from kap_sdk.models.company import Company
+from kap_sdk import _get_browser_config
+
+
+@dataclass
+class CompanyInfo:
+    address: str = ""
+    mail: list[str] = None
+    website: str = ""
+    companys_duration: str = ""
+    independent_audit_firm: str = ""
+    indices: list[str] = None
+    sectors: list[str] = None
+    equity_market: str = ""
+
+    def dict(self):
+        return asdict(self)
+
+
+_GENERAL_URL = "https://www.kap.org.tr/tr/sirket-bilgileri/ozet/"
+
+
+def _find_cells(
+    soup: BeautifulSoup,
+    string: str,
+) -> list[str]:
+    data = soup.find('h3', string=string)
+    if data:
+        next_container = data.find_next(["p", "div"])
+        if next_container is None:
+            return [""]
+        text_elements = next_container.find_all(string=True, recursive=True)
+        if not text_elements:
+            return [""]
+        return [a.text for a in text_elements]
+    return [""]
+
+
+async def scrape_company_info(company: Company) -> 'CompanyInfo':
+    """Scrape detailed information for a specific company from KAP website.
+
+    Args:
+        company (Company): The company object to scrape information for.
+
+    Returns:
+        CompanyInfo: A CompanyInfo object with detailed data from the KAP website.
+    """
+    browser = None
+    try:
+        config = _get_browser_config()
+        browser = await launch(**config)
+        page = await browser.newPage()
+        await page.goto(_GENERAL_URL + company.path, {"waitUntil": "domcontentloaded"})
+        await page.waitForSelector('#financialTable', timeout=10000)
+        content = await page.content()
+        soup = BeautifulSoup(content, 'html.parser')
+        info = CompanyInfo()
+
+        info.address = _find_cells(soup, 'Merkez Adresi')[0]
+        info.mail = _find_cells(soup, 'Elektronik Posta Adresi')
+        info.website = _find_cells(soup, 'İnternet Adresi')[0]
+        info.companys_duration = _find_cells(soup, 'Şirketin Süresi')[0]
+        info.independent_audit_firm = _find_cells(
+            soup, 'Bağımsız Denetim Kuruluşu')[0]
+        info.indices = _find_cells(soup, 'Şirketin Dahil Olduğu Endeksler')
+        info.sectors = _find_cells(soup, 'Şirketin Sektörü')
+        info.equity_market = _find_cells(
+            soup, 'Sermaye Piyasası Aracının İşlem Gördüğü Pazar')[0]
+        return info
+    except Exception as e:
+        logger.error(f"Error scraping company info: {e}")
+        return None
+    finally:
+        if browser:
+            await browser.close()
