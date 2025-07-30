d7a69642752bd7fa8a2e4bc4cf79b2d7504adb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:21 +0000

    Add package gygo42/7.8.7/gygo42-7.8.7.tar.gz

---
+++ packages/gygo42/gygo42-7.8.7.tar.gz/gygo42-7.8.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gygo42",
+    version="7.8.7",
+    author="asd dasd",
+    description="zxczxczxc",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["pyperclip", "mistralai"], 
+)
