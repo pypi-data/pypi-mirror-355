0725840c647ad1249254ef4e5b0d207d7ad7b7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:36 +0000

    Add package discosg/0.0.4/discosg-0.0.4.tar.gz

---
+++ packages/discosg/discosg-0.0.4.tar.gz/discosg-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='discosg',
+    version='0.0.4',
+    author='Shaoqing Lin, Zhuang Li',
+    author_email='sqlinn@whu.edu.cn',
+    description='A package for discourse-level scene graph parsing and evaluation.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ShaoqLin/DiscoSG',
+    package_dir={'': "src"},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    python_requires=">=3.8.0",
+    install_requires=[
+        'torch<=2.6.0',
+        'transformers',
+        'tqdm',
+        'nltk',
+        'tqdm',
+        'peft',
+        'huggingface_hub'
+        # Add other dependencies needed for your package
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Add additional classifiers as appropriate for your project
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
+    ],
+)
