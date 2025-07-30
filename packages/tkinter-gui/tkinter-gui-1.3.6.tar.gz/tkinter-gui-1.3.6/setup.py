e484e98e983ee81462f6326a874949f1bcc90604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:17 +0000

    Add package tkinter-gui/1.3.6/tkinter-gui-1.3.6.tar.gz

---
+++ packages/tkinter-gui/tkinter-gui-1.3.6.tar.gz/tkinter-gui-1.3.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='tkinter-gui',
+	version='1.3.6',
+	author='danon',
+	author_email='',
+	description='',
+	packages=['gui_'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.9',
+)
