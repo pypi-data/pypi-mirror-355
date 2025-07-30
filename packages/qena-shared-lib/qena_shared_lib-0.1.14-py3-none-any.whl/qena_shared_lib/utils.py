aad36f7f145e634e91567e0c85181a833eaf603b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:17 +0000

    Add package qena-shared-lib/0.1.14/qena_shared_lib-0.1.14-py3-none-any.whl

---
+++ packages/qena-shared-lib/qena_shared_lib-0.1.14-py3-none-any.whl/qena_shared_lib/utils.py
@@ -0,0 +1,34 @@
+from asyncio import AbstractEventLoop, get_running_loop
+
+from pydantic import TypeAdapter
+
+__all__ = ["AsyncEventLoopMixin", "TypeAdapterCache"]
+
+
+class AsyncEventLoopMixin:
+    _LOOP: AbstractEventLoop | None = None
+
+    @property
+    def loop(self) -> AbstractEventLoop:
+        if self._LOOP is None:
+            self._LOOP = get_running_loop()
+
+        return self._LOOP
+
+    def init(self) -> None:
+        self._LOOP = get_running_loop()
+
+
+class TypeAdapterCache:
+    _cache: dict[type, TypeAdapter] = {}
+
+    @classmethod
+    def cache_annotation(cls, annotation: type) -> None:
+        if annotation not in cls._cache:
+            cls._cache[annotation] = TypeAdapter(annotation)
+
+    @classmethod
+    def get_type_adapter(cls, annotation: type) -> TypeAdapter:
+        cls.cache_annotation(annotation)
+
+        return cls._cache[annotation]
