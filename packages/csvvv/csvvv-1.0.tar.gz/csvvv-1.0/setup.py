b8804f26f7701ebe1f18e5f4f34d54b6418b6799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:24 +0000

    Add package csvvv/1.0/csvvv-1.0.tar.gz

---
+++ packages/csvvv/csvvv-1.0.tar.gz/csvvv-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="csvvv",
+    version="1.0",
+    description="A utility for handling CSV files (for cybersecurity demo purposes only).",
+    long_description="This package is designed for educational or demonstration purposes. It may contain security risks and should not be used in production environments.",
+    long_description_content_type="text/markdown",
+    author="Jaehoon Lee",
+    author_email="your_email@example.com",
+    py_modules=["csvvv"],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+    ],
+    python_requires='>=3.6',
+)
