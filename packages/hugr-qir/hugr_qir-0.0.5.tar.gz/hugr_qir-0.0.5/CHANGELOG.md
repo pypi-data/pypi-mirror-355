0b8937f72f97919dfc6ce87d6be20aac226f2993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:37 +0000

    Add package hugr-qir/0.0.5/hugr_qir-0.0.5.tar.gz

---
+++ packages/hugr-qir/hugr_qir-0.0.5.tar.gz/hugr_qir-0.0.5/CHANGELOG.md
@@ -0,0 +1,17 @@
+## Changelog
+
+### v0.0.5
+
+- update hugr_to_qir to take ModulePointer and bytes for conversion
+
+### v0.0.4
+
+- update guppylang version requirement 0.19.1
+
+### v0.0.3
+
+- update quantinuum-qircheck version requirement to 0.2.0
+
+### v0.0.1
+
+- add hugr_to_qir conversion function
---
+++ packages/hugr-qir/hugr_qir-0.0.5.tar.gz/hugr_qir-0.0.5/guppy_examples/quantum-functions-2.py
@@ -0,0 +1,39 @@
+import sys
+from typing import no_type_check
+
+from guppylang import guppy, quantum, qubit
+from guppylang.std.angles import angles
+from guppylang.std.builtins import result
+from guppylang.std.quantum import h, measure
+
+guppy.load(quantum)
+guppy.load(angles)
+
+
+@guppy
+@no_type_check
+def fun_func(q: qubit) -> None:
+    h(q)
+
+
+@guppy
+@no_type_check
+def fun_func_2(q0: qubit, q1: qubit) -> None:
+    fun_func(q0)
+    fun_func(q1)
+
+
+@guppy
+@no_type_check
+def main() -> None:
+    q0 = qubit()
+    q1 = qubit()
+
+    fun_func_2(q0, q1)
+
+    result("0", measure(q0))
+    result("1", measure(q1))
+
+
+if __name__ == "__main__":
+    sys.stdout.buffer.write(guppy.get_module().compile().package.to_bytes())
