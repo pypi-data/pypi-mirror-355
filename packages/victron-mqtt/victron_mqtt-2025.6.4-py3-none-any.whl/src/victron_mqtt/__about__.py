c883f15b61272d4dc217f2255616e3ccde6cfed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:16 +0000

    Add package victron-mqtt/2025.6.4/victron_mqtt-2025.6.4-py3-none-any.whl

---
+++ packages/victron-mqtt/victron_mqtt-2025.6.4-py3-none-any.whl/src/victron_mqtt/__about__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present Johan du Plessis https://github.com/johanslab
+#
+# SPDX-License-Identifier: MIT
+"""
+Version information.
+"""
+
+__version__ = "2025.6.4"
+__author__ = "Johan du Plessis"
