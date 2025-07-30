1d6e8dba3fbffa8779caba9a4785feb1ddb57ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:23 +0000

    Add package agentops/0.4.15/agentops-0.4.15.tar.gz

---
+++ packages/agentops/agentops-0.4.15.tar.gz/agentops-0.4.15/agentops/instrumentation/common/__init__.py
@@ -0,0 +1,4 @@
+from agentops.instrumentation.common.attributes import AttributeMap, _extract_attributes_from_mapping
+from agentops.instrumentation.common.wrappers import _with_tracer_wrapper
+
+__all__ = ["AttributeMap", "_extract_attributes_from_mapping", "_with_tracer_wrapper"]
