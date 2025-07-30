56c8f736a6e0422284072beae467825ffdca8ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:42 +0000

    Add package sx126x/2.2.0/sx126x-2.2.0.tar.gz

---
+++ packages/sx126x/sx126x-2.2.0.tar.gz/sx126x-2.2.0/sx126x/__init__.py
@@ -0,0 +1,26 @@
+from sx126x.sx126x import SX126X
+from sx126x.enums import *
+from sx126x.models import *
+
+__version__ = "2.2.0"  # Keep in sync with pyproject.toml
+
+__all__ = [
+    "SX126X",
+    "AirSpeed",
+    "AmbientNoise",
+    "BaudRate",
+    "Command",
+    "LBT",
+    "Mode",
+    "PacketSize",
+    "Parity",
+    "Register",
+    "Relay",
+    "RSSI",
+    "TransferMethod",
+    "TransmitPower",
+    "WORControl",
+    "WORPeriod",
+    "Address",
+    "CryptKey",
+]
