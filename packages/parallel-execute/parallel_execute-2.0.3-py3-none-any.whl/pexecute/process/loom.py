fc22f3474cde2bfc5e09895037affa66de9db66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:33 +0000

    Add package parallel-execute/2.0.3/parallel_execute-2.0.3-py3-none-any.whl

---
+++ packages/parallel-execute/parallel_execute-2.0.3-py3-none-any.whl/pexecute/process/loom.py
@@ -0,0 +1,23 @@
+import warnings
+from concurra import TaskRunner
+
+
+class ProcessLoom(TaskRunner):
+    """Deprecated Loom interface using multi-processing.
+    Use the new Concurra library with TaskRunner for more features: https://pypi.org/project/concurra/
+    """
+
+    def __init__(self, max_runner_cap=4, **kwargs):
+        kwargs.pop('max_concurrency', None)
+        kwargs.pop('use_multiprocessing', None)
+        super().__init__(max_concurrency=max_runner_cap, use_multiprocessing=True, **kwargs)
+
+    def execute(self, verify=True, raise_exception=False, error_message=None):
+        output = self.run(verify=verify, raise_exception=raise_exception, error_message=error_message)
+        for _, result in output.items():
+            result['output'] = result.get('result', None)
+            result['started_time'] = result.get('start_time', None)
+            result['finished_time'] = result.get('end_time', None)
+            result['execution_time'] = result.get('duration_seconds', None)
+            result['got_error'] = result.get('has_failed', None)
+        return output
