0fbeb36f122b81d104a8c9623f2ee3f1cd27796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:42 +0000

    Add package resilient-requester/0.1.3/resilient_requester-0.1.3-py3-none-any.whl

---
+++ packages/resilient-requester/resilient_requester-0.1.3-py3-none-any.whl/resilient_requester/helpers/helper_functions.py
@@ -0,0 +1,54 @@
+import logging
+import random
+import time
+
+from fake_useragent import UserAgent
+
+
+def get_headers():
+    ua = UserAgent()
+    return {
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'connection': 'keep-alive',
+        'user-agent': ua.random,
+    }
+
+def check_params(proxy, headers, timeout, retries):
+    if proxy:
+        if not isinstance(proxy, dict):
+            raise TypeError("Proxy must be a dictionary")
+    if headers:
+        if not isinstance(headers, dict):
+            raise TypeError("Headers must be a dictionary")
+    if timeout:
+        if not isinstance(timeout, int):
+            raise TypeError("Timeout must be an integer")
+
+    if not timeout or timeout < 5:
+        logging.info("Timeout must be at least 5 seconds. Setting to 5 seconds.")
+        timeout = 5
+
+    if not retries or retries < 1:
+        logging.info("Retries must be at least 1. Setting to 1.")
+        retries = 1
+
+    if not headers:
+        logging.info("No headers provided. Setting to default.")
+        headers = get_headers()
+
+    if not proxy:
+        logging.info("No proxy provided. Adding a proxy is advised.")
+
+    return proxy, headers, timeout, retries
+
+def handle_exception(e, attempts, retries):
+    logging.error(f"Exception occurred: {e}")
+    if attempts < retries:
+        attempts += 1
+        waiting_time = random.randint(1, 5)
+        logging.info(f"Retrying in {waiting_time} seconds...")
+        time.sleep(waiting_time)
+        return attempts
+    logging.error(f"Maximum number of retries {retries} reached. Exiting...")
+    return None
