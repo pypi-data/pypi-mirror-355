165ce67365e1ea7d506c85c979ea0185f7e49d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:39 +0000

    Add package ultradetector/0.1.0/ultradetector-0.1.0.tar.gz

---
+++ packages/ultradetector/ultradetector-0.1.0.tar.gz/ultradetector-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ultradetector",  # PyPI 上的包名（必须唯一）
+    py_modules=["ultradetector"],  # 模块名
+    version="0.1.0",  # 版本号
+    author="weki",
+    python_requires="==3.10.*",  # 严格限定Python 3.10
+    install_requires=[
+        "torch==2.6.*",  # 限定torch 2.6系列
+        "numpy==2.1.*",  # 限定numpy 2.1系列
+        "nibabel",
+        "matplotlib",
+        "opencv-python",
+        "psutil"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
---
+++ packages/ultradetector/ultradetector-0.1.0.tar.gz/ultradetector-0.1.0/ultradetector/ultralytics/models/yolo/classify/__init__.py
@@ -0,0 +1,7 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+from ultradetector.ultralytics.models.yolo.classify.predict import ClassificationPredictor
+from ultradetector.ultralytics.models.yolo.classify.train import ClassificationTrainer
+from ultradetector.ultralytics.models.yolo.classify.val import ClassificationValidator
+
+__all__ = "ClassificationPredictor", "ClassificationTrainer", "ClassificationValidator"
