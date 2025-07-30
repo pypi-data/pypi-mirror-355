14295bb6b098db070be6b52a6896157a74949335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:58 +0000

    Add package asup/0.0.14/asup-0.0.14.tar.gz

---
+++ packages/asup/asup-0.0.14.tar.gz/asup-0.0.14/src/asup/print.py
@@ -0,0 +1,59 @@
+import importlib.resources as pkg_resources
+import subprocess
+import os
+
+from html2image import Html2Image
+import jinja2
+
+from asup.tasks import Task
+
+
+def print_task(task: Task):
+    hti = Html2Image(size=(1109, 696))
+    with pkg_resources.path("asup", "template.html") as search_path:
+        templateLoader = jinja2.FileSystemLoader(
+            searchpath=os.path.dirname(search_path)
+        )
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        TEMPLATE_FILE = "template.html"
+        template = templateEnv.get_template(TEMPLATE_FILE)
+        templateVars = {
+            "header": task.name,
+            "content": task.description,
+            "footer": "•" * task.priority,
+        }
+        cwd = os.getcwd()
+
+        outputText = template.render(templateVars)
+        with open("/tmp/out.html", "w+") as f:
+            f.write(outputText)
+
+    hti.screenshot(
+        url="file:///tmp/out.html",
+        save_as="asup.png",
+    )
+    subprocess.run(
+        [
+            "brother_ql",
+            "print",
+            "-l",
+            "62",
+            "--red",
+            "-r",
+            "90",
+            "asup.png",
+        ],
+        check=True,
+    )
+    os.remove(cwd + "asup.png")
+    os.remove("/tmp/out.html")
+
+
+if __name__ == "__main__":
+    task = Task(
+        name="Example Task",
+        description="This is an example task description. 123",
+        completed=False,
+        priority=3,
+    )
+    print_task(task)
