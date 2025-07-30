2b22ab56bbfa7c6d89e6a8b8e891dcb656ec6de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:05:54 +0000

    Add package terra_ui_components/0.0.49/terra_ui_components-0.0.49.tar.gz

---
+++ packages/terra_ui_components/terra_ui_components-0.0.49.tar.gz/terra_ui_components-0.0.49/docs/pages/components/data-rods.md
@@ -0,0 +1,25 @@
+---
+meta:
+    title: Data Rods
+    description:
+layout: component
+---
+
+```html:preview
+<terra-data-rods
+    variable-entry-id="NLDAS_FORA0125_H_2_0_CRainf_frac"
+    location="38.96, -78.75"
+></terra-data-rods>
+```
+
+## Examples
+
+### First Example
+
+TODO
+
+### Second Example
+
+TODO
+
+[component-metadata:terra-data-rods]
---
+++ packages/terra_ui_components/terra_ui_components-0.0.49.tar.gz/terra_ui_components-0.0.49/src/components/data-rods/data-rods.styles.ts
@@ -0,0 +1,32 @@
+import { css } from 'lit'
+
+export default css`
+    :host {
+        display: grid;
+        gap: 1.5rem 0.75rem;
+        grid-template-rows: auto;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    terra-variable-combobox {
+        grid-column: 1 / 2;
+    }
+
+    terra-spatial-picker {
+        grid-column: 2 / 3;
+    }
+
+    terra-date-range-slider {
+        grid-column: 1 / 3;
+        padding-block-start: 2rem;
+    }
+
+    terra-time-series {
+        grid-column: 1 / 3;
+    }
+
+    terra-spatial-picker::part(leaflet-bbox),
+    terra-spatial-picker::part(leaflet-edit) {
+        display: none;
+    }
+`
