10fba2c09cb4610f4c8652a9c6cce38f7ae9df53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:44 +0000

    Add package aiocallback/0.1.6/aiocallback-0.1.6.tar.gz

---
+++ packages/aiocallback/aiocallback-0.1.6.tar.gz/aiocallback-0.1.6/aiocallback/__init__.py
@@ -0,0 +1,16 @@
+# ruff: noqa: E402,F401
+from .events import (
+    EventList,
+    EventListMetaclass,
+    EventWrapper,
+    SelfEventWrapper,
+    contextevent,
+    event,
+    subclasscontextevent,
+    subcontextevent,
+    subclassevent,
+)
+
+__author__ = "Vizonex"
+
+from .__version__ import __version__
