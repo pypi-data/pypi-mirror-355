e3ba9b2d1b88d19e342df9d4ab960b9c72c46e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:55 +0000

    Add package kinde-python-sdk/2.0.0b11/kinde_python_sdk-2.0.0b11-py3-none-any.whl

---
+++ packages/kinde-python-sdk/kinde_python_sdk-2.0.0b11-py3-none-any.whl/kinde_sdk/auth/__init__.py
@@ -0,0 +1,9 @@
+from .oauth import OAuth
+from .token_manager import TokenManager
+from .user_session import UserSession
+from .permissions import permissions
+from .claims import claims
+from .feature_flags import feature_flags
+from .portals import portals
+
+__all__ = ["OAuth", "TokenManager", "UserSession", "permissions", "claims", "feature_flags", "portals"]
