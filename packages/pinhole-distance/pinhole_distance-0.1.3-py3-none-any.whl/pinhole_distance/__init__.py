a04027eaa4129d59a052eefe5a902d23e4c347e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:25 +0000

    Add package pinhole-distance/0.1.3/pinhole_distance-0.1.3-py3-none-any.whl

---
+++ packages/pinhole-distance/pinhole_distance-0.1.3-py3-none-any.whl/pinhole_distance/__init__.py
@@ -0,0 +1,8 @@
+from .classes import DistortionTable, Lens, Sensor, Package
+
+# Export pre-made packages for different camera modules
+from .fake_flat import PACKAGE as fake_flat
+from .ov5647 import PACKAGE as ov5647
+from .rpi_cam_2 import PACKAGE as rpi_cam_2
+from .usb_pinhole import PACKAGE as usb_pinhole
+
