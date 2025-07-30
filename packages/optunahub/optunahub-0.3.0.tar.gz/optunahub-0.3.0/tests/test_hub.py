10d126350b02a9c154c72a5a9e1dfdb34e631fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:12:18 +0000

    Add package optunahub/0.3.0/optunahub-0.3.0.tar.gz

---
+++ packages/optunahub/optunahub-0.3.0.tar.gz/optunahub-0.3.0/tests/test_hub.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import os
+import shutil
+
+import optuna
+import pytest
+from pytest import MonkeyPatch
+
+import optunahub
+from optunahub.hub import _extract_hostname
+
+
+@pytest.mark.parametrize("git_command", ["/usr/bin/git", None])
+def test_load_module(monkeypatch: MonkeyPatch, git_command: str | None) -> None:
+    def objective(trial: optuna.Trial) -> float:
+        x = trial.suggest_float("x", 0, 1)
+
+        return x
+
+    monkeypatch.setattr(shutil, "which", lambda cmd: git_command)
+    m = optunahub.load_module("samplers/simulated_annealing", force_reload=True)
+    assert m.__name__ == "optunahub_registry.package.samplers.simulated_annealing"
+
+    # Confirm no error occurs by running optimization
+    sampler = m.SimulatedAnnealingSampler()
+    study = optuna.create_study(sampler=sampler)
+    study.optimize(objective, n_trials=10)
+
+
+def test_load_local_module() -> None:
+    def objective(trial: optuna.Trial) -> float:
+        x = trial.suggest_float("x", 0, 1)
+
+        return x
+
+    m = optunahub.load_local_module(
+        "package_for_test_hub",
+        registry_root=os.path.dirname(__file__),
+    )
+    assert m.__name__ == "optunahub_registry.package.package_for_test_hub"
+
+    # Confirm no error occurs by running optimization
+    sampler = m.TestSampler()
+    study = optuna.create_study(sampler=sampler)
+    study.optimize(objective, n_trials=10)
+
+
+@pytest.mark.parametrize(
+    "uri, expected_hostname",
+    [
+        ("git@github.com", "github.com"),
+        ("git@gitlab.example.com", "gitlab.example.com"),
+        ("https://api.github.com", "api.github.com"),
+        ("https://gitlab.example.com/api/v4/", "gitlab.example.com"),
+    ],
+)
+def test_extract_hostname(uri: str, expected_hostname: str) -> None:
+    assert _extract_hostname(uri) == expected_hostname
