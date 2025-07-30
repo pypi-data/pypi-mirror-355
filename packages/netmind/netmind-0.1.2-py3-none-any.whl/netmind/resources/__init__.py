a3ce10df7693a6ac6bebf6b7202f0f3183c54be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:59 +0000

    Add package netmind/0.1.2/netmind-0.1.2-py3-none-any.whl

---
+++ packages/netmind/netmind-0.1.2-py3-none-any.whl/netmind/resources/__init__.py
@@ -0,0 +1,13 @@
+from netmind.resources.chat import Chat, AsyncChat
+from netmind.resources.embeddings import Embeddings, AsyncEmbeddings
+from netmind.resources.files import Files, AsyncFiles
+
+
+__all__ = [
+    "Chat",
+    "AsyncChat",
+    "Embeddings",
+    "AsyncEmbeddings",
+    "Files",
+    "AsyncFiles",
+]
