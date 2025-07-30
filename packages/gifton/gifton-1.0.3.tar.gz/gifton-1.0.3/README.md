f9aed22627b41188ca4b64fb1f96034829d9a0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:14 +0000

    Add package gifton/1.0.3/gifton-1.0.3.tar.gz

---
+++ packages/gifton/gifton-1.0.3.tar.gz/gifton-1.0.3/README.md
@@ -0,0 +1,3 @@
+# gifton
+
+This is a library with encryption. I created it to surprise my friend for his birthday. Please don't delete it.
---
+++ packages/gifton/gifton-1.0.3.tar.gz/gifton-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gifton",
+    version="1.0.3",
+    description="This is a library with encryption. I created it to surprise my friend for his birthday. Please don't delete it.",
+    author="eropens",
+    author_email="eropn313@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6",
+)
