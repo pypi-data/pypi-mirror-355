440ffefecd0ff7916b57c35cb2a367fe33717752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:20 +0000

    Add package LCPDelta/2.1.8/lcpdelta-2.1.8.tar.gz

---
+++ packages/LCPDelta/lcpdelta-2.1.8.tar.gz/lcpdelta-2.1.8/lcpdelta_python_package/src/lcp_delta/common/constants.py
@@ -0,0 +1,6 @@
+MAIN_BASE_URL = "https://enactapifd.lcp.uk.com"
+MAIN_BASE_URL_DEV = "https://enact-backupapi-one.lcp.uk.com"
+
+EPEX_BASE_URL = "https://enact-epex.azurefd.net"
+SERIES_BASE_URL = "https://enact-functionapp-siteapi.azurewebsites.net"
+SERIES_BASE_URL_DEV = "https://enact-functionapp-siteapi-staging.azurewebsites.net"
