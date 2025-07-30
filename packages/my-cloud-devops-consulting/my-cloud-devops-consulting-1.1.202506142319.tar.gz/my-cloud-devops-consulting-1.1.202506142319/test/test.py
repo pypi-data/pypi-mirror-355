6192b972f089f4a87c35fc853efbfdc8e0cba58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:29 +0000

    Add package my-cloud-devops-consulting/1.1.202506142319/my-cloud-devops-consulting-1.1.202506142319.tar.gz

---
+++ packages/my-cloud-devops-consulting/my-cloud-devops-consulting-1.1.202506142319.tar.gz/my-cloud-devops-consulting-1.1.202506142319/test/test.py
@@ -0,0 +1,7 @@
+# Monthly_Income = int(input ("  Monthly Income ?  "))
+# Rent = int( input (" rent   "))
+# Groceries = int(input (" Groceries ") )
+# Transportation = int(input (" Transportation "))
+# Money_left_after_expenses= Monthly_Income - Rent -Groceries -Transportation
+# print( "They are left with ",Money_left_after_expenses)
+print("HELLLO "," Love",sep="\U0001F496")
