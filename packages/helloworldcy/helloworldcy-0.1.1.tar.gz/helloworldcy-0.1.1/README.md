d6b70d7f7b1a0c04434b7897672e99dd94eb7a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:15 +0000

    Add package helloworldcy/0.1.1/helloworldcy-0.1.1.tar.gz

---
+++ packages/helloworldcy/helloworldcy-0.1.1.tar.gz/helloworldcy-0.1.1/README.md
@@ -0,0 +1,3 @@
+# HelloWorldCy
+
+Una librería mínima en Cython que imprime "Hello, World!" y se actualiza automáticamente.
---
+++ packages/helloworldcy/helloworldcy-0.1.1.tar.gz/helloworldcy-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="helloworldcy",
+    version="0.1.1",
+    packages=["helloworldcy"],
+    ext_modules=cythonize("helloworldcy/hello.pyx"),
+    zip_safe=False,
+)
