4c8b20d17073e841b9523fb8fbc998deb493b613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:00 +0000

    Add package face-mask-sdk/0.1.0/face_mask_sdk-0.1.0-py3-none-any.whl

---
+++ packages/face-mask-sdk/face_mask_sdk-0.1.0-py3-none-any.whl/sdk/config.py
@@ -0,0 +1,13 @@
+BRIGHTNESS_FACTOR = 1.5
+DARKEN_FACTOR = 0.7
+STABLE_FRAMES_THRESHOLD = 3
+SHARPNESS_THRESHOLD = 50
+MOTION_BLUR_THRESHOLD = 100
+
+COLORS = {
+    'no_mask': (0, 255, 0),
+    'mask': (0, 0, 255),
+    'warning': (0, 255, 255),
+    'info': (255, 255, 255),
+    'stable': (255, 0, 0)
+}
