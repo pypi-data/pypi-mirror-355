6221db98dbf75705318262d55d1927019e5371f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:12 +0000

    Add package csv-to-sql-dump/0.0.8/csv_to_sql_dump-0.0.8.tar.gz

---
+++ packages/csv-to-sql-dump/csv_to_sql_dump-0.0.8.tar.gz/csv_to_sql_dump-0.0.8/dump.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+
+DROP TABLE IF EXISTS dogs;
+CREATE TABLE dogs (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `age` INT,
+    `gender` VARCHAR(255),
+    `breed` VARCHAR(255),
+    `dob` DATETIME
+);
+
+INSERT INTO dogs (`name`, `age`, `gender`, `breed`, `dob`) VALUES
+    ('rocky', 4, 'male', 'german shepherd', '2025-01-15'),
+    ('fifi', 5, 'female', 'german shepherd', '2023-09-09'),
+    ('baaron', 6, 'male', NULL, '2017-07-31')
+;
