9d79ff25ab3adb84e6100d17695ea7f614a5c160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:21 +0000

    Add package attoworld/2025.0.24/attoworld-2025.0.24-cp39-cp39-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/attoworld/attoworld-2025.0.24-cp39-cp39-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/attoworld/file/__init__.py
@@ -0,0 +1,22 @@
+"""
+This module will contain functions for loading the various data formats used across the labs.
+"""
+
+from .file_io import load_waves_from_matfile, load_waveform_from_text, load_spectrum_from_text
+from .FROG_result import FrogResult, read_Trebino_FROG_matrix, read_Trebino_FROG_speck, read_Trebino_FROG_data
+from .interface_simulations import LunaResult
+from .profileAndIntensity import profile_analysis
+from .dataclasses import Waveform, ComplexSpectrum, IntensitySpectrum, ComplexEnvelope
+__all__ = ["load_waves_from_matfile",
+    "load_waveform_from_text",
+    "load_spectrum_from_text",
+    "FrogResult",
+    "read_Trebino_FROG_matrix",
+    "read_Trebino_FROG_speck",
+    "read_Trebino_FROG_data",
+    "LunaResult",
+    "profile_analysis",
+    "Waveform",
+    "ComplexSpectrum",
+    "IntensitySpectrum",
+    "ComplexEnvelope"]
---
+++ packages/attoworld/attoworld-2025.0.24-cp39-cp39-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/attoworld/personal/nick/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file contains functions primarily used by Nick Karpowicz.
+"""
+
+from .nick import dark_plot
+
+__all__ = ["dark_plot"]
