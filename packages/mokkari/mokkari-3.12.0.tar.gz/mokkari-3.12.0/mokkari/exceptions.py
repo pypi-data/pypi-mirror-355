e23543d4088e75e7c734482ab6bae11261517225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:47 +0000

    Add package mokkari/3.12.0/mokkari-3.12.0.tar.gz

---
+++ packages/mokkari/mokkari-3.12.0.tar.gz/mokkari-3.12.0/mokkari/exceptions.py
@@ -0,0 +1,34 @@
+"""Exceptions module.
+
+This module provides the following classes:
+
+- ApiError
+- AuthenticationError
+- CacheError
+"""
+
+from __future__ import annotations
+
+
+class ApiError(Exception):
+    """Class for any api errors."""
+
+    def __init__(self: ApiError, *args, **kwargs: dict[str, any]) -> None:
+        """Initialize an ApiError."""
+        Exception.__init__(self, *args, **kwargs)
+
+
+class AuthenticationError(Exception):
+    """Class for any authentication errors."""
+
+    def __init__(self: AuthenticationError) -> None:
+        """Initialize the Authentication error."""
+        Exception.__init__(self, "Missing authorization information")
+
+
+class CacheError(Exception):
+    """Class for any database cache errors."""
+
+    def __init__(self: CacheError, *args, **kwargs: dict[str, any]) -> None:
+        """Initialize an CacheError."""
+        Exception.__init__(self, *args, **kwargs)
