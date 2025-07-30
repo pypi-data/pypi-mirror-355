413253eb6955eb9823400dc0bdd2ef875c1c2f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:01 +0000

    Add package protspace/2.0.1/protspace-2.0.1-py3-none-any.whl

---
+++ packages/protspace/protspace-2.0.1-py3-none-any.whl/protspace/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "2.0.1"
+
+from .utils import add_feature_style, prepare_json
+
+__all__ = ["prepare_json", "add_feature_style"]
+
+try:
+    from . import app, main
+
+    __all__.extend(["main", "app"])
+except ImportError:
+    # If the web frontend is needed, please install it, e.g. via `uv sync --all-extras`
+    pass
