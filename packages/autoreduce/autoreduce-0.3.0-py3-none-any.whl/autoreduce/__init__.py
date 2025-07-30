0cfa978b0d87225b4b83c0e1435002bec6fe0980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:39 +0000

    Add package autoreduce/0.3.0/autoreduce-0.3.0-py3-none-any.whl

---
+++ packages/autoreduce/autoreduce-0.3.0-py3-none-any.whl/autoreduce/__init__.py
@@ -0,0 +1,6 @@
+"""AutoReduce: Automated model reduction tools for SBML models."""
+
+__version__ = "0.3.0"
+
+from .system import System  # noqa: F401
+from .converters import load_sbml, load_ODE_model  # noqa: F401
