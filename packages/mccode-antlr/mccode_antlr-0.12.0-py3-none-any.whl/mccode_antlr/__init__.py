feda4bb455b8ccd30d4ea5f23c98040345b37cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:57 +0000

    Add package mccode-antlr/0.12.0/mccode_antlr-0.12.0-py3-none-any.whl

---
+++ packages/mccode-antlr/mccode_antlr-0.12.0-py3-none-any.whl/mccode_antlr/__init__.py
@@ -0,0 +1,9 @@
+"""Monte Carlo Particle Ray Tracing compiler, Volume 4"""
+import mccode_antlr.grammar
+__author__ = "Gregory Tucker"
+__affiliation__ = "European Spallation Source ERIC"
+
+from .version import version
+__version__ = version()
+
+__all__ = ["__author__", "__affiliation__", "__version__", "version"]
