12481172a072b566648aeea7f7fcf7f59a8fab73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:05 +0000

    Add package graphex/3.5.7/graphex-3.5.7-py3-none-any.whl

---
+++ packages/graphex/graphex-3.5.7-py3-none-any.whl/graphex/algorithms/isomorphism/__init__.py
@@ -0,0 +1,7 @@
+from graphex.algorithms.isomorphism.isomorph import *
+from graphex.algorithms.isomorphism.vf2userfunc import *
+from graphex.algorithms.isomorphism.matchhelpers import *
+from graphex.algorithms.isomorphism.temporalisomorphvf2 import *
+from graphex.algorithms.isomorphism.ismags import *
+from graphex.algorithms.isomorphism.tree_isomorphism import *
+from graphex.algorithms.isomorphism.vf2pp import *
---
+++ packages/graphex/graphex-3.5.7-py3-none-any.whl/graphex/classes/tests/test_graph_historical.py
@@ -0,0 +1,12 @@
+"""Original NetworkX graph tests"""
+
+import graphex as nx
+
+from .historical_tests import HistoricalTests
+
+
+class TestGraphHistorical(HistoricalTests):
+    @classmethod
+    def setup_class(cls):
+        HistoricalTests.setup_class()
+        cls.G = nx.Graph
