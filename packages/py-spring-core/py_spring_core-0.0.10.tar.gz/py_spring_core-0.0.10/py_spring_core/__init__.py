96729d6e102a109d97af3037bfe44bf80b78147e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:11:47 +0000

    Add package py-spring-core/0.0.10/py_spring_core-0.0.10.tar.gz

---
+++ packages/py-spring-core/py_spring_core-0.0.10.tar.gz/py_spring_core-0.0.10/py_spring_core/__init__.py
@@ -0,0 +1,15 @@
+from py_spring_core.core.application.py_spring_application import PySpringApplication
+from py_spring_core.core.entities.bean_collection import BeanCollection
+from py_spring_core.core.entities.component import Component, ComponentScope
+from py_spring_core.core.entities.controllers.rest_controller import RestController
+from py_spring_core.core.entities.controllers.route_mapping import (
+    DeleteMapping,
+    GetMapping,
+    PatchMapping,
+    PostMapping,
+    PutMapping,
+)
+from py_spring_core.core.entities.entity_provider import EntityProvider
+from py_spring_core.core.entities.properties.properties import Properties
+
+__version__ = "0.0.10"
