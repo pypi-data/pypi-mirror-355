577c23e979e04ad5416227bb57d73dfc99f4386f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:08 +0000

    Add package aio-fluid/1.5.2/aio_fluid-1.5.2-py3-none-any.whl

---
+++ packages/aio-fluid/aio_fluid-1.5.2-py3-none-any.whl/fluid/utils/dates.py
@@ -0,0 +1,25 @@
+from datetime import date, datetime, timezone
+from typing import Any
+from zoneinfo import ZoneInfo
+
+UTC = ZoneInfo("UTC")
+
+
+def utcnow() -> datetime:
+    return datetime.now(tz=UTC)
+
+
+def as_utc(dt: date | None) -> datetime:
+    if dt is None:
+        return utcnow()
+    elif isinstance(dt, datetime):
+        return dt.replace(tzinfo=UTC)
+    else:
+        return datetime(dt.year, dt.month, dt.day, tzinfo=UTC)
+
+
+def isoformat(dt: datetime, **kwargs: Any) -> str:
+    if dt.tzinfo is timezone.utc:
+        return dt.replace(tzinfo=None).isoformat(**kwargs) + "Z"
+    else:
+        return dt.isoformat(**kwargs)
