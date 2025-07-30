4ac0306e463f5849e8631762e713ec7f750a5ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:22 +0000

    Add package chalk-sqlalchemy-redshift/0.8.15.dev0/chalk_sqlalchemy_redshift-0.8.15.dev0-py3-none-any.whl

---
+++ packages/chalk-sqlalchemy-redshift/chalk_sqlalchemy_redshift-0.8.15.dev0-py3-none-any.whl/sqlalchemy_redshift/__init__.py
@@ -0,0 +1,32 @@
+from pkg_resources import DistributionNotFound, get_distribution, parse_version
+
+for package in ['psycopg2', 'psycopg2-binary', 'psycopg2cffi']:
+    try:
+        if get_distribution(package).parsed_version < parse_version('2.5'):
+            raise ImportError('Minimum required version for psycopg2 is 2.5')
+        break
+    except DistributionNotFound:
+        pass
+
+# Set version statically to avoid import issues during build
+__version__ = '0.8.15.dev0'
+
+from sqlalchemy.dialects import registry  # noqa
+
+registry.register(
+    "redshift", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_psycopg2"
+)
+registry.register(
+    "redshift.psycopg2", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_psycopg2"
+)
+registry.register(
+    'redshift+psycopg2cffi', 'sqlalchemy_redshift.dialect',
+    'RedshiftDialect_psycopg2cffi',
+)
+
+registry.register(
+    "redshift+redshift_connector", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_redshift_connector"
+)
