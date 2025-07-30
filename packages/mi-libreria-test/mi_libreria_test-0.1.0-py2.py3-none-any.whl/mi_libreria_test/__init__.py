fe1dc136a9773fb24c1dcaaf7335154b02694f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:20 +0000

    Add package mi-libreria-test/0.1.0/mi_libreria_test-0.1.0-py2.py3-none-any.whl

---
+++ packages/mi-libreria-test/mi_libreria_test-0.1.0-py2.py3-none-any.whl/mi_libreria_test/__init__.py
@@ -0,0 +1,10 @@
+#write a function that returns the sum of two numbers
+def suma(a, b):
+    """
+    Suma dos números y devuelve el resultado.
+
+    :param a: Primer número.
+    :param b: Segundo número.
+    :return: La suma de a y b.
+    """
+    return a + b
