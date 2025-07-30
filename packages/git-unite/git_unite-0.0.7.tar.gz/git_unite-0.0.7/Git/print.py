ee174bac419b4cfdbaf6b16e166509bfd1b5313a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:51 +0000

    Add package git-unite/0.0.7/git_unite-0.0.7.tar.gz

---
+++ packages/git-unite/git_unite-0.0.7.tar.gz/git_unite-0.0.7/Git/print.py
@@ -0,0 +1,44 @@
+import click
+
+
+
+def Print_Header(tag):
+	header = {
+		'wd:dirty': {'text': 'Working directory not clean', 'fg': 167,},
+		'commit:ahead': {'text': 'Push pending', 'fg': 167,},
+		'remote:none': {'text': 'No remote', 'fg': 202,},
+		'branch:other': {'text': 'Branch: Other', 'fg': 70,},
+		'branch:development': {'text': 'Branch: Development', 'fg': 70,},
+	}
+
+	message = click.style(**header[tag], bold=False)
+	click.echo(message)
+
+
+def Print_Section(text):
+	click.echo(click.style(text, fg='yellow'))
+
+
+def Print_Repository(instance):
+	message = click.style(f'\t{instance.path}')
+	click.echo(message)
+
+
+def Print_Path(instance):
+	message = click.style(f'\t{instance}')
+	click.echo(message)
+
+
+def Print_Warning(text):
+	message = click.style(text, fg='yellow')
+	click.echo(message)
+
+def Print_Info(text):
+	click.echo(click.style(text, fg='green'))
+
+def Print_Empty():
+	click.echo('')
+
+def Print_Action(action, item):
+	message = click.style(action) + ' ' + click.style(item, fg='green')
+	click.echo(message)
