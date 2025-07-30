687c1075a8af4d1882ca8480a2769242603ccea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:59 +0000

    Add package hyapp/5.3.0/hyapp-5.3.0.tar.gz

---
+++ packages/hyapp/hyapp-5.3.0.tar.gz/hyapp-5.3.0/src/hyapp/traces.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import uuid
+from contextvars import ContextVar
+
+# See also: https://github.com/snok/asgi-correlation-id
+TRACE_ID_VAR: ContextVar[str | None] = ContextVar("trace_id", default=None)
+
+
+def new_trace_id(
+    subprefix: str, prefix: str = "", total_len: int = 16, parent: str | None = None, parent_sep: str = "__"
+) -> str:
+    uuid_len = total_len - len(prefix) - len(subprefix)
+    uuid_val = uuid.uuid4().hex[:uuid_len]  # This could be simple `getrandbits` though.
+    result = f"{prefix}{subprefix}{uuid_val}"
+    if parent:
+        result = f"{parent}{parent_sep}{result}"
+    return result
