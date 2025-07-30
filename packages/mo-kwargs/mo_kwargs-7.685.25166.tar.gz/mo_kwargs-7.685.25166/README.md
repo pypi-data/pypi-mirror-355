1f272edee6a320e254ddea926801b32998124318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:11 +0000

    Add package mo-kwargs/7.685.25166/mo_kwargs-7.685.25166.tar.gz

---
+++ packages/mo-kwargs/mo_kwargs-7.685.25166.tar.gz/mo_kwargs-7.685.25166/README.md
@@ -0,0 +1,109 @@
+
+# More KWARGS!
+
+Object destructuring of function parameters for Python!
+
+[![PyPI Latest Release](https://img.shields.io/pypi/v/mo-kwargs.svg)](https://pypi.org/project/mo-kwargs/)
+ [![Build Status](https://github.com/klahnakoski/mo-kwargs/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/klahnakoski/mo-kwargs/actions/workflows/build.yml)
+ [![Coverage Status](https://coveralls.io/repos/github/klahnakoski/mo-kwargs/badge.svg?branch=dev)](https://coveralls.io/github/klahnakoski/mo-kwargs?branch=dev)
+[![Downloads](https://static.pepy.tech/badge/mo-kwargs)](https://pepy.tech/project/mo-kwargs)
+
+
+## Motivation
+
+Javascript has [object destructuring](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring), and it can be used for function parameters. This has a couple of benefts over Python's keyword arguments:
+
+* Extra caller parameters are ignored (eg `f({a, b, c})`)
+* Duplicate parameters are handled elegantly (eg `f({a, a})`) 
+
+The `mo-kwargs` library provides this functionality with the `@override` decorator, with additional benefits:
+ 
+ * required parameters throw an error if missing, just like regular Python
+ * all parameters, even ones not in the argument list, are passed in the optional `kwargs` parameter 
+ 
+The `@override` decorator adds a `kwargs` argument which can be passed a dict of call parameters; but unlike `**kwargs`, it will not raise duplicate key exceptions.
+
+## Provide default values
+
+We decorate the `login()` function with `@override`. `username` is a required parameter, and `password` will default to `None`. 
+
+        @override
+        def login(username, password=None):
+            pass
+
+Define some `dicts` for use with our `kwargs` parameter:
+
+        creds = {"userame": "ekyle", "password": "password123"}
+        alt_creds = {"username": "klahnakoski"}
+
+
+The simplest case is when we use `kwargs` with no overrides
+
+        login(kwargs=creds)
+        # SAME AS
+        login(**creds)
+        # SAME AS
+        login(username="ekyle", password="password123")
+
+You may override any property in `kwargs`: In this case it is `password`
+
+        login(password="123", kwargs=creds)
+        # SAME AS
+        login(username="ekyle", password="123")
+
+There is no problem with overriding everything in `kwargs`:
+
+        login(username="klahnakoski", password="asd213", kwargs=creds)
+        # SAME AS
+        login(username="klahnakoski", password="asd213")
+
+You may continue to use `**kwargs`; which provides a way to overlay one parameter template (`creds`) with another (`alt_creds`)
+
+        login(kwargs=creds, **alt_creds)
+        # SAME AS
+        login(username="klahnakoski", password="password123")
+
+## Handle too many parameters
+
+Sometimes your method parameters come from a configuration file, or some other outside source which is outside your control. There may be more parameters than your method is willing to accept.  
+
+        creds = {"username": "ekyle", "password": "password123", "port":9000}
+        def login(username, password=None):
+             print(kwargs.get("port"))
+
+Without `mo-kwargs`, passing the `creds` dictionary directly to `login()` would raise a key error
+
+        >>> login(**creds)
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        TypeError: login() got an unexpected keyword argument 'port'
+            
+The traditional solution is to pass the parameters explicitly:
+
+        login(username=creds.username, password=creds.password)
+
+but that can get get tedious when done often, or the parameter list get long. `mo-kwargs` allows you to pass the whole dictionary to the `kwargs` parameter; only the parameters used by the method are used:
+
+        @override
+        def login(username, password=None):
+            pass
+         
+        login(kwargs=creds)
+        # SAME AS
+        login(username=creds.username, password=creds.password)
+
+## Package all parameters
+
+Your method can accept `kwargs` as a parameter. If it does, ensure it defaults to `None` so that it's not required.
+
+        @override
+        def login(username, password=None, kwargs=None):
+            print(kwargs.get("username"))
+            print(kwargs.get("port"))
+
+`kwargs` will always be a dict, possibly empty, with the full set of parameters. This is different from using `**kwargs` which contains only the remainder of the keyword parameters.
+
+        >>> creds = {"username": "ekyle", "password": "password123", "port":9000}
+        >>> login(**creds)
+        ekyle
+        9000
---
+++ packages/mo-kwargs/mo_kwargs-7.685.25166.tar.gz/mo_kwargs-7.685.25166/mo_kwargs/__init__.py
@@ -0,0 +1,192 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://www.mozilla.org/en-US/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+
+import sys
+from functools import update_wrapper
+
+from mo_dots import get_logger, is_data, to_data, is_many
+
+KWARGS = str("kwargs")
+
+
+def override(kwargs=None):
+    """
+    :param kwargs: Alternative argument name that will receive all parameters
+
+    THIS DECORATOR WILL PUT ALL PARAMETERS INTO THE `kwargs` ARGUMENT AND
+    THEN PUT ALL `kwargs` PARAMETERS INTO THE FUNCTION PARAMETERS. THIS HAS
+    THE BENEFIT OF HAVING ALL PARAMETERS IN ONE PLACE (kwargs), PLUS ALL
+    PARAMETERS ARE EXPLICIT FOR CLARITY.
+
+    OF COURSE, THIS MEANS PARAMETER ASSIGNMENT MAY NOT BE UNIQUE: PARAMETER
+    VALUES ARE CHOSEN IN THE FOLLOWING ORDER:
+    1) EXPLICT CALL PARAMETERS
+    2) PARAMETERS FOUND IN `kwargs`
+    3) DEFAULT VALUES ASSIGNED IN FUNCTION DEFINITION
+    """
+
+    def output(func):
+        func_name = func.__name__
+        known_kwargs = get_function_arguments(func)[: func.__code__.co_argcount + func.__code__.co_kwonlyargcount]
+        known_args = known_kwargs[: func.__code__.co_argcount]
+        defaults = {k: v for k, v in zip(reversed(known_kwargs), reversed(func.__defaults__ or [])) if v is not None}
+
+        def raise_error(e, a, k):
+            packed = k.copy()
+            packed.update(dict(zip(known_kwargs, a)))
+            err = str(e)
+            if func_name in err and (
+                "takes at least" in err or "takes exactly " in err or "required positional argument" in err
+            ):
+                missing = [p for p in known_kwargs if str(p) not in packed]
+                given = [p for p in known_kwargs if str(p) in packed]
+                if not missing:
+                    raise e
+                else:
+                    get_logger().error(
+                        "Problem calling {func_name}:  Expecting parameter {missing}, given {given}",
+                        func_name=func_name,
+                        missing=missing,
+                        given=given,
+                        stack_depth=2,
+                        cause=e,
+                    )
+            raise e
+
+        if kwargs not in known_kwargs:
+            # ADDING A kwargs PARAMETER TO SOME REGULAR METHOD
+            def wo_kwargs(*given_args, **given_kwargs):
+                settings = given_kwargs.get(kwargs, {})
+                ordered_params = dict(zip(known_args, given_args))
+                a, k = params_pack(known_kwargs, defaults, settings, given_kwargs, ordered_params)
+                try:
+                    return func(*a, **k)
+                except TypeError as e:
+                    raise_error(e, a, k)
+
+            return update_wrapper(wo_kwargs, func)
+
+        elif func_name in ("__init__", "__new__") or known_kwargs[0] in ("self", "cls"):
+
+            def w_bound_method(*given_args, **given_kwargs):
+                if len(given_args) == 2 and len(given_kwargs) == 0 and is_data(given_args[1]):
+                    # ASSUME SECOND UNNAMED PARAM IS kwargs
+                    a, k = params_pack(
+                        known_kwargs, defaults, given_args[1], {known_kwargs[0]: given_args[0]}, given_kwargs,
+                    )
+                elif kwargs in given_kwargs and is_data(given_kwargs[kwargs]):
+                    # PUT args INTO given_kwargs
+                    a, k = params_pack(
+                        known_kwargs, defaults, given_kwargs[kwargs], dict(zip(known_args, given_args)), given_kwargs,
+                    )
+                else:
+                    a, k = params_pack(known_kwargs, defaults, dict(zip(known_args, given_args)), given_kwargs)
+                try:
+                    return func(*a, **k)
+                except TypeError as e:
+                    tb = getattr(e, "__traceback__", None)
+                    if tb is not None:
+                        trace = _parse_traceback(tb)
+                    else:
+                        trace = get_traceback(0)
+                    raise_error(e, a, k)
+
+            return update_wrapper(w_bound_method, func)
+
+        else:
+
+            def w_kwargs(*given_args, **given_kwargs):
+                if len(given_args) == 1 and len(given_kwargs) == 0 and is_data(given_args[0]):
+                    # ASSUME SINGLE PARAMETER IS kwargs
+                    a, k = params_pack(known_kwargs, defaults, given_args[0])
+                elif kwargs in given_kwargs and is_data(given_kwargs[kwargs]):
+                    # PUT given_args INTO given_kwargs
+                    a, k = params_pack(
+                        known_kwargs, defaults, given_kwargs[kwargs], dict(zip(known_args, given_args)), given_kwargs,
+                    )
+                else:
+                    # PULL kwargs OUT INTO PARAMS
+                    a, k = params_pack(known_kwargs, defaults, dict(zip(known_args, given_args)), given_kwargs)
+                try:
+                    return func(*a, **k)
+                except TypeError as e:
+                    raise_error(e, a, k)
+
+            return update_wrapper(w_kwargs, func)
+
+    def params_pack(params, *args):
+        """
+        :param params:
+        :param args:
+        :return: (args, kwargs) pair
+        """
+        settings = {}
+        for a in args:
+            for k, v in a.items():
+                settings[str(k)] = v
+        settings[kwargs] = to_data(settings)
+
+        if params and params[0] in ("self", "cls"):
+            s = settings.get(params[0])
+            if s is None:
+                return (
+                    [],
+                    {k: settings[k] for k in params[1:] if k in settings},
+                )
+            else:
+                return (
+                    [s],
+                    {k: settings[k] for k in params[1:] if k in settings},
+                )
+        else:
+            return ([], {k: settings[k] for k in params if k in settings})
+
+    if isinstance(kwargs, str):
+        # COMPLEX VERSION @override(kwargs="other")
+        return output
+    elif kwargs == None:
+        raise NotImplementedError("use @override without calling")
+    else:
+        # SIMPLE VERSION @override
+        func, kwargs = kwargs, KWARGS
+        return output(func)
+
+
+def get_traceback(start):
+    """
+    SNAGGED FROM traceback.py
+
+    RETURN list OF dicts DESCRIBING THE STACK TRACE
+    """
+    tb = sys.exc_info()[2]
+    for i in range(start):
+        tb = tb.tb_next
+    return _parse_traceback(tb)
+
+
+def _parse_traceback(tb):
+    if is_many(tb):
+        get_logger().error("Expecting a tracback object, not a list")
+    trace = []
+    while tb is not None:
+        f = tb.tb_frame
+        trace.append({
+            "file": f.f_code.co_filename,
+            "line": tb.tb_lineno,
+            "method": f.f_code.co_name,
+        })
+        tb = tb.tb_next
+    trace.reverse()
+    return trace
+
+
+def get_function_arguments(func):
+    return func.__code__.co_varnames
