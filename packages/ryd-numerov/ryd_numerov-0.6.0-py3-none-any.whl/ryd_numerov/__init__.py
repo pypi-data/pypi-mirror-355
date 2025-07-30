e20f02ccdaecb4f398c7038301841c90427abf45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:51 +0000

    Add package ryd-numerov/0.6.0/ryd_numerov-0.6.0-py3-none-any.whl

---
+++ packages/ryd-numerov/ryd_numerov-0.6.0-py3-none-any.whl/ryd_numerov/__init__.py
@@ -0,0 +1,15 @@
+from ryd_numerov import angular, elements, model, radial
+from ryd_numerov.rydberg import RydbergState
+from ryd_numerov.units import ureg
+
+__all__ = [
+    "RydbergState",
+    "angular",
+    "elements",
+    "model",
+    "radial",
+    "ureg",
+]
+
+
+__version__ = "0.6.0"
