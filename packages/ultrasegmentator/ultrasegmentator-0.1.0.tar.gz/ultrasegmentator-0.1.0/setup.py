83c553be940c8721347da577d5acefae3e797bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:08 +0000

    Add package ultrasegmentator/0.1.0/ultrasegmentator-0.1.0.tar.gz

---
+++ packages/ultrasegmentator/ultrasegmentator-0.1.0.tar.gz/ultrasegmentator-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ultrasegmentator",  # PyPI 上的包名（必须唯一）
+    py_modules=["ultrasegmentator"],  # 模块名
+    version="0.1.0",  # 版本号
+    author="weki",
+    python_requires="==3.10.*",  # 严格限定Python 3.10
+    install_requires=[
+        "torch==2.6.*",  # 限定torch 2.6系列
+        "numpy==2.1.*",  # 限定numpy 2.1系列
+        "tqdm",
+        "acvl_utils",
+        "batchgenerators",
+        "nibabel",
+        "dynamic_network_architectures",
+        "batchgeneratorsv2",
+        "matplotlib",
+        "seaborn"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
