692fe8a46cbf12f743743695a5221ac9cfb23b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:39 +0000

    Add package bluer-ugv/6.154.1/bluer_ugv-6.154.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.154.1-py3-none-any.whl/bluer_ugv/__init__.py
@@ -0,0 +1,17 @@
+NAME = "bluer_ugv"
+
+ICON = "🐬"
+
+DESCRIPTION = f"{ICON} AI x ROS."
+
+VERSION = "6.154.1"
+
+REPO_NAME = "bluer-ugv"
+
+MARQUEE = "https://raw.githubusercontent.com/kamangir/assets2/refs/heads/main/bluer-swallow/20250616_134654.jpg"
+
+ALIAS = "@ugv"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
---
+++ packages/bluer-ugv/bluer_ugv-6.154.1-py3-none-any.whl/bluer_ugv/swallow/session/classical/keyboard.py
@@ -0,0 +1,73 @@
+import keyboard
+
+from bluer_sbc.session.functions import reply_to_bash
+
+from bluer_ugv.swallow.session.classical.setpoint import ClassicalSetPoint
+from bluer_ugv.logger import logger
+
+bash_keys = {
+    "i": "exit",
+    "n": "shutdown",
+    "t": "reboot",
+    "u": "update",
+}
+
+
+class ClassicalKeyboard:
+    def __init__(
+        self,
+        setpoint: ClassicalSetPoint,
+    ):
+        logger.info(
+            "{}: {}".format(
+                self.__class__.__name__,
+                ", ".join(
+                    [f"{key}:{action}" for key, action in bash_keys.items()],
+                ),
+            )
+        )
+
+        self.setpoint = setpoint
+
+    def update(self) -> bool:
+        for key, event in bash_keys.items():
+            if keyboard.is_pressed(key):
+                reply_to_bash(event)
+                return False
+
+        if keyboard.is_pressed(" "):
+            self.setpoint.stop()
+
+        if keyboard.is_pressed("x"):
+            self.setpoint.start()
+
+        if keyboard.is_pressed("a"):
+            self.setpoint.put(
+                what="steering",
+                value=20,
+            )
+        elif keyboard.is_pressed("d"):
+            self.setpoint.put(
+                what="steering",
+                value=-20,
+            )
+        else:
+            self.setpoint.put(
+                what="steering",
+                value=0,
+                log=False,
+            )
+
+        if keyboard.is_pressed("s"):
+            self.setpoint.put(
+                what="speed",
+                value=self.setpoint.get(what="speed") - 10,
+            )
+
+        if keyboard.is_pressed("w"):
+            self.setpoint.put(
+                what="speed",
+                value=self.setpoint.get(what="speed") + 10,
+            )
+
+        return True
