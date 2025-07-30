761b4831bdd4f6ca670fee0486f63889df5fb29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:34 +0000

    Add package AeroSandbox/4.2.8/aerosandbox-4.2.8-py3-none-any.whl

---
+++ packages/AeroSandbox/aerosandbox-4.2.8-py3-none-any.whl/aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup/test_basic.py
@@ -0,0 +1,17 @@
+import aerosandbox as asb
+from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import (
+    airplane,
+)
+
+
+def test_aero_buildup():
+    analysis = asb.AeroBuildup(
+        airplane=airplane,
+        op_point=asb.OperatingPoint(),
+    )
+    return analysis.run()
+
+
+if __name__ == "__main__":
+    aero = test_aero_buildup()
+    # pytest.main()
