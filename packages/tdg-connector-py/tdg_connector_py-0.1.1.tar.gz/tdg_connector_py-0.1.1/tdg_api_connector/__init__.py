2a11e093448dad0476b3b78ee6018b0a746a1dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:20 +0000

    Add package tdg-connector-py/0.1.1/tdg_connector_py-0.1.1.tar.gz

---
+++ packages/tdg-connector-py/tdg_connector_py-0.1.1.tar.gz/tdg_connector_py-0.1.1/tdg_api_connector/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'tdg_api_connector'
+__version__ = '0.1.1'
+__author__ = 'Pankaj Gawade'
+
+from .api.append import AppendAPI
+from .api.lookup import LookupAPI
+from .api.verify import VerifyAPI
