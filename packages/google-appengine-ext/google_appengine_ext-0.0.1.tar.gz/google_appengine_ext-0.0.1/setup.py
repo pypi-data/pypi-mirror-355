adc28b38a5753f346a7512db98c7bf430bdcd8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:58 +0000

    Add package google-appengine-ext/0.0.1/google_appengine_ext-0.0.1.tar.gz

---
+++ packages/google-appengine-ext/google_appengine_ext-0.0.1.tar.gz/google_appengine_ext-0.0.1/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='google-appengine-ext',
+    version='0.0.1',
+    description='Security research package for Dependency Confusion PoC',
+    long_description='This is a harmless package used for ethical security testing. If you are seeing this, please notify your internal security team.',
+    long_description_content_type='text/plain',
+    author='Security Researcher',
+    author_email='darshanbughunter@gmail.com',
+    url='http://canarytokens.com/tags/qt4vuhdo3drbmi91225iwz10j/payments.js',
+    packages=['google_appengine_ext'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    zip_safe=False
+)
