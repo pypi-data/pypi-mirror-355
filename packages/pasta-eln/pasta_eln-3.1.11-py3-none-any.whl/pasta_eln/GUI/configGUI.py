86fb1131c1bc01a2a6fb7e22ceab81b59d2074b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:43 +0000

    Add package pasta-eln/3.1.11/pasta_eln-3.1.11-py3-none-any.whl

---
+++ packages/pasta-eln/pasta_eln-3.1.11-py3-none-any.whl/pasta_eln/GUI/configGUI.py
@@ -0,0 +1,60 @@
+""" Main class of config tab on GUI elements """
+import json
+from pathlib import Path
+from typing import Callable
+from PySide6.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout, QGroupBox,  # pylint: disable=no-name-in-module
+                               QVBoxLayout)
+from ..fixedStringsJson import CONF_FILE_NAME, configurationGUI
+from ..guiCommunicate import Communicate
+from ..guiStyle import TextButton, addRowList
+from ..miscTools import restart
+
+
+class ConfigurationGUI(QDialog):
+  """ Main class of config tab on GUI elements """
+  def __init__(self, comm:Communicate, callbackFinished:Callable[[bool],None]):
+    """
+    Initialization
+
+    Args:
+      comm (Communicate): communication
+      callbackFinished (function): callback function to call upon end
+    """
+    super().__init__()
+    self.comm = comm
+    self.callbackFinished = callbackFinished
+    #GUI elements
+    if hasattr(self.comm.backend, 'configuration'):
+      onDisk = self.comm.backend.configuration['GUI']
+      mainL  = QVBoxLayout(self)
+      for label, items  in configurationGUI.items():
+        groupbox = QGroupBox(label.capitalize())
+        mainL.addWidget(groupbox)
+        sectionL = QFormLayout(groupbox)
+        for k,v in items.items():
+          setattr(self, k, addRowList(sectionL, label=v[0], default=str(onDisk[k]), itemList=[str(i) for i in v[2]]))
+          getattr(self, k).setStyleSheet(self.comm.palette.get('secondaryText', 'color'))
+    #final button box
+    buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Save | QDialogButtonBox.StandardButton.Cancel)
+    buttonBox.clicked.connect(self.closeDialog)
+    mainL.addWidget(buttonBox)
+
+
+  def closeDialog(self, btn:TextButton) -> None:
+    """
+    Save changes to hard-disk
+    """
+    if btn.text().endswith('Cancel'):
+      self.reject()
+      self.callbackFinished(False)
+    else:
+      for items in configurationGUI.values():
+        for k in items.keys():
+          try:
+            self.comm.backend.configuration['GUI'][k] = int(getattr(self, k).currentText())
+          except Exception:
+            self.comm.backend.configuration['GUI'][k] = getattr(self, k).currentText()
+      with open(Path.home()/CONF_FILE_NAME, 'w', encoding='utf-8') as fConf:
+        fConf.write(json.dumps(self.comm.backend.configuration,indent=2))
+      restart()
+    return
---
+++ packages/pasta-eln/pasta_eln-3.1.11-py3-none-any.whl/pasta_eln/GUI/details.py
@@ -0,0 +1,211 @@
+""" widget that shows the details of the items """
+import logging
+from enum import Enum
+from pathlib import Path
+from typing import Any
+from PySide6.QtCore import Qt, Slot  # pylint: disable=no-name-in-module
+from PySide6.QtWidgets import QScrollArea, QTextEdit  # pylint: disable=no-name-in-module
+from ..fixedStringsJson import SORTED_DB_KEYS, defaultDataHierarchyNode
+from ..guiCommunicate import Communicate
+from ..guiStyle import IconButton, Image, Label, TextButton, addDocDetails, showMessage, widgetAndLayout
+from ._contextMenu import CommandMenu, executeContextMenu, initContextMenu
+
+
+class Details(QScrollArea):
+  """ widget that shows the details of the items """
+  def __init__(self, comm:Communicate):
+    super().__init__()
+    self.comm = comm
+    comm.changeDetails.connect(self.change)
+    comm.testExtractor.connect(self.testExtractor)
+    self.doc:dict[str,Any]  = {}
+    self.docID= ''
+    self.rescaleTexts:list[QTextEdit] = []
+
+    # GUI elements
+    self.mainW, self.mainL = widgetAndLayout('V', None)
+    self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+    self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+    self.setWidgetResizable(True)
+    self.setWidget(self.mainW)
+
+    headerW, self.headerL = widgetAndLayout('H', self.mainL, spacing='m', top='s', right='s')
+    headerW.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+    headerW.customContextMenuRequested.connect(lambda pos: initContextMenu(self, pos))
+    self.specialW, self.specialL = widgetAndLayout('V', self.mainL, top='s')
+    self.specialW.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+    self.specialW.customContextMenuRequested.connect(lambda pos: initContextMenu(self, pos))
+    self.btnDetails = TextButton('Details', self, [Command.SHOW, 'Details'], self.mainL, \
+                                 'Show / hide details', checkable=True, style='margin-top: 3px')
+    self.metaDetailsW, self.metaDetailsL  = widgetAndLayout('V', self.mainL)
+    self.metaDetailsW.setMaximumWidth(self.width())
+    self.btnVendor = TextButton('Vendor metadata', self, [Command.SHOW, 'Vendor'], self.mainL, \
+                                'Show / hide vendor metadata', checkable=True, style='margin-top: 15px')
+    self.metaVendorW, self.metaVendorL = widgetAndLayout('V', self.mainL)
+    self.metaVendorW.setMaximumWidth(self.width())
+    self.btnUser = TextButton('User metadata', self, [Command.SHOW, 'User'], self.mainL, \
+                              'Show / hide user metadata', checkable=True, style='margin-top: 15px')
+    self.metaUserW, self.metaUserL     = widgetAndLayout('V', self.mainL)
+    self.metaUserW.setMaximumWidth(self.width())
+    self.btnDatabase = TextButton('ELN details', self, [Command.SHOW,'Database'], self.mainL, \
+                                  'Show / hide database details', checkable= True, style='margin-top: 15px')
+    self.metaDatabaseW, self.metaDatabaseL = widgetAndLayout('V', self.mainL)
+    self.metaDatabaseW.setMaximumWidth(self.width())
+    self.mainL.addStretch(1)
+
+
+  @Slot(str)                                # type: ignore[arg-type]
+  def change(self, docID:str) -> None:
+    """
+    What happens when details should change
+
+    Args:
+      docID (str): document-id; '' string=draw nothing; 'redraw' implies redraw
+    """
+    logging.debug('details:changeDetails |%s|',docID)
+    # Delete old widgets from layout
+    for i in reversed(range(self.headerL.count())):
+      aWidget = self.headerL.itemAt(i).widget()
+      if aWidget is not None:
+        aWidget.setParent(None)
+    for i in reversed(range(self.metaDetailsL.count())):
+      self.metaDetailsL.itemAt(i).widget().setParent(None)
+    for i in reversed(range(self.metaVendorL.count())):
+      self.metaVendorL.itemAt(i).widget().setParent(None)
+    for i in reversed(range(self.metaUserL.count())):
+      self.metaUserL.itemAt(i).widget().setParent(None)
+    for i in reversed(range(self.metaDatabaseL.count())):
+      self.metaDatabaseL.itemAt(i).widget().setParent(None)
+    for i in reversed(range(self.specialL.count())):
+      self.specialL.itemAt(i).widget().setParent(None)
+    self.specialW.hide()
+    self.metaDetailsW.hide()
+    self.metaVendorW.hide()
+    self.metaUserW.hide()
+    self.metaDatabaseW.hide()
+    self.btnDetails.setChecked(True)
+    self.btnVendor.setChecked(True)
+    self.btnUser.setChecked(True)
+    self.btnDatabase.setChecked(False)
+    self.rescaleTexts = []
+    if not docID:  #if given '' docID, return
+      return
+    # Create new
+    if docID!='redraw':
+      self.docID = docID
+    if self.docID=='':
+      return
+    self.doc   = self.comm.backend.db.getDoc(self.docID, noError=True)
+    if 'name' not in self.doc:  #keep empty details and wait for user to click
+      self.comm.changeTable.emit('','')
+      return
+    if self.doc['type'][0] not in self.comm.backend.db.dataHierarchy('', ''):
+      dataHierarchyNode = defaultDataHierarchyNode
+    else:
+      dataHierarchyNode = self.comm.backend.db.dataHierarchy(self.doc['type'][0], 'meta')
+    label = self.doc['name'] if len(self.doc['name'])<80 else self.doc['name'][:77]+'...'
+    Label(label,'h1', self.headerL)
+    self.headerL.addStretch(1)
+    IconButton('mdi.file-tree-outline', self, [Command.TO_PROJECT], self.headerL, tooltip='Change to project')
+    IconButton('fa5s.times-circle',     self, [Command.CLOSE],      self.headerL, tooltip='Close')
+    if 'metaVendor' not in self.doc:
+      self.btnVendor.hide()
+    if 'metUser' not in self.doc:
+      self.btnUser.hide()
+    size = self.comm.backend.configuration['GUI']['imageSizeDetails'] \
+                      if hasattr(self.comm.backend, 'configuration') else 300
+    for key in self.doc:
+      if key=='image':
+        Image(self.doc['image'], self.specialL, anyDimension=size)
+        self.specialW.show()
+      elif key=='content':
+        text = QTextEdit()
+        text.setMarkdown(self.doc['content'])
+        text.setFixedHeight(int(size/3*2))
+        text.setReadOnly(True)
+        self.specialL.addWidget(text)
+        self.specialW.show()
+      elif key in ['name']:  #skip
+        continue
+      elif key in SORTED_DB_KEYS:
+        addDocDetails(self, self.metaDatabaseL, key, self.doc[key], dataHierarchyNode)
+        self.btnDatabase.setChecked(False)
+      elif key=='metaVendor':
+        self.btnVendor.show()
+        addDocDetails(self, self.metaVendorL,   '',  self.doc[key], dataHierarchyNode)
+        self.metaVendorW.show()
+      elif key=='metaUser':
+        self.btnUser.show()
+        addDocDetails(self, self.metaUserL,     '',  self.doc[key], dataHierarchyNode)
+        self.metaUserW.show()
+      else:
+        addDocDetails(self, self.metaDetailsL,  key, self.doc[key], dataHierarchyNode)
+        self.metaDetailsW.show()
+    return
+
+
+  def execute(self, command:list[Any]) -> None:
+    """
+    Hide / show the widget underneath the button
+
+    Args:
+      command (list): area to show/hide
+    """
+    if command[0] is Command.SHOW:
+      if getattr(self, f'btn{command[1]}').isChecked(): #get button in question
+        getattr(self, f'meta{command[1]}W').show()
+      else:
+        getattr(self, f'meta{command[1]}W').hide()
+    elif command[0] is Command.CLOSE:
+      self.comm.changeTable.emit('','')
+    elif command[0] is Command.TO_PROJECT:
+      self.comm.changeProject.emit(self.doc['branch'][0]['stack'][0], self.doc['id'])
+    elif isinstance(command[0], CommandMenu):
+      if executeContextMenu(self, command):
+        self.comm.changeTable.emit('','')
+        self.comm.changeDetails.emit(self.doc['id'])
+    else:
+      print('**ERROR details command unknown:',command)
+    return
+
+
+  @Slot()
+  def testExtractor(self) -> None:
+    """
+    User selects to test extractor on this dataset
+    """
+    logging.debug('details:testExtractor')
+    if len(self.doc)>1:
+      pathStr = self.doc['branch'][0]['path']
+      if pathStr is None:
+        showMessage(self, 'Warning', 'Selected item has no path.')
+      else:
+        path = Path(pathStr)
+        if not path.as_posix().startswith('http'):
+          path = self.comm.backend.basePath/path
+        report = self.comm.backend.testExtractor(path, outputStyle='html', style={'main':'/'.join(self.doc['type'])})
+        showMessage(self, 'Report of extractor test', report, style='QLabel {min-width: 800px}')
+    else:
+      showMessage(self, 'Warning', 'No item was selected via table-view, i.e. no details are shown.')
+    return
+
+
+  def resizeWidth(self, width:int) -> None:
+    """ called if details is resized by splitter at the parent widget
+    - resize all text-documents
+    """
+    if not self.rescaleTexts:
+      return
+    self.metaDetailsW.setFixedWidth(width)
+    for text in self.rescaleTexts:
+      text.document().setTextWidth(width-80)
+      height:int = text.document().size().toTuple()[1] # type:ignore[index]
+      text.setFixedHeight(height)
+    return
+
+
+class Command(Enum):
+  """ Commands used in this file """
+  SHOW             = 1
+  CLOSE            = 2
+  TO_PROJECT       = 3
