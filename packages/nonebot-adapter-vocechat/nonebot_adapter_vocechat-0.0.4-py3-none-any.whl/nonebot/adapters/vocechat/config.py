6b93348a00a79202c86635e72ad40f3bb7ed9fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:13:24 +0000

    Add package nonebot-adapter-vocechat/0.0.4/nonebot_adapter_vocechat-0.0.4-py3-none-any.whl

---
+++ packages/nonebot-adapter-vocechat/nonebot_adapter_vocechat-0.0.4-py3-none-any.whl/nonebot/adapters/vocechat/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class BotConfig(BaseModel):
+    name: str
+    server: str
+    api_key: str
+
+class Config(BaseModel):
+    vocechat_bots: List[BotConfig] = Field(default_factory=list)
