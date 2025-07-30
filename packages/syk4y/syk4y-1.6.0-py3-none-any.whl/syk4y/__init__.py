95c5cf9a870956a99f1d9b8a6cf53de42f31b450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:19 +0000

    Add package syk4y/1.6.0/syk4y-1.6.0-py3-none-any.whl

---
+++ packages/syk4y/syk4y-1.6.0-py3-none-any.whl/syk4y/__init__.py
@@ -0,0 +1,13 @@
+from .printer import inspect
+from .compress import fast_compress, fast_decompress, zip, unzip, to_zip, to_lz4
+
+
+__all__ = [
+    "inspect",
+    "fast_compress",
+    "fast_decompress",
+    "zip",
+    "unzip",
+    "to_zip",
+    "to_lz4"
+]
