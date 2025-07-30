485d8b76af4edf741e1e3dc8604568c5152870b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:55 +0000

    Add package graphe-circulant/0.1.5/graphe_circulant-0.1.5.tar.gz

---
+++ packages/graphe-circulant/graphe_circulant-0.1.5.tar.gz/graphe_circulant-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graphe_circulant',
+    version = '0.1.5',
+    author='lakrakar_labibi',
+    author_email='lakrakarouafaa@email.com',
+    description='Package pour le calcul de diamètres de graphes circulants',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'networkx',  # si tu l’utilises
+    ],
+    entry_points={
+        'console_scripts': [
+            'graphe_circulant = graphe_circulant.interface:lancer_interface',
+        ]
+    },
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+)
