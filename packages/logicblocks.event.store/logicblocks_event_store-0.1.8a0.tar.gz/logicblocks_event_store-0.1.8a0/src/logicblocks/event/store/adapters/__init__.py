b884b3c91f30f4dc4bf7ef2764a25006bda37ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:35 +0000

    Add package logicblocks.event.store/0.1.8a0/logicblocks_event_store-0.1.8a0.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.8a0.tar.gz/logicblocks_event_store-0.1.8a0/src/logicblocks/event/store/adapters/__init__.py
@@ -0,0 +1,12 @@
+from .base import EventSerialisationGuarantee, EventStorageAdapter
+from .memory import InMemoryEventStorageAdapter
+from .postgres import PostgresEventStorageAdapter
+from .postgres import QuerySettings as PostgresQuerySettings
+
+__all__ = [
+    "EventStorageAdapter",
+    "EventSerialisationGuarantee",
+    "InMemoryEventStorageAdapter",
+    "PostgresEventStorageAdapter",
+    "PostgresQuerySettings",
+]
