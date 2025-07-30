3000a9370d1da475cc457bd9944f94086ecf4e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:08 +0000

    Add package intelliopsopenfgasdk-python/0.1.6/intelliopsopenfgasdk_python-0.1.6-py3-none-any.whl

---
+++ packages/intelliopsopenfgasdk-python/intelliopsopenfgasdk_python-0.1.6-py3-none-any.whl/intelliopsopenfgasdk_python/__init__.py
@@ -0,0 +1,11 @@
+from .intelliops_openfga_sdk import IntelliOpsOpenFgaSDK
+from .models import CreateFgaModel, CreateDataSourceModel, CheckAccessModel
+
+__all__ = [
+    "IntelliOpsOpenFgaSDK",
+    "CreateFgaModel",
+    "CreateDataSourceModel",
+    "CheckAccessModel",
+]
+__version__ = "0.1.6"
+__author__ = "IntelliOps"
