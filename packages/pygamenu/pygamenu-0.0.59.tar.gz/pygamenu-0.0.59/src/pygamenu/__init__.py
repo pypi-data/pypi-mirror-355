a55f3c5d22cc47b75fb5ad8e04d6a6b6d2eb56dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:32 +0000

    Add package pygamenu/0.0.59/pygamenu-0.0.59.tar.gz

---
+++ packages/pygamenu/pygamenu-0.0.59.tar.gz/pygamenu-0.0.59/src/pygamenu/__init__.py
@@ -0,0 +1,4 @@
+from .mainn.main import initialize, View, STYLES, \
+                        Element, DivElement, PolygonElement, \
+                        TextElement, ImageElement, FrameElement
+print('Running PygaMenu version 0.0.59')
