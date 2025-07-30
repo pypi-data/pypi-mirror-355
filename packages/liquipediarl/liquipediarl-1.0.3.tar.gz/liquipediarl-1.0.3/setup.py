d03b91530e9877c6e8206437efb49349599868bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:37 +0000

    Add package liquipediarl/1.0.3/liquipediarl-1.0.3.tar.gz

---
+++ packages/liquipediarl/liquipediarl-1.0.3.tar.gz/liquipediarl-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liquipediarl',
+    version='1.0.3',
+    description='Liquipedia Rocket League API Wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Kian Mortimer',
+    author_email='kmortimer@proton.me',
+    url='https://github.com/kianmortimer/liquipediarl',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.7',
+)
