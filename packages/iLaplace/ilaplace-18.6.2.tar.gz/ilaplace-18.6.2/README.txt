b0c4f7dce330c704824309a3afe016cc512e2ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:34 +0000

    Add package iLaplace/18.6.2/ilaplace-18.6.2.tar.gz

---
+++ packages/iLaplace/ilaplace-18.6.2.tar.gz/ilaplace-18.6.2/README.txt
@@ -0,0 +1,18 @@
+Inverse Laplace — Simple Interface for Powerful Math
+inverse-laplace is a lightweight Python wrapper around advanced symbolic and numerical math tools (sympy, mpmath, scipy) that provides a clean and intuitive interface for computing inverse Laplace transforms using Talbot’s method.
+
+Why This Library?
+If you've ever tried to compute inverse Laplace transforms numerically in Python, you've probably noticed:
+mpmath.invertlaplace() is powerful but raw and low-level
+sympy gives symbolic transforms, but not numerical answers
+Combining them can be verbose and repetitive
+
+In short: You focus on the math, we handle the machinery.
+
+This project is licensed under the MIT License.
+It internally utilizes components from the following libraries:
+mpmath
+sympy
+numpy
+
+All third-party libraries retain their original licenses and attributions.
---
+++ packages/iLaplace/ilaplace-18.6.2.tar.gz/ilaplace-18.6.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='iLaplace',
+  version='18.6.2',
+  description="A minimal Python interface for computing inverse Laplace transforms using Talbot’s method — designed as a clean and practical wrapper around mpmath, sympy, and numpy.",
+  long_description=open('README.txt').read(),
+  url='',  
+  author='Mohammad H. Rostami',
+  author_email='mhro.r84@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Laplace Inverse''Laplace Inverse Calculator''Fast Laplace Inverse Calculate''iLaplace''Fat iLaplace Calculator''Fast iLpalace Calculate''Math''Talbot''inverse Laplace transforms''Laplace',
+  packages=find_packages(),
+  install_requires=['mpmath','sympy','numpy']
+)
