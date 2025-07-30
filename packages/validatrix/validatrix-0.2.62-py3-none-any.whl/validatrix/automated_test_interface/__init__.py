b3f8e62cb88643a55bf37c74a7b9d31ccb450054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:18 +0000

    Add package validatrix/0.2.62/validatrix-0.2.62-py3-none-any.whl

---
+++ packages/validatrix/validatrix-0.2.62-py3-none-any.whl/validatrix/automated_test_interface/__init__.py
@@ -0,0 +1,11 @@
+"""
+automated_test Interface module for Validatrix.
+Provides functionality for adding individual test cases to the test suite,
+and running the test suite.
+"""
+
+from .automated_test_interface import *
+
+__all__ = [
+    'AutomatedTestInterface',    
+] 
