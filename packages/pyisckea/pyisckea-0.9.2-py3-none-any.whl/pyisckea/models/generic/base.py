b6ad388a36644c9bd617b2988eb48400f6ff754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:58 +0000

    Add package pyisckea/0.9.2/pyisckea-0.9.2-py3-none-any.whl

---
+++ packages/pyisckea/pyisckea-0.9.2-py3-none-any.whl/pyisckea/models/generic/base.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+def normalize_keys(string: str) -> str:
+    return string.replace("_", "-")
+
+
+class KeaBaseModel(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=normalize_keys, populate_by_name=True, use_enum_values=True
+    )
+
+
+class KeaModel(KeaBaseModel):
+    user_context: Optional[dict] = None
+    comment: Optional[str] = None
+    unknown_map_entry: Optional[str] = None
