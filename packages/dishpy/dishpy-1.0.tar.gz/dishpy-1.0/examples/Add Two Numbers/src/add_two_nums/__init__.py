cc6a7f16f89539ac6580e4baf9240cf41792178c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:09:06 +0000

    Add package dishpy/1.0/dishpy-1.0.tar.gz

---
+++ packages/dishpy/dishpy-1.0.tar.gz/dishpy-1.0/examples/Add Two Numbers/src/add_two_nums/__init__.py
@@ -0,0 +1,4 @@
+"Put your package code!"
+from my_module.test import A
+def add_two_numbers(a, b):
+    return a + b
---
+++ packages/dishpy/dishpy-1.0.tar.gz/dishpy-1.0/examples/Calculator/src/main.py
@@ -0,0 +1,4 @@
+from add_two_nums import add_two_numbers
+from vex import Brain
+brain = Brain()
+brain.screen.print(str("10 + 3 = ") + str(add_two_numbers(10, 3)))
