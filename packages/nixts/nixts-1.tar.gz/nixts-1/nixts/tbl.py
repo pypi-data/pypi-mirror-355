397991ad8414d6428af32701429ea85709dccb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:48 +0000

    Add package nixts/1/nixts-1.tar.gz

---
+++ packages/nixts/nixts-1.tar.gz/nixts-1/nixts/tbl.py
@@ -0,0 +1,56 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "brk": "dbg",
+    "cfg": "irc",
+    "dbg": "dbg",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "dbg": "896a73d992745421ca93d8f8766ef18e",
+    "err": "89910392b217e6e72687369ce3ef7379",
+    "flt": "d719ae5006fa0b46b25f471bad8b87ba",
+    "fnd": "8328dc47025894bca026391838968e85",
+    "irc": "2f2a30d4ca36e8fb97a5f580c780aa1c",
+    "log": "70a38e5a66cb9b48c3c3a05e6ba92285",
+    "method": "0546afceec446f405aca02576a2fa1d2",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "f8100f98e47621ba07a789ea71199e50",
+    "slg": "99648f4d801c2356bbe18abef05e1a4b",
+    "tbl": "a894b2f81f332fe1ee55806dff2e595d",
+    "tdo": "01ea8420fce608cecde99eeab02feaa0",
+    "thr": "0a5ec1652afa0daf9edab6df5131211d",
+    "tmr": "a7aa00ef787fe4b9cd6f2a402e138114",
+    "upt": "28bf9e5e23f88130486b31d71e68fece",
+    "utils": "503abdd178e7f4519e605667a6a6e8f6",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
