0a84da644b21d90386a5e75b3136689526a548ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:31 +0000

    Add package meu-investimento-test/0.1/meu_investimento_test-0.1.0.tar.gz

---
+++ packages/meu-investimento-test/meu_investimento_test-0.1.0.tar.gz/meu_investimento_test-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+   name='meu_investimento_test',
+   version='0.1.0',
+   packages=find_packages(),
+   install_requires=[],
+   author='Vinicius S. de Oliveira',
+   author_email='vin.s.oliveira@icloud.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/tadrianonet/meu_investimento',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+   long_description=long_description,
+   long_description_content_type='text/markdown'
+)
