0342105fb8ac02cdca0d17c752ce473a8b1bde8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:59 +0000

    Add package midas-mosaik/2.1.0a5/midas_mosaik-2.1.0a5-py3-none-any.whl

---
+++ packages/midas-mosaik/midas_mosaik-2.1.0a5-py3-none-any.whl/midas/api/__init__.py
@@ -0,0 +1,5 @@
+from .fnc_analyze import analyze as analyze
+from .fnc_configure import configure as configure
+from .fnc_download import download as download
+from .fnc_list import list_scenarios as list_scenarios
+from .fnc_run import run as run
