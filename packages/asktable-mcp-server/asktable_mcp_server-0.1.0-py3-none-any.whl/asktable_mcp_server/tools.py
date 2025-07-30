231ca08704800c429d2cc7c4d1514becfc77f31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:09 +0000

    Add package asktable-mcp-server/0.1.0/asktable_mcp_server-0.1.0-py3-none-any.whl

---
+++ packages/asktable-mcp-server/asktable_mcp_server-0.1.0-py3-none-any.whl/asktable_mcp_server/tools.py
@@ -0,0 +1,18 @@
+from asktable import Asktable
+
+
+async def get_asktable_data(api_key ,datasource_id, question):
+    base_url = "https://api.asktable.com"
+    asktable_client = Asktable(base_url= base_url,api_key=api_key)
+    answer_response = asktable_client.answers.create(datasource_id=datasource_id, question=question)
+    if answer_response.answer is None:
+        return "没有查询到相关信息"
+    return answer_response.answer.text
+
+async def get_asktable_sql(api_key , datasource_id, question):
+    base_url = "https://api.asktable.com"
+    asktable_client = Asktable(base_url= base_url,api_key=api_key)
+    query_response = asktable_client.sqls.create(datasource_id=datasource_id, question=question)
+    if query_response.query.sql is None: 
+        return "没有查询到相关信息"
+    return query_response.query.sql
