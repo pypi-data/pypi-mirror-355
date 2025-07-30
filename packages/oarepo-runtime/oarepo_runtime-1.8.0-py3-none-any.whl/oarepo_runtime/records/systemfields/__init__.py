960235d6047013e9057c35a8aef3d0ea763872d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:07 +0000

    Add package oarepo-runtime/1.8.0/oarepo_runtime-1.8.0-py3-none-any.whl

---
+++ packages/oarepo-runtime/oarepo_runtime-1.8.0-py3-none-any.whl/oarepo_runtime/records/systemfields/__init__.py
@@ -0,0 +1,32 @@
+from .icu import (
+    FulltextIndexField,
+    ICUField,
+    ICUSearchField,
+    ICUSortField,
+    ICUSuggestField,
+)
+from .mapping import MappingSystemFieldMixin, SystemFieldDumperExt
+from .selectors import (
+    FilteredSelector,
+    FirstItemSelector,
+    MultiSelector,
+    PathSelector,
+    Selector,
+)
+from .synthetic import SyntheticSystemField
+
+__all__ = (
+    "ICUField",
+    "ICUSuggestField",
+    "ICUSortField",
+    "ICUSearchField",
+    "FulltextIndexField",
+    "MappingSystemFieldMixin",
+    "SystemFieldDumperExt",
+    "SyntheticSystemField",
+    "PathSelector",
+    "Selector",
+    "FirstItemSelector",
+    "FilteredSelector",
+    "MultiSelector",
+)
