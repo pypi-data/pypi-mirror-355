e15c7708c4a3e9b700646cc11f89928147762d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:30 +0000

    Add package cc-qa/0.1.2/cc_qa-0.1.2-py3-none-any.whl

---
+++ packages/cc-qa/cc_qa-0.1.2-py3-none-any.whl/cc_qa/_constants.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+
+# Definition of maximum deviations from the given frequency
+deltdic = {}
+deltdic["monmax"] = timedelta(days=31.01).total_seconds()
+deltdic["monmin"] = timedelta(days=27.99).total_seconds()
+deltdic["mon"] = timedelta(days=31).total_seconds()
+deltdic["daymax"] = timedelta(days=1.01).total_seconds()
+deltdic["daymin"] = timedelta(days=0.99).total_seconds()
+deltdic["day"] = timedelta(days=1).total_seconds()
+deltdic["1hrmin"] = timedelta(hours=0.99).total_seconds()
+deltdic["1hrmax"] = timedelta(hours=1.01).total_seconds()
+deltdic["1hr"] = timedelta(hours=1).total_seconds()
+deltdic["3hrmin"] = timedelta(hours=2.99).total_seconds()
+deltdic["3hrmax"] = timedelta(hours=3.01).total_seconds()
+deltdic["3hr"] = timedelta(hours=3).total_seconds()
+deltdic["6hrmin"] = timedelta(hours=5.99).total_seconds()
+deltdic["6hrmax"] = timedelta(hours=6.01).total_seconds()
+deltdic["6hr"] = timedelta(hours=6).total_seconds()
+deltdic["yrmax"] = timedelta(days=366.01).total_seconds()
+deltdic["yrmin"] = timedelta(days=359.99).total_seconds()
+deltdic["yr"] = timedelta(days=360).total_seconds()
