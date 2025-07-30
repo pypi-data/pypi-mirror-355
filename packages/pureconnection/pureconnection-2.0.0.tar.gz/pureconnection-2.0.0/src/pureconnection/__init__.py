cdd23dc1f3e9fe08291ac5a6a4ad3d71699c5f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:44 +0000

    Add package pureconnection/2.0.0/pureconnection-2.0.0.tar.gz

---
+++ packages/pureconnection/pureconnection-2.0.0.tar.gz/pureconnection-2.0.0/src/pureconnection/__init__.py
@@ -0,0 +1,8 @@
+# src/pureconnection/__init__.py
+
+# This is make Class open to Public API
+# from pureconnection import PrettyPrinter
+
+from .utils import PrettyPrinter
+
+__all__ = ["PrettyPrinter"]
