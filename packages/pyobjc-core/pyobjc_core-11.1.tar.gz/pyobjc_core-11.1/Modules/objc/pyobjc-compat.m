08a9c754c87970fa1ae8cde3f82696cc3860e7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:39 +0000

    Add package pyobjc-core/11.1/pyobjc_core-11.1.tar.gz

---
+++ packages/pyobjc-core/pyobjc_core-11.1.tar.gz/pyobjc_core-11.1/Modules/objc/pyobjc-compat.m
@@ -0,0 +1,161 @@
+#include "pyobjc.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+int
+PyObjC_Cmp(PyObject* o1, PyObject* o2, int* result)
+{
+    int r;
+
+    r = PyObject_RichCompareBool(o1, o2, Py_EQ);
+    if (r == -1) {
+        return -1;
+    } else if (r == 1) {
+        *result = 0;
+        return 0;
+    }
+
+    r = PyObject_RichCompareBool(o1, o2, Py_LT);
+    if (r == -1) {
+        return -1;
+    } else if (r == 1) {
+        *result = -1;
+        return 0;
+    }
+
+    r = PyObject_RichCompareBool(o1, o2, Py_GT);
+    if (r == -1) {
+        return 1;
+    } else if (r == 1) {
+        *result = 1;
+        return 0;
+    }
+
+    PyErr_Format(PyExc_TypeError, "%R and %R cannot be compared", o1, o2);
+    return -1;
+}
+
+/* XXX: Can we do without this function, it is a little too intimate with
+ * unicode details.
+ */
+const char* _Nullable PyObjC_Unicode_Fast_Bytes(PyObject* object)
+{
+    /* Having a unicode string is a precondition, checked manually
+     * that callers check the type beforehand.
+     */
+    assert(PyUnicode_Check(object));
+
+    if (!PyUnicode_IS_ASCII(object)) {
+        /* The code below raises the correct error in a roundabout
+         * way. That's because UnicodeEncodeError expects more
+         * than one argument.
+         */
+        PyObject* r = PyUnicode_AsASCIIString(object);
+        if (unlikely(r != NULL)) { // LCOV_BR_EXCL_LINE
+            // LCOV_EXCL_START
+            PyErr_SetString(PyObjCExc_Error, "Raising UnicodeError failed");
+            Py_DECREF(r);
+            return NULL;
+            // LCOV_EXCL_STOP
+        }
+
+        assert(r == NULL);
+        return NULL;
+    }
+
+    return (const char*)(PyUnicode_DATA(object));
+}
+
+#if PY_VERSION_HEX < 0x03090000
+/*
+ * Internal compatibility stubs for some parts of the vectorcall API.
+ *
+ * This implementation does not support keyword names, ass that both
+ * complicates the implementation and is not necessary for PyObjC.
+ */
+
+PyObject* _Nullable PyObject_Vectorcall(PyObject* callable,
+                                        PyObject* _Nonnull const* _Nonnull args,
+                                        size_t nargsf, PyObject* _Nullable kwnames)
+{
+    size_t i;
+    if (kwnames != NULL) {
+        PyErr_SetString(PyExc_RuntimeError,
+                        "PyObjC's vectorcall compat does not support keyword arguments");
+        return NULL;
+    }
+
+    PyObject* tuple = PyTuple_New(PyVectorcall_NARGS(nargsf));
+    if (tuple == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < PyVectorcall_NARGS(nargsf); i++) {
+        if (args[i] == NULL) {
+            PyErr_SetString(PyExc_RuntimeError, "NULL argument");
+            Py_DECREF(tuple);
+            return NULL;
+        }
+        PyTuple_SET_ITEM(tuple, i, args[i]);
+        Py_INCREF(args[i]);
+    }
+
+    PyObject* res = PyObject_CallObject(callable, tuple);
+    Py_DECREF(tuple);
+    return res;
+}
+
+PyObject*
+PyObject_VectorcallMethod(PyObject* name, PyObject* _Nonnull const* _Nonnull args,
+                          size_t    nargsf, PyObject* _Nullable kwnames)
+{
+    size_t i;
+    if (kwnames != NULL) {
+        PyErr_SetString(PyExc_RuntimeError,
+                        "PyObjC's vectorcall compat does not support keyword arguments");
+        return NULL;
+    }
+    if (name == NULL) {
+        assert(PyErr_Occurred());
+        return NULL;
+    }
+    if (PyVectorcall_NARGS(nargsf) == 0) {
+        PyErr_SetString(PyExc_ValueError, "Missing first argument");
+        return NULL;
+    }
+
+    PyObject* callable = PyObject_GetAttr(args[0], name);
+    if (callable == NULL) {
+        return NULL;
+    }
+
+    if (PyVectorcall_NARGS(nargsf) == 1) {
+        PyObject* res = PyObject_CallFunction(callable, NULL);
+        Py_DECREF(callable);
+        return res;
+    }
+
+    PyObject* tuple = PyTuple_New(PyVectorcall_NARGS(nargsf) - 1);
+    if (tuple == NULL) {
+        return NULL;
+    }
+
+    for (i = 1; i < PyVectorcall_NARGS(nargsf); i++) {
+        if (args[i] == NULL) {
+            PyErr_SetString(PyExc_RuntimeError, "NULL argument");
+            Py_DECREF(tuple);
+            return NULL;
+        }
+        PyTuple_SET_ITEM(tuple, i - 1, args[i]);
+        Py_INCREF(args[i]);
+    }
+
+    PyObject* res = PyObject_CallObject(callable, tuple);
+    Py_DECREF(tuple);
+    Py_DECREF(callable);
+    return res;
+}
+
+#endif
+
+NS_ASSUME_NONNULL_END
---
+++ packages/pyobjc-core/pyobjc_core-11.1.tar.gz/pyobjc_core-11.1/Modules/objc/test/coding.m
@@ -0,0 +1,163 @@
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Foundation/Foundation.h>
+
+@interface PyObjC_TestCodingClass : NSObject {
+    id returnObject;
+}
+- (void)encodeWithCoder:(NSCoder*)coder;
+- (void)runThread:(id)object;
+- (id)returnObject;
+
++ (int)fetchInt:(NSCoder*)coder;
++ (double)fetchDouble:(NSCoder*)coder;
++ (NSData*)fetchData:(NSCoder*)coder;
++ (NSArray*)fetchArray:(NSCoder*)coder;
+@end
+
+@interface
+NSObject (IKnowWhatImDoing)
+- (id)call;
+@end
+
+@implementation PyObjC_TestCodingClass
+- (int)_privateMethodWithArg:(float)arg
+{
+    return (int)arg;
+}
+
+- (void)runThread:(id)object
+{
+    NSObject* pool = [[NSAutoreleasePool alloc] init];
+    returnObject   = [object call];
+    [returnObject retain];
+    [pool release];
+}
+
+- (id)returnObject
+{
+    return returnObject;
+}
+
+- (void)encodeWithCoder:(NSCoder*)coder
+{
+    double d        = 1.5;
+    int    iArray[] = {3, 4, 5, 6};
+    [coder encodeValueOfObjCType:@encode(double) at:&d];
+    [coder encodeArrayOfObjCType:@encode(int) count:4 at:iArray];
+    [coder encodeBytes:"hello world" length:11];
+}
+
++ (int)fetchInt:(NSCoder*)coder
+{
+    int i;
+    [[clang::suppress]]
+    [coder decodeValueOfObjCType:@encode(int) at:&i];
+    return i;
+}
+
++ (double)fetchDouble:(NSCoder*)coder
+{
+    double i;
+    [[clang::suppress]]
+    [coder decodeValueOfObjCType:@encode(double) at:&i];
+    return i;
+}
+
++ (NSData*)fetchData:(NSCoder*)coder
+{
+    void*      data;
+    NSUInteger length;
+
+    data = [coder decodeBytesWithReturnedLength:&length];
+    return [NSData dataWithBytes:data length:length];
+}
+
++ (NSArray*)fetchArray:(NSCoder*)coder
+{
+    int data[10];
+
+    [coder decodeArrayOfObjCType:@encode(int) count:10 at:data];
+    return [NSArray arrayWithObjects:[NSNumber numberWithInt:data[0]],
+                                     [NSNumber numberWithInt:data[1]],
+                                     [NSNumber numberWithInt:data[2]],
+                                     [NSNumber numberWithInt:data[3]],
+                                     [NSNumber numberWithInt:data[4]],
+                                     [NSNumber numberWithInt:data[5]],
+                                     [NSNumber numberWithInt:data[6]],
+                                     [NSNumber numberWithInt:data[7]],
+                                     [NSNumber numberWithInt:data[8]],
+                                     [NSNumber numberWithInt:data[9]], nil];
+}
+
++ (NSString*)fetchObjectDescription:(NSObject*)value
+{
+    return [value description];
+}
+
+@end
+
+static PyMethodDef mod_methods[] = {{0, 0, 0, 0}};
+
+static int mod_exec_module(PyObject* m)
+{
+    if (PyObjC_ImportAPI(m) < 0) { // LCOV_BR_EXCL_LINE
+        return -1; // LCOV_EXCL_LINE
+    }
+
+    if (PyModule_AddObject(m, // LCOV_BR_EXCL_LINE
+                "PyObjC_TestCodingClass",
+                           PyObjC_IdToPython([PyObjC_TestCodingClass class]))
+        == -1) {
+        return -1; // LCOV_EXCL_LINE
+    }
+    return 0;
+}
+
+static struct PyModuleDef_Slot mod_slots[] = {
+    {
+        .slot = Py_mod_exec,
+        .value = (void*)mod_exec_module
+    },
+#if PY_VERSION_HEX >= 0x030c0000
+    {
+        /* This extension does not use the CPython API other than initializing
+         * the module, hence is safe with subinterpreters and per-interpreter
+         * GILs
+         */
+        .slot = Py_mod_multiple_interpreters,
+        .value = Py_MOD_PER_INTERPRETER_GIL_SUPPORTED,
+    },
+#endif
+#if PY_VERSION_HEX >= 0x030d0000
+    {
+        .slot = Py_mod_gil,
+        .value = Py_MOD_GIL_NOT_USED,
+    },
+#endif
+    {  /* Sentinel */
+        .slot = 0,
+        .value = 0
+    }
+};
+
+static struct PyModuleDef mod_module = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "coding",
+    .m_doc = NULL,
+    .m_size = 0,
+    .m_methods = mod_methods,
+    .m_slots = mod_slots,
+    .m_traverse = NULL,
+    .m_clear = NULL,
+    .m_free = NULL,
+};
+
+PyObject* PyInit_coding(void);
+
+PyObject* __attribute__((__visibility__("default"))) _Nullable PyInit_coding(void)
+{
+    return PyModuleDef_Init(&mod_module);
+}
