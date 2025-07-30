d34d24d208b7f07106572b307c1e8b6b064fbe2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:09 +0000

    Add package more-tk/0.1.0/more_tk-0.1.0-py3-none-any.whl

---
+++ packages/more-tk/more_tk-0.1.0-py3-none-any.whl/more_tk/src/more_tk/__init__.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+app = None
+
+def mtk(geo="400x400", title="Wrapped by more_tk", bg="white", full=False):
+    global app
+    if app is None:
+        app = tk.Tk()
+        app.geometry(geo)
+        app.title(title)
+        app.config(bg=bg)
+        if full == False:
+            app.attributes("-fullscreen", False)
+        elif full == True:
+            app.attributes("-fullscreen", True)
+        else:
+            print("'full' must be 'True', 'False' or just not set")
+    return app
+
+def mtk_mainloop():
+    global app
+    if app is not None:
+        app.mainloop()
+
+def label_mtk(text="No Text, define the text", bg_mtk="white", fg_mtk="black", x=0,y=0):
+    label_ = tk.Label(text=text, bg=bg_mtk, fg=fg_mtk)
+    label_.place(x=x,y=y)
+
+def button_mtk(text="No Text, define the text", bg_mtk="white", fg_mtk="black", x=0,y=0):
+    button_ = tk.Label(text=text, bg=bg_mtk, fg=fg_mtk)
+    button_.place(x=x, y=y)
---
+++ packages/more-tk/more_tk-0.1.0-py3-none-any.whl/more_tk/tests/__init__.py
@@ -0,0 +1,5 @@
+from more_tk.src.more_tk import mtk, mtk_mainloop, label_mtk
+
+app = mtk(bg="blue", full="")
+
+mtk_mainloop()
