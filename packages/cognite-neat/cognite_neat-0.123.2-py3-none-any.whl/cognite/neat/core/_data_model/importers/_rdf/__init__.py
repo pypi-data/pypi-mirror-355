57aae86bd0cc8bac6cd4d6f57e29913286ca3d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:59 +0000

    Add package cognite-neat/0.123.2/cognite_neat-0.123.2-py3-none-any.whl

---
+++ packages/cognite-neat/cognite_neat-0.123.2-py3-none-any.whl/cognite/neat/core/_data_model/importers/_rdf/__init__.py
@@ -0,0 +1,5 @@
+from ._imf2data_model import IMFImporter
+from ._inference2rdata_model import InferenceImporter, SubclassInferenceImporter
+from ._owl2data_model import OWLImporter
+
+__all__ = ["IMFImporter", "InferenceImporter", "OWLImporter", "SubclassInferenceImporter"]
