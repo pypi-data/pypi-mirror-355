3fa4f6fcb2a98547f8f260d9142501f31d904ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:08 +0000

    Add package appdynamics-bindeps-linux-aarch64/25.6.0/appdynamics_bindeps_linux_aarch64-25.6.0-cp313-none-any.whl

---
+++ packages/appdynamics-bindeps-linux-aarch64/appdynamics_bindeps_linux_aarch64-25.6.0-cp313-none-any.whl/appdynamics_bindeps/zmq-cp313/tests/test_ssh.py
@@ -0,0 +1,9 @@
+from appdynamics_bindeps.zmq.ssh.tunnel import select_random_ports
+
+
+def test_random_ports():
+    for i in range(4096):
+        ports = select_random_ports(10)
+        assert len(ports) == 10
+        for p in ports:
+            assert ports.count(p) == 1
