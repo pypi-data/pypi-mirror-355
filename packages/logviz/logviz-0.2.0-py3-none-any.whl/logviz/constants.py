c4b9e9864ba6cb6d22ed74f7f08d3f5a289768e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:46 +0000

    Add package logviz/0.2.0/logviz-0.2.0-py3-none-any.whl

---
+++ packages/logviz/logviz-0.2.0-py3-none-any.whl/logviz/constants.py
@@ -0,0 +1,10 @@
+import logging
+
+LEVEL_MAP = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "success": logging.SUCCESS,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
