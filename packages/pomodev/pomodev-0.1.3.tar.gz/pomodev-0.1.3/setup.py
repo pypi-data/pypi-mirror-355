7e7c893f3b81bcdea9c4bd175f6892efc47ecf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:13:55 +0000

    Add package pomodev/0.1.3/pomodev-0.1.3.tar.gz

---
+++ packages/pomodev/pomodev-0.1.3.tar.gz/pomodev-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pomodev',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pomodev=devtimer.__main__:run',
+        ],
+    },
+    author='Dhruv Tiwari',
+    description='A Pomodoro CLI timer with Git integration.',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
