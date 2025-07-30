0aab5589a07822ec3dd05d1d0c8cd0122bfaf451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:17 +0000

    Add package llamea/1.0.6/llamea-1.0.6-py3-none-any.whl

---
+++ packages/llamea/llamea-1.0.6-py3-none-any.whl/llamea/utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class NoCodeException(Exception):
+    """Could not extract generated code."""
+
+    pass
+
+
+def handle_timeout(signum, frame):
+    """Raise a timeout exception"""
+    raise TimeoutError
+
+
+def discrete_power_law_distribution(n, beta):
+    """
+    Power law distribution function from:
+    # Benjamin Doerr, Huu Phuoc Le, Régis Makhmara, and Ta Duy Nguyen. 2017.
+    # Fast genetic algorithms.
+    # In Proceedings of the Genetic and Evolutionary Computation Conference (GECCO '17).
+    # Association for Computing Machinery, New York, NY, USA, 777–784.
+    # https://doi.org/10.1145/3071178.3071301
+    """
+
+    def discrete_power_law(n, alpha, beta):
+        half_n = int(n / 2)
+        C_beta_half_n = 0
+        for i in range(1, half_n + 1):
+            C_beta_half_n += i ** (-beta)
+        probability_alpha = C_beta_half_n ** (-1) * alpha ** (-beta)
+        return probability_alpha
+
+    half_n = int(n / 2)
+    elements = [alpha for alpha in range(1, half_n + 1)]
+    probabilities = [discrete_power_law(n, alpha, beta) for alpha in elements]
+    if elements == []:
+        return 0.05
+    else:
+        sample = np.random.choice(elements, p=probabilities)
+        return sample / n
