bf9ec694280cb74905af332d2caa466d281b8438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:17 +0000

    Add package shmtu-auth/2.0.6/shmtu_auth-2.0.6-py3-none-any.whl

---
+++ packages/shmtu-auth/shmtu_auth-2.0.6-py3-none-any.whl/shmtu_auth/main_start.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from shmtu_auth.src.entry import entry
+
+
+def main():
+    entry()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/shmtu-auth/shmtu_auth-2.0.6-py3-none-any.whl/shmtu_auth/src/config/build_info.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from shmtu_auth import __version__ as version
+
+program_version = "2.0.6"
+docker_build_time = ""
+exe_build_time = ""
+wheel_build_time = "2025-06-16 01:36:56"
+branch = "main"
