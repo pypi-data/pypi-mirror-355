92d758eb5b3e7f4810963b888bf156aae652fd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:55 +0000

    Add package transcribe-anything/3.2.1/transcribe_anything-3.2.1-py3-none-any.whl

---
+++ packages/transcribe-anything/transcribe_anything-3.2.1-py3-none-any.whl/transcribe_anything/cli_init_insane.py
@@ -0,0 +1,34 @@
+"""
+Main entry point.
+"""
+
+import logging
+import os
+
+from transcribe_anything.insanley_fast_whisper_reqs import get_environment
+
+# for resource loading
+
+
+logger = logging.getLogger("transcribe_anything")
+logger.setLevel(logging.INFO)
+
+# _MODEL = os.environ.get("TRANSCRIBE_ANYTHING_MODEL", "large-v3")
+# _DEVICE = os.environ.get("TRANSCRIBE_ANYTHING_DEVICE", "insane")
+# _BATCH_SIZE = os.environ.get("TRANSCRIBE_ANYTHING_BATCH_SIZE", 8)
+
+
+def main() -> int:
+    """Main entry point for the transcribe_anything package."""
+    # locate the bundled sample.mp3
+    print("Installing transcribe_anything (device insane) environment...")
+    env = get_environment(has_nvidia=True)
+    env.run(["pwd"])
+    print("Installing static ffmpeg...")
+    os.system("static_ffmpeg -version")
+    return 0
+
+
+if __name__ == "__main__":
+
+    main()
