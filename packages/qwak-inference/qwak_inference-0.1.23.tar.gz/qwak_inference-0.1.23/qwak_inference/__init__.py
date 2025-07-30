ff47a93047cb93685762fdc663a6fa645e63b569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:20 +0000

    Add package qwak-inference/0.1.23/qwak_inference-0.1.23.tar.gz

---
+++ packages/qwak-inference/qwak_inference-0.1.23.tar.gz/qwak_inference-0.1.23/qwak_inference/__init__.py
@@ -0,0 +1,23 @@
+from qwak_inference.realtime_client import RealTimeClient
+
+clients = ["RealTimeClient"]
+try:
+    from qwak.inner import wire_dependencies
+
+    from qwak_inference.batch_client.batch_client import BatchInferenceClient
+    from qwak_inference.feedback_client import FeedbackClient
+
+    clients.extend(["BatchInferenceClient", "FeedbackClient"])
+
+    wire_dependencies()
+except ImportError:
+    # We are conditional loading these clients since the skinny does
+    # not support them due to the pandas, numpy, joblib, etc. dependencies
+    print(
+        "Notice that BatchInferenceClient and FeedbackClient are not available in the skinny package. "
+        'In order to use them, please install them as extras: pip install "qwak-inference[batch,feedback]".'
+    )
+
+__all__ = clients
+
+__version__ = "0.1.23"
