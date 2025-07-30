687dcd40d13352272f64dda344f8e40bc503b885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:56 +0000

    Add package loginserviceanfia/0.1.6/loginserviceanfia-0.1.6-py3-none-any.whl

---
+++ packages/loginserviceanfia/loginserviceanfia-0.1.6-py3-none-any.whl/loginserviceanfia/__init__.py
@@ -0,0 +1,6 @@
+from .loginserviceanfia import move_window_to_primary_monitor
+from .loginserviceanfia import login_row
+from .loginserviceanfia import build_config
+from .loginserviceanfia import get_login_info_from_config
+from .loginserviceanfia import simulate_user_login
+from .loginserviceanfia import build_connection_string
