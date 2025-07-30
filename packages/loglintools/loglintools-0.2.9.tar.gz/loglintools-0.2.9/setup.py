47a849ec929fd3017c7c8793c216def7dc7f1291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:22 +0000

    Add package loglintools/0.2.9/loglintools-0.2.9.tar.gz

---
+++ packages/loglintools/loglintools-0.2.9.tar.gz/loglintools-0.2.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loglintools',                     # Название библиотеки
+    version='0.2.9',                       # Версия
+    packages=['loglintools'],              # Автоматический поиск пакетов
+    author='MK',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
