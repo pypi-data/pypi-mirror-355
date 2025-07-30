d87a29658a4b14d5271cbfb5f99c508b4efa7b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:38 +0000

    Add package ultradetector/0.2.2/ultradetector-0.2.2.tar.gz

---
+++ packages/ultradetector/ultradetector-0.2.2.tar.gz/ultradetector-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ultradetector",  # PyPI library
+    py_modules=["ultradetector"],  # module name
+    version="0.2.2",  # version
+    author="weki",
+    # Python version
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "nibabel",
+        "matplotlib",
+        "opencv-python",
+        "psutil",
+        "pyyaml",
+        "requests",
+        "ultralytics"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ultradetector=ultradetector.python_api:main'
+        ]
+    },
+)
