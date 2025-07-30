a5146a1a9efc85d74e25e3024de33c53c1acd3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:10 +0000

    Add package cellnotifier/0.1.0/cellnotifier-0.1.0.tar.gz

---
+++ packages/cellnotifier/cellnotifier-0.1.0.tar.gz/cellnotifier-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cellnotifier',
+    version='0.1.0',
+    description='A lightweight library to notify the user via voice when a task or cell finishes running.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mopoco',
+    author_email='your@email.com',
+    url='https://github.com/yourusername/cellnotifier',  # Optional if you’ll host it on GitHub
+    packages=find_packages(),
+    install_requires=[
+        'gTTS',
+        'playsound'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
