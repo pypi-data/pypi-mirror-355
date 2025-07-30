e1168a219d96600461933fd54c6527bccdb3a110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:15 +0000

    Add package instill-sdk/0.17.3rc1/instill_sdk-0.17.3rc1.tar.gz

---
+++ packages/instill-sdk/instill_sdk-0.17.3rc1.tar.gz/instill_sdk-0.17.3rc1/instill/helpers/errors.py
@@ -0,0 +1,33 @@
+class ModelPathException(Exception):
+    def __str__(self) -> str:
+        return "model path is not valid"
+
+
+class ModelVramException(Exception):
+    def __str__(self) -> str:
+        return "model projected vram usage is more than the GPU can handle"
+
+
+class ModelConfigException(Exception):
+    def __init__(self, field):
+        self.field = field
+
+    def __str__(
+        self,
+    ) -> str:
+        return f"model config file `instill.yaml` is missing {self.field} field"
+
+
+class InvalidInputException(Exception):
+    def __init__(self, field):
+        self.field = field
+
+    def __str__(
+        self,
+    ) -> str:
+        return f"trigger request input error: {self.field}"
+
+
+class InvalidOutputShapeException(Exception):
+    def __str__(self) -> str:
+        return "outputs length not matched"
