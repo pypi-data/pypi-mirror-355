f3418e9ecbb90116330007fc2e45d54a45018aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:28 +0000

    Add package premier/0.4.9/premier-0.4.9-py3-none-any.whl

---
+++ packages/premier/premier-0.4.9-py3-none-any.whl/premier/__init__.py
@@ -0,0 +1,11 @@
+from .main import Premier as Premier
+from .throttler.api import fixed_window as fixed_window
+from .throttler.api import leaky_bucket as leaky_bucket
+from .throttler.api import sliding_window as sliding_window
+from .throttler.api import token_bucket as token_bucket
+from .throttler.errors import QuotaExceedsError as QuotaExceedsError
+from .throttler.throttler import Throttler as Throttler
+from .timer import ILogger as ILogger
+
+VERSION = "0.4.9"
+__version__ = VERSION
