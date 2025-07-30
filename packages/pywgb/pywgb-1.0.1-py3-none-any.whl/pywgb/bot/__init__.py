a22620f08abed47451282c1015335390184ea25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:05 +0000

    Add package pywgb/1.0.1/pywgb-1.0.1-py3-none-any.whl

---
+++ packages/pywgb/pywgb-1.0.1-py3-none-any.whl/pywgb/bot/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+All bot classes
+
+- Author: Rex Zhou <879582094@qq.com>
+- Created Time: 2025/5/27 13:54
+- Copyright: Copyright © 2025 Rex Zhou. All rights reserved.
+"""
+
+from ._smart import TextBot, MarkdownBot, ImageBot
+from ._smart import NewsBot, FileBot, VoiceBot
+from ._smart import TextCardBot, NewsCardBot, SmartBot
+
+__all__ = [
+    "TextBot",
+    "MarkdownBot",
+    "ImageBot",
+    "NewsBot",
+    "FileBot",
+    "VoiceBot",
+    "TextCardBot",
+    "NewsCardBot",
+    "SmartBot",
+]
