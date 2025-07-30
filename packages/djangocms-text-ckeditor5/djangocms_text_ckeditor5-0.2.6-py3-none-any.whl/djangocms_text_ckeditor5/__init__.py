57b7b921742263c3a923315b1de0764cca9c10a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:17 +0000

    Add package djangocms-text-ckeditor5/0.2.6/djangocms_text_ckeditor5-0.2.6-py3-none-any.whl

---
+++ packages/djangocms-text-ckeditor5/djangocms_text_ckeditor5-0.2.6-py3-none-any.whl/djangocms_text_ckeditor5/__init__.py
@@ -0,0 +1,18 @@
+from djangocms_text.editors import RTEConfig
+
+
+__version__ = "0.2.6"
+
+
+ckeditor5 = RTEConfig(
+    name="ckeditor5",
+    config="CKEDITOR5",
+    js=("djangocms_text_ckeditor5/bundles/bundle.ckeditor5.min.js",),
+    css={
+        "all": (
+            "djangocms_text_ckeditor5/css/cms.ckeditor5.css",
+            "djangocms_text_ckeditor5/css/cms.linkfield.css",
+        )
+    },
+    inline_editing=True,
+)
