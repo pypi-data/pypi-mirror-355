9c8be0013688cced2be0df6f3fcd4df71a759adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:22 +0000

    Add package gensin/4/gensin-4.tar.gz

---
+++ packages/gensin/gensin-4.tar.gz/gensin-4/gensin/__init__.py
@@ -0,0 +1,36 @@
+
+import requests
+import pyperclip as pc
+
+def o3(promt):
+        gpt_key = 'shds-3K2ZkpQU46dRK0FyVW3jhfL6L5O'
+        """Get response from LLM"""
+        url = "https://gptunnel.ru/v1/chat/completions"
+
+        headers = {
+            "Authorization": f"Bearer {gpt_key}",
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "model": "o3",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": """
+                        У меня сейчас экзамен. 
+                        Пиши только код целиком без пояснений и коментариев.
+                        НИЧЕГО КРОМЕ КОДА НЕ КИДАЙ!!!
+                        НО ЕСЛИ Я ПРОШУ ОБЬЯСНЕНИЯ КОДА, ТОГДА УЖЕ МОЖЕШЬ ОБЬЯСНЯТЬ ТЕКСТОМ, ТО ЕСТЬ МОЖЕШЬ КРОМЕ КОДА ЧТО-НИБУДЬ ПИСАТЬ, ЕСЛИ НЕ ПРОШУ ОБЬЯСНЕНИЙ - не пиши кроме кода ничего 
+                        '```python' - вот такое не вставляй (нужен только сам код)
+                    """,
+                },
+                {"role": "user", "content": promt},
+            ],
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            pc.copy(response.json()["choices"][0]["message"]["content"])
+        else:
+            pc.copy('import numpy as np')
---
+++ packages/gensin/gensin-4.tar.gz/gensin-4/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gensin',
+      version='4',
+      packages=['gensin'],
+      author_email='antoha123_3@mail.ru')
