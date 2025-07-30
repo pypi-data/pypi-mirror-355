1f6047ee7b627970b9e62871bad3355612ddb230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package aiorgwadmin/1.1.0/aiorgwadmin-1.1.0.tar.gz

---
+++ packages/aiorgwadmin/aiorgwadmin-1.1.0.tar.gz/aiorgwadmin-1.1.0/aiorgwadmin/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'aiorgwadmin'
+__version__ = "1.1.0"
+__author__ = 'Derek Yarnell, Mikle Green'
+__license__ = 'LGPL v2.1'
+
+from .rgw import RGWAdmin
+from .user import RGWUser, RGWQuota, RGWSwiftKey, RGWSubuser, RGWKey, RGWCap
