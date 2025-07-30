3ab3db0ee818b2795b138e17e9d8f9305f1c56b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:08 +0000

    Add package whisptray/0.3.2/whisptray-0.3.2-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/whisptray/whisptray-0.3.2-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/whisptray/essential_thread.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+from threading import Thread
+from time import sleep
+import os
+
+@contextmanager
+def essential_thread():
+    """A context manager that causes the program to exit if the thread exits
+    unexpectedly."""
+    failed = True
+    try:
+        yield
+        failed = False
+    finally:
+        if failed:
+            # Exit after a short delay to allow the exception to be logged.
+            def exit_soon():
+                sleep(1)
+                os._exit(1)
+            thread = Thread(target=exit_soon, daemon=True)
+            thread.start()
