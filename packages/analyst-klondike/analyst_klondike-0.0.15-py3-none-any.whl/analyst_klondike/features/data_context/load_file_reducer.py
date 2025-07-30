34ec59c873c4938ba4654aaa736ac64bb731ce99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:38 +0000

    Add package analyst-klondike/0.0.15/analyst_klondike-0.0.15-py3-none-any.whl

---
+++ packages/analyst-klondike/analyst_klondike-0.0.15-py3-none-any.whl/analyst_klondike/features/data_context/load_file_reducer.py
@@ -0,0 +1,17 @@
+from analyst_klondike.features.data_context.init_action import (
+    InitAction,
+    init_state)
+from analyst_klondike.features.data_context.set_opened_file_action import SetOpenedFileAction
+from analyst_klondike.state.app_state import AppState
+from analyst_klondike.state.base_action import BaseAction
+
+
+def apply(state: AppState, action: BaseAction) -> AppState:
+    if isinstance(action, InitAction):
+        init_state(state, action.data, action.file_path)
+        return state
+    if isinstance(action, SetOpenedFileAction):
+        state.current.opened_file_path = action.opened_file_path
+        state.current.opened_file_name = action.opened_file_name
+        return state
+    return state
