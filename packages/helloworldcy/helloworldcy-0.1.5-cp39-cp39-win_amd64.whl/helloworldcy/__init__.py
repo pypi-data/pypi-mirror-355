34e6691a7654ad30029ad5f84e2edfdbf96ee203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:26 +0000

    Add package helloworldcy/0.1.5/helloworldcy-0.1.5-cp39-cp39-win_amd64.whl

---
+++ packages/helloworldcy/helloworldcy-0.1.5-cp39-cp39-win_amd64.whl/helloworldcy/__init__.py
@@ -0,0 +1,16 @@
+from .hello import say_hello
+
+# Actualización forzada al importar
+import subprocess, sys, pkg_resources
+from packaging import version
+
+def force_update_if_needed(package_name="helloworldcy", min_version="0.1.5"):
+    try:
+        current = pkg_resources.get_distribution(package_name).version
+        #if version.parse(current) < version.parse(min_version):
+        print()
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", package_name])
+    except Exception as e:
+        print("")
+
+force_update_if_needed()
