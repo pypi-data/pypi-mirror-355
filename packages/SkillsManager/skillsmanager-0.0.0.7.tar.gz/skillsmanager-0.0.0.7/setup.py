b391b7093d4725f8f7e117069d563837deabcd71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:38 +0000

    Add package SkillsManager/0.0.0.7/skillsmanager-0.0.0.7.tar.gz

---
+++ packages/SkillsManager/skillsmanager-0.0.0.7.tar.gz/skillsmanager-0.0.0.7/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="SkillsManager",
+    version="0.0.0.7",                         # bump to whatever's next
+    packages=find_packages(where="SkillsManager"),
+    package_dir={"": "SkillsManager"},
+    include_package_data=True,
+    install_requires=[
+        "pyperclip",
+        "pyautogui",
+        "python-dotenv",
+        "google-genai",
+        "uv",
+    ],
+    author="Tristan McBride Sr.",
+    author_email="TristanMcBrideSr@users.noreply.github.com",
+    description="A modern way to auto load AI Skills/Tools",
+)
