87793e7eb7d9ab7c106a05637a5a6d909af8d6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:34 +0000

    Add package demo-package-2025/0.1/demo_package_2025-0.1-py3-none-any.whl

---
+++ packages/demo-package-2025/demo_package_2025-0.1-py3-none-any.whl/demo_package_2025/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+
+print("demo_package_2025 package is loaded.")
---
+++ packages/demo-package-2025/demo_package_2025-0.1-py3-none-any.whl/demo_package_2025/calculator.py
@@ -0,0 +1,10 @@
+# calculator.py
+
+def add_numbers(a, b):
+    """
+    두 숫자를 더하는 함수
+    :param a: 첫 번째 숫자
+    :param b: 두 번째 숫자
+    :return: 두 숫자의 합
+    """
+    return a + b
