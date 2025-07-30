d9164b5b8f2dcc89f7b4381b110029ffb3cbca0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:21:53 +0000

    Add package pabutools/1.1.16/pabutools-1.1.16-py3-none-any.whl

---
+++ packages/pabutools/pabutools-1.1.16-py3-none-any.whl/pabutools/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Simon Rey, Grzegorz Pierczyński, Markus Utke and Piotr Skowron"
+__email__ = "simon.j.rey@proton.me"
+__version__ = "1.1.16"
+
+
+import logging
+
+logging.getLogger('pabutools').addHandler(logging.NullHandler())
