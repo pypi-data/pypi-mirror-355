cea66a91e3cb362a842a71c59e67558054e66a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:01 +0000

    Add package typemapping/0.1.0/typemapping-0.1.0-py3-none-any.whl

---
+++ packages/typemapping/typemapping-0.1.0-py3-none-any.whl/typemapping/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "VarTypeInfo",
+    "get_field_type",
+    "get_func_args",
+    "is_Annotated",
+    "map_dataclass_fields",
+    "map_func_args",
+    "map_model_fields",
+    "map_init_field",
+    "map_return_type",
+]
+
+
+from typemapping.typemapping import (
+    VarTypeInfo,
+    get_field_type,
+    get_func_args,
+    is_Annotated,
+    map_dataclass_fields,
+    map_func_args,
+    map_init_field,
+    map_model_fields,
+    map_return_type,
+)
