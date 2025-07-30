caa095e89b6961c7a2cdb7082ee98b2f4cdab811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:40 +0000

    Add package validatrix/0.2.71/validatrix-0.2.71.tar.gz

---
+++ packages/validatrix/validatrix-0.2.71.tar.gz/validatrix-0.2.71/validatrix/internal_thermistor_emulation/__init__.py
@@ -0,0 +1,10 @@
+"""
+Digital read  Interface module for Validatrix.
+Provides functionality for Digital read operations.
+"""
+
+from .internal_thermistor_emulation_interface import *
+
+__all__ = [
+    'Internal_ThermistorEmulationInterface',    
+] 
