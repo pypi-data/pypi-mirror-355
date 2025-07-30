c8a8eaf5cba6974bbc4fc9e7c022b645a5ecd635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:35 +0000

    Add package sagea/0.1.0a7/sagea-0.1.0a7.tar.gz

---
+++ packages/sagea/sagea-0.1.0a7.tar.gz/sagea-0.1.0a7/sagea/pysrc/data_collection/collect_auxiliary.py
@@ -0,0 +1,43 @@
+#!/use/bin/env python
+# coding=utf-8
+# @Author  : Shuhao Liu
+# @Time    : 2025/6/16 21:01 
+# @File    : collect_auxiliary.py
+import pathlib
+import os
+import ssl
+
+import wget
+
+from sagea.pysrc.auxiliary.FileTool import FileTool
+from sagea.pysrc.data_collection.processing_bar import bar
+
+
+def collect_auxiliary(local_dir):
+    """download and unzip auxiliary and validation data from https://zenodo.org/records/15675307."""
+
+    """prepare"""
+    local_dir = pathlib.Path(local_dir)
+
+    """download data"""
+    url = f"https://zenodo.org/records/15675307/files/sagea_data.zip?download=1"
+
+    local_path = local_dir / "sagea_data.zip"
+
+    print(f"downloading: {url}")
+
+    wget.download(url, str(local_path), bar=bar)
+
+    print()
+
+    print(f"unzipping: {local_path} ...")
+    """unzip"""
+
+    FileTool.un_zip(local_path, local_dir)
+
+    os.remove(local_path)
+    print("done!\n")
+
+
+if __name__ == "__main__":
+    pass
