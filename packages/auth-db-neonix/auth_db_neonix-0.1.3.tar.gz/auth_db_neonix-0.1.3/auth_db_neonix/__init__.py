4a3535705a3268c55c3ee82d207a51a64070f42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:38 +0000

    Add package auth-db-neonix/0.1.3/auth_db_neonix-0.1.3.tar.gz

---
+++ packages/auth-db-neonix/auth_db_neonix-0.1.3.tar.gz/auth_db_neonix-0.1.3/auth_db_neonix/__init__.py
@@ -0,0 +1,36 @@
+from .security.auth import login, signup, signup, verify_cookie_from_module, verify_cookie_from_https
+from .security.cripto import encrypt, decrypt
+
+from .services.fire_client_service import FirebaseClient
+from .services.sqlite_service import SQLiteManager
+from .services.data_retriver_service import DataRetriever
+from .services.firebase_init import firebase_init
+
+from .models.user import User
+from .dto.user_settings_dto import DbSetting
+from .dto.base_settings_dto import BaseSettingsDto
+import main
+
+# TODOs:
+# TODO - Add session management helper for web frameworks (e.g., Flask, FastAPI)
+# TODO - Implement caching layer for Firebase reads
+# TODO - Add CLI wrapper for auth and settings commands
+# TODO - Add function to list all settings for a given user
+# TODO - Provide unified exception handling and logging utils
+
+__all__ = [
+    "login",
+    "signup",
+    "verify_cookie_from_module",
+    "verify_cookie_from_https",
+    "FirebaseClient",
+    "SQLiteManager",
+    "DataRetriever",
+    "User",
+    "DbSetting",
+    "BaseSettingsDto",
+    "encrypt",
+    "decrypt",
+    "firebase_init",
+    "main"
+]
