918c9b97d5689591b44e31719490eccea08c8f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:02 +0000

    Add package apek/0.0.3a2/apek-0.0.3a2.tar.gz

---
+++ packages/apek/apek-0.0.3a2.tar.gz/apek-0.0.3a2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-module-docstring
+
+
+
+from setuptools import setup, find_packages
+
+f1 = open("README.md", "r", encoding="utf-8")
+
+setup(
+    name = "apek",
+    version = "0.0.3a2",
+    author = "Apeking1819",
+    description = "A module for creating large numbers.",
+    long_description = f1.read(),
+    long_description_content_type = "text/markdown",
+    packages = find_packages(),
+    python_requires = '>=3.12',
+    install_requires = [
+        "rich>=14.0.0"
+    ]
+)
+
+f1.close()
