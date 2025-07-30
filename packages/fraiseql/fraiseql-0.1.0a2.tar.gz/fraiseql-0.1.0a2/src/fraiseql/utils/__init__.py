f8ad3152c76577fd75e87ea016efee0eca1cfccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:40 +0000

    Add package fraiseql/0.1.0a2/fraiseql-0.1.0a2.tar.gz

---
+++ packages/fraiseql/fraiseql-0.1.0a2.tar.gz/fraiseql-0.1.0a2/src/fraiseql/utils/__init__.py
@@ -0,0 +1,10 @@
+"""IP-related utility functions and helpers.
+
+This package provides utility functions for handling IP addresses and
+networks. It includes helper functions for validating, formatting, and
+manipulating IP addresses.
+
+Modules:
+--------
+- ip_utils: Contains the main functions for working with IP addresses.
+"""
