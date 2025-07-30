9531309fce3554722e2aa79fea57ef29a24f3d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:58 +0000

    Add package hack4u-test02726/5.1.0/hack4u_test02726-5.1.0.tar.gz

---
+++ packages/hack4u-test02726/hack4u_test02726-5.1.0.tar.gz/hack4u_test02726-5.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hack4u_test02726",
+    version="5.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Pablo Valero Martinez",
+    description="Una biblioteca para consultar cursos de hack4u",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hack4u.io",
+)
+
