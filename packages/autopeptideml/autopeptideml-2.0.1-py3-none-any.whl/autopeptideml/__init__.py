5ddd3d050fe173cbf120aa631d7e9d0aacba14d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:21:22 +0000

    Add package autopeptideml/2.0.1/autopeptideml-2.0.1-py3-none-any.whl

---
+++ packages/autopeptideml/autopeptideml-2.0.1-py3-none-any.whl/autopeptideml/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for AutoPeptideML."""
+
+__author__ = """Raul Fernandez-Diaz"""
+__email__ = 'raul.fernandezdiaz@ucdconnect.ie'
+# __all__ = ['AutoPeptideML', '__version__', '']
+
+from .autopeptideml import AutoPeptideML, __version__
