0367c785333134096aa4d067a327ce2d6ad3c40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:32 +0000

    Add package chatgraph/0.6.3/chatgraph-0.6.3-py3-none-any.whl

---
+++ packages/chatgraph/chatgraph-0.6.3-py3-none-any.whl/chatgraph/bot/chatbot_model.py
@@ -0,0 +1,236 @@
+import inspect
+from functools import wraps
+from logging import debug, error
+import asyncio
+import re
+
+from ..error.chatbot_error import ChatbotMessageError
+from ..messages.message_consumer import MessageConsumer
+from ..types.request_types import UserCall
+from ..types.message_types import Message, Button
+from ..types.end_types import (
+    RedirectResponse,
+    EndChatResponse,
+    TransferToHuman,
+    TransferToMenu,
+)
+from ..types.route import Route
+from .chatbot_router import ChatbotRouter
+from ..types.background_task import BackgroundTask
+from .default_functions import voltar
+
+DEFAULT_FUNCTION: dict[str, callable] = {
+    r"^\s*(voltar)\s*$": voltar,
+}
+
+
+class ChatbotApp:
+    """
+    Classe principal para a aplicação do chatbot, gerencia as rotas e a lógica de processamento de mensagens.
+    """
+
+    def __init__(
+        self,
+        message_consumer: MessageConsumer = None,
+        default_functions: dict[str, callable] = DEFAULT_FUNCTION,
+    ):
+        """
+        Inicializa a classe ChatbotApp com um estado de usuário e um consumidor de mensagens.
+
+        Args:
+            message_consumer (MessageConsumer): O consumidor de mensagens que lida com a entrada de mensagens no sistema.
+            default_functions (dict[str, callable]): Dicionário de funções padrão que podem ser usadas antes das rotas.
+        """
+        if not message_consumer:
+            message_consumer = MessageConsumer.load_dotenv()
+
+        self.default_functions = default_functions
+        self.__message_consumer = message_consumer
+        self.__routes = {}
+
+    def include_router(self, router: ChatbotRouter):
+        """
+        Inclui um roteador de chatbot com um prefixo nas rotas da aplicação.
+
+        Args:
+            router (ChatbotRouter): O roteador contendo as rotas a serem adicionadas.
+        """
+        self.__routes.update(router.routes)
+
+    def route(self, route_name: str):
+        """
+        Decorador para adicionar uma função como uma rota na aplicação do chatbot.
+
+        Args:
+            route_name (str): O nome da rota para a qual a função deve ser associada.
+
+        Returns:
+            function: O decorador que adiciona a função à rota especificada.
+        """
+        route_name = route_name.strip().lower()
+
+        def decorator(func):
+            params = {}
+            signature = inspect.signature(func)
+            output_param = signature.return_annotation
+
+            for name, param in signature.parameters.items():
+                param_type = (
+                    param.annotation
+                    if param.annotation != inspect.Parameter.empty
+                    else "Any"
+                )
+                params[param_type] = name
+                debug(f"Parameter: {name}, Type: {param_type}")
+
+            self.__routes[route_name] = {
+                "function": func,
+                "params": params,
+                "return": output_param,
+            }
+
+            @wraps(func)
+            async def wrapper(*args, **kwargs):
+                return await func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    def start(self):
+        """
+        Inicia o consumo de mensagens pelo chatbot, processando cada mensagem recebida.
+        """
+        self.__message_consumer.reprer()
+        asyncio.run(self.__message_consumer.start_consume(self.process_message))
+
+    async def process_message(self, userCall: UserCall):
+        """
+        Processa uma mensagem recebida, identificando a rota correspondente e executando a função associada.
+
+        Args:
+            userCall (UserCall): A mensagem a ser processada.
+
+        Raises:
+            ChatbotMessageError: Se nenhuma rota for encontrada para o menu atual do usuário.
+        """
+        user_id = userCall.user_id
+        route = userCall.route.lower()
+        route_handler = route.split(".")[-1]
+
+        matchDefault = False
+
+        for regex, func in self.default_functions.items():
+            if re.match(regex, userCall.content_message):
+                matchDefault = True
+                debug(f"Função padrão encontrada: {func.__name__} para a rota {route}")
+                handler = {
+                    "function": func,
+                    "params": {UserCall: "userCall", Route: "route"},
+                }
+                break
+
+        if not matchDefault:
+            handler = self.__routes.get(route_handler, None)
+
+        if not handler:
+            raise ChatbotMessageError(user_id, f"Rota não encontrada para {route}!")
+
+        func = handler["function"]
+        userCall_name = handler["params"].get(UserCall, None)
+        route_state_name = handler["params"].get(Route, None)
+
+        kwargs = {}
+        if userCall_name:
+            kwargs[userCall_name] = userCall
+        if route_state_name:
+            kwargs[route_state_name] = Route(route, list(self.__routes.keys()))
+
+        if asyncio.iscoroutinefunction(func):
+            userCall_response = await func(**kwargs)
+        else:
+            loop = asyncio.get_running_loop()
+            userCall_response = await loop.run_in_executor(None, lambda: func(**kwargs))
+
+        if matchDefault:
+            userCall.content_message = ""
+
+        if isinstance(userCall_response, (list, tuple)):
+            for response in userCall_response:
+                await self.__process_func_response(response, userCall, route=route)
+        else:
+            await self.__process_func_response(userCall_response, userCall, route=route)
+
+    async def __process_func_response(
+        self, userCall_response, userCall: UserCall, route: str
+    ):
+        """
+        Processa a resposta de uma função associada a uma rota, enviando mensagens ou ajustando estados.
+
+        Args:
+            userCall_response: A resposta gerada pela função da rota.
+            userCall (UserCall): O objeto UserCall associado à mensagem processada.
+            route (str): O nome da rota atual.
+        """
+        loop = asyncio.get_running_loop()
+
+        if isinstance(userCall_response, (str, float, int)):
+            # Envia o resultado como mensagem (executando a chamada síncrona no executor)
+            await loop.run_in_executor(None, userCall.send, Message(userCall_response))
+            return
+
+        elif isinstance(userCall_response, Route):
+            userCall.route = userCall_response.current
+            return
+
+        elif isinstance(userCall_response, (Message, Button)):
+            # Envia o objeto Message ou Button
+            await loop.run_in_executor(None, userCall.send, userCall_response)
+            return
+
+        elif isinstance(userCall_response, EndChatResponse):
+            await loop.run_in_executor(
+                None,
+                userCall.end_chat,
+                userCall_response.observations,
+                userCall_response.tabulation_id,
+                userCall_response.tabulation_name,
+            )
+            return
+
+        elif isinstance(userCall_response, TransferToHuman):
+            await loop.run_in_executor(
+                None,
+                userCall.transfer_to_human,
+                userCall_response.observations,
+                userCall_response.campaign_id,
+                userCall_response.campaign_name,
+            )
+            return
+
+        elif isinstance(userCall_response, TransferToMenu):
+            await loop.run_in_executor(
+                None,
+                userCall.transfer_to_menu,
+                userCall_response.menu,
+                userCall_response.user_message,
+            )
+            return
+
+        elif isinstance(userCall_response, RedirectResponse):
+            route = route + "." + userCall_response.route
+            userCall.route = route
+            await self.process_message(userCall)
+
+        elif not userCall_response:
+            route = route + "." + route.split(".")[-1]
+            userCall.route = route
+            return
+
+        elif isinstance(userCall_response, BackgroundTask):
+            response = await userCall_response.run()
+            await self.__process_func_response(response, userCall, route=route)
+
+        else:
+            error("Tipo de retorno inválido!")
+            return None
---
+++ packages/chatgraph/chatgraph-0.6.3-py3-none-any.whl/chatgraph/messages/message_consumer.py
@@ -0,0 +1,150 @@
+import json
+import asyncio
+from logging import debug, info
+import os
+import aio_pika
+from typing import Callable
+from ..auth.credentials import Credential
+from ..types.request_types import UserCall, UserState, ChatID
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+from rich.panel import Panel
+from urllib.parse import quote
+
+class MessageConsumer:
+    def __init__(
+        self,
+        credential: Credential,
+        amqp_url: str,
+        grpc_uri: str,
+        queue_consume: str,
+        prefetch_count: int = 1,
+        virtual_host: str = "/",
+    ) -> None:
+        self.__virtual_host = virtual_host
+        self.__prefetch_count = prefetch_count
+        self.__queue_consume = queue_consume
+        self.__amqp_url = amqp_url
+        self.__grpc_uri = grpc_uri
+        self.__credentials = credential
+
+    @classmethod
+    def load_dotenv(
+        cls,
+        user_env: str = "RABBIT_USER",
+        pass_env: str = "RABBIT_PASS",
+        uri_env: str = "RABBIT_URI",
+        queue_env: str = "RABBIT_QUEUE",
+        prefetch_env: str = "RABBIT_PREFETCH",
+        vhost_env: str = "RABBIT_VHOST",
+        grpc_uri: str = "GRPC_URI",
+    ) -> "MessageConsumer":
+        username = os.getenv(user_env)
+        password = os.getenv(pass_env)
+        url = os.getenv(uri_env)
+        queue = os.getenv(queue_env)
+        prefetch = os.getenv(prefetch_env, 1)
+        vhost = os.getenv(vhost_env, "/")
+        grpc = os.getenv(grpc_uri)
+
+        if not username or not password or not url or not queue or not grpc:
+            raise ValueError("Corrija as variáveis de ambiente!")
+
+        return cls(
+            credential=Credential(username=username, password=password),
+            amqp_url=url,
+            queue_consume=queue,
+            prefetch_count=int(prefetch),
+            virtual_host=vhost,
+            grpc_uri=grpc,
+        )
+
+    async def start_consume(self, process_message: Callable):
+        try:
+            user = quote(self.__credentials.username)
+            pwd = quote(self.__credentials.password)
+            vhost = quote(self.__virtual_host)
+            uri = self.__amqp_url
+            amqp_url = f"amqp://{user}:{pwd}@{uri}/{vhost}"
+            connection = await aio_pika.connect_robust(amqp_url)
+
+            async with connection:
+                channel = await connection.channel()
+                await channel.set_qos(prefetch_count=self.__prefetch_count)
+                
+                try:
+                    queue = await channel.get_queue(self.__queue_consume, ensure=True)
+                except aio_pika.exceptions.ChannelNotFoundEntity:
+                    arguments = {
+                        "x-dead-letter-exchange": "log_error",  # Dead Letter Exchange
+                        "x-expires": 86400000,                 # Expiração da fila (em milissegundos)
+                        "x-message-ttl": 300000                # Tempo de vida das mensagens (em milissegundos)
+                    }
+                    queue = await channel.declare_queue(self.__queue_consume, durable=True, arguments=arguments)
+                
+                info("[x] Server inicializado! Aguardando solicitações RPC")
+
+                async for message in queue:
+                    async with message.process():
+                        await self.on_request(message.body, process_message)
+        except Exception as e:
+            print(f"Erro durante o consumo de mensagens: {e}")
+            # Reiniciar a conexão em caso de falha
+            # await self.start_consume(process_message)
+
+    async def on_request(self, body: bytes, process_message: Callable):
+        try:
+            message = body.decode()
+            message_json = json.loads(message)
+            pure_message = self.__transform_message(message_json)
+            await process_message(pure_message)
+        except Exception as e:
+            print(f"Erro ao processar mensagem: {e}")
+
+    def __transform_message(self, message: dict) -> UserCall:
+        user_state = message.get("user_state", {})
+        observation = user_state.get("observation", {})
+        if isinstance(observation, str):
+            observation = json.loads(observation)
+
+        usercall = UserCall(
+            user_state=UserState(
+                chatID=ChatID(
+                    user_id=user_state['chat_id'].get("user_id", ""),
+                    company_id=user_state['chat_id'].get("company_id", ""),
+                ),
+                menu=user_state.get("menu", ""),
+                route=user_state.get("route", ""),
+                protocol=user_state.get("protocol", ""),
+                observation=observation,
+            ),
+            type_message=message.get("type_message", ""),
+            content_message=message.get("content_message", ""),
+            grpc_uri=self.__grpc_uri,
+        )
+        
+        return usercall
+
+    def reprer(self):
+        console = Console()
+
+        title_text = Text("ChatGraph", style="bold red", justify="center")
+        title_panel = Panel.fit(title_text, title=" ", border_style="bold red", padding=(1, 4))
+
+        separator = Text("🐇🐇🐇 RabbitMessageConsumer 📨📨📨", style="cyan", justify="center")
+
+        table = Table(show_header=True, header_style="bold magenta", title="RabbitMQ Consumer")
+        table.add_column("Atributo", justify="center", style="cyan", no_wrap=True)
+        table.add_column("Valor", justify="center", style="magenta")
+
+        table.add_row("Virtual Host", self.__virtual_host)
+        table.add_row("Prefetch Count", str(self.__prefetch_count))
+        table.add_row("Queue Consume", self.__queue_consume)
+        table.add_row("AMQP URL", self.__amqp_url)
+        table.add_row("Username", self.__credentials.username)
+        table.add_row("Password", "******")
+
+        console.print(title_panel, justify="center")
+        console.print(separator, justify="center")
+        console.print(table, justify="center")
