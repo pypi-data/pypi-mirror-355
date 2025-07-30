ca956fe872015204e868ade86d873ffd29c409a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:59 +0000

    Add package cave-infra/0.1.1/cave_infra-0.1.1.tar.gz

---
+++ packages/cave-infra/cave_infra-0.1.1.tar.gz/cave_infra-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+project_folder = "cave"
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(
+    name='cave-infra',
+    version='0.1.1',
+    packages=find_packages(),
+    license='MIT',
+    description='Automation toolkit for automated provisioning virtual infrastructure.',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "Jinja2==3.1.6",
+        "libvirt-python==11.4.0",
+        "MarkupSafe==3.0.2"
+    ],
+    url='https://github.com/sn0ja/cave/',
+    author='sn0ja',
+)
