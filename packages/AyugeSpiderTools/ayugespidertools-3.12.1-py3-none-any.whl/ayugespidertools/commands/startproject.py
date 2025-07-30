8fe9c6c0c93a45c684db887d2a84983cb30f5c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:44 +0000

    Add package AyugeSpiderTools/3.12.1/ayugespidertools-3.12.1-py3-none-any.whl

---
+++ packages/AyugeSpiderTools/ayugespidertools-3.12.1-py3-none-any.whl/ayugespidertools/commands/startproject.py
@@ -0,0 +1,24 @@
+import argparse
+from pathlib import Path
+
+from scrapy.commands.startproject import Command
+
+import ayugespidertools
+
+
+class AyuCommand(Command):
+    def run(self, args: list[str], opts: argparse.Namespace) -> None:
+        # 若想自定义 TEMPLATES_TO_RENDER 的文件模版，需重写父类的 run 方法，示例请查看提交历史
+        super().run(args, opts)
+        print("Or you can start your first spider with ayuge:")
+        print("    ayuge genspider example example.com")
+
+    @property
+    def templates_dir(self) -> str:
+        assert self.settings is not None
+        return str(
+            Path(
+                Path(ayugespidertools.__path__[0], "templates"),
+                "project",
+            )
+        )
