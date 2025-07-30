ac3517a1e868f5165e9712211be86d72007ee545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:46 +0000

    Add package my-cloud-devops-consulting/1.1.202506162252/my-cloud-devops-consulting-1.1.202506162252.tar.gz

---
+++ packages/my-cloud-devops-consulting/my-cloud-devops-consulting-1.1.202506162252.tar.gz/my-cloud-devops-consulting-1.1.202506162252/test/test.py
@@ -0,0 +1,16 @@
+# grocery_list =["milk", "bread", "eggs", "cheese", "apples"]
+# print(grocery_list)
+# grocery_list.append("chocolate")
+# print(grocery_list)
+# del(grocery_list[1])
+# print(grocery_list)
+# print(len(grocery_list))
+# grocery_list.sort()
+# print(grocery_list)
+# print(grocery_list[0])
+# print(grocery_list[4])
+# print(grocery_list[-1])  # last item
+list_numbers =[10,2,45,1,2,4,5,7,9,2,4,790]
+list_numbers.sort()
+list_numbers.reverse()
+print(list_numbers)
