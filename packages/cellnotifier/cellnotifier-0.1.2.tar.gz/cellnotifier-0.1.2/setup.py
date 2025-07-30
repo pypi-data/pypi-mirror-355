541d8ad7fbda1cfb09cc5facdb19682e4f3d959a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:13 +0000

    Add package cellnotifier/0.1.2/cellnotifier-0.1.2.tar.gz

---
+++ packages/cellnotifier/cellnotifier-0.1.2.tar.gz/cellnotifier-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='cellnotifier',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your@email.com',
+    description='Notify when a cell or process finishes running',
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
