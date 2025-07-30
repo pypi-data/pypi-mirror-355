f4336368f12c17660d2aeb734744721998c56f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:17 +0000

    Add package hiediff/0.1.0/hiediff-0.1.0.tar.gz

---
+++ packages/hiediff/hiediff-0.1.0.tar.gz/hiediff-0.1.0/HieDiff/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+
+
+from ._spatial_reconstruction import spatial_reconstruction
+from ._HieDiff import run_HieDiff
+from ._HieDiff_minibatch import run_HieDiff_minibatch
+from ._regulons import regulons, aucell
+from ._utils import refine_spatial_domains
+from ._heatmap import heatmap_aucell
+from ._scatterplots import embedding_aucell
+from ._spatial import spatial_aucell
+
+
+
+
+
+
+
+
+
---
+++ packages/hiediff/hiediff-0.1.0.tar.gz/hiediff-0.1.0/README.md
@@ -0,0 +1,5 @@
+# HieDiff
+
+HieDiff (Hierarchical flow Diffusion foundation model) is a python package for spatial transcriptomics analysis.
+
+Please visit [HieDiff documentation website](https://hiediff.readthedocs.io/) for details about installation, tutorials, API and references.
