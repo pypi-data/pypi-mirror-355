694bacc804390b004467703775d9321efe64490e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:46 +0000

    Add package y360-orglib/0.0.8/y360_orglib-0.0.8-py3-none-any.whl

---
+++ packages/y360-orglib/y360_orglib-0.0.8-py3-none-any.whl/y360_orglib/disk/__init__.py
@@ -0,0 +1,20 @@
+"""
+Disk Client module for interacting with the Y360 Disk
+
+Details https://yandex.ru/dev/disk-api/doc/ru/
+"""
+
+__version__ = "0.1.0"
+
+from y360_orglib.disk.disk_client import DiskClient
+from y360_orglib.disk.models import PublicResourcesList, Resource, BaseAccess, UserAccess, MacroAccess, PublicSettings
+
+__all__ = [
+    "DiskClient",
+    "PublicResourcesList",
+    "Resource",
+    "BaseAccess",
+    "UserAccess",
+    "MacroAccess",
+    "PublicSettings",
+    ]
