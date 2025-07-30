d5f997b268eafd7cc7f36930aad379bf66c1daef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:44 +0000

    Add package bvhTools/1.2.3/bvhtools-1.2.3.tar.gz

---
+++ packages/bvhTools/bvhtools-1.2.3.tar.gz/bvhtools-1.2.3/src/bvhTools/testingPurposes.py
@@ -0,0 +1,11 @@
+from bvhIO import readBvh, writeBvh, writeBvhToCsv, writePositionsToCsv
+from bvhManipulation import centerSkeletonRoot, centerSkeletonFeet, centerSkeletonAroundJoint, rotateSkeletonWorld, rotateSkeletonLocal, centerSkeletonXZ, moveSkeleton
+from bvhSlicer import getBvhSlices, groupBvhSlices, appendBvhSlices
+from bvhVisualizerMpl import showBvhAnimation
+from bvhSkeletonEditor import scaleSkeleton
+if __name__ == "__main__":
+    bvhData = readBvh("testBvhFiles/test.bvh")
+    bvhDataNew = scaleSkeleton(bvhData, 1.1)
+    writeBvh(bvhDataNew, "testBvhFiles/testScaled.bvh")
+    # bvhData.setRestPose({'LeftLeg': [ 1.31714436e-05, 90, -9.00000553e+01]})
+    # writeBvh(bvhData, "testBvhFiles/testNewRest.bvh")
