aca73b3b0756047a62733679874a7c0acfc123b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:18 +0000

    Add package batchfactory/0.5.0/batchfactory-0.5.0-py3-none-any.whl

---
+++ packages/batchfactory/batchfactory-0.5.0-py3-none-any.whl/batchfactory/__init__.py
@@ -0,0 +1,7 @@
+from .core import *
+from . import op
+from . import brokers
+from .lib.utils import format_number, hash_text
+from .lib import base64_utils
+from .lib.llm_backend import LLMMessage, LLMRequest, LLMResponse, LLMTokenCounter
+from .lib.prompt_maker import PromptMaker, BasicPromptMaker
