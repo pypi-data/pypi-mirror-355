f985d0c8fd375885b622dde34e0d4c5597927a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:07 +0000

    Add package cognite-toolkit/0.5.46/cognite_toolkit-0.5.46.tar.gz

---
+++ packages/cognite-toolkit/cognite_toolkit-0.5.46.tar.gz/cognite_toolkit-0.5.46/cognite_toolkit/_cdf_tk/builders/__init__.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from ._base import Builder, DefaultBuilder, get_loader
+from ._datamodels import DataModelBuilder
+from ._file import FileBuilder
+from ._function import FunctionBuilder
+from ._location import LocationBuilder
+from ._raw import RawBuilder
+from ._streamlit import StreamlitBuilder
+from ._transformation import TransformationBuilder
+
+
+def create_builder(
+    resource_folder: str,
+    build_dir: Path | None,
+) -> Builder:
+    if builder_cls := _BUILDER_BY_RESOURCE_FOLDER.get(resource_folder):
+        return builder_cls(build_dir)  # type: ignore[abstract]
+
+    return DefaultBuilder(build_dir, resource_folder)
+
+
+_BUILDER_BY_RESOURCE_FOLDER = {_builder._resource_folder: _builder for _builder in Builder.__subclasses__()}
+__all__ = [
+    "Builder",
+    "DataModelBuilder",
+    "DefaultBuilder",
+    "FileBuilder",
+    "FunctionBuilder",
+    "LocationBuilder",
+    "RawBuilder",
+    "StreamlitBuilder",
+    "TransformationBuilder",
+    "create_builder",
+    "get_loader",
+]
---
+++ packages/cognite-toolkit/cognite_toolkit-0.5.46.tar.gz/cognite_toolkit-0.5.46/last_changelog_entry.md
@@ -0,0 +1,10 @@
+## cdf 
+
+### Fixed
+
+- Running `cdf deploy` on Windows will no longer give a difference hash
+than on Unix/Mac.
+
+## templates
+
+No changes.
