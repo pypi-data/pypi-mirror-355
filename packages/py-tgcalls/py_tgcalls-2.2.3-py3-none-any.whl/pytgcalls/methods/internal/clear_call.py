7f2b66e1004aded4a54d9e0db529c43e30bf23d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:17 +0000

    Add package py-tgcalls/2.2.3/py_tgcalls-2.2.3-py3-none-any.whl

---
+++ packages/py-tgcalls/py_tgcalls-2.2.3-py3-none-any.whl/pytgcalls/methods/internal/clear_call.py
@@ -0,0 +1,17 @@
+from ntgcalls import ConnectionNotFound
+from ntgcalls import TelegramServerError
+
+from ...scaffold import Scaffold
+
+
+class ClearCall(Scaffold):
+    async def _clear_call(self, chat_id: int):
+        if chat_id in self._wait_connect:
+            self._wait_connect[chat_id].set_exception(
+                TelegramServerError(),
+            )
+        try:
+            await self._binding.stop(chat_id)
+        except ConnectionNotFound:
+            pass
+        self._clear_cache(chat_id)
