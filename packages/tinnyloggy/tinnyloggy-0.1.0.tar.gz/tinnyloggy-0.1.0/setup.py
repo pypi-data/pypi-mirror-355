65632771909225a325b7ff6a068ca3d83b51e0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:21 +0000

    Add package tinnyloggy/0.1.0/tinnyloggy-0.1.0.tar.gz

---
+++ packages/tinnyloggy/tinnyloggy-0.1.0.tar.gz/tinnyloggy-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tinnyloggy',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'colorama'
+    ],
+    author='Aydyn Maxadov',
+    description='Lightweight terminal logger with timestamps and optional caller inspection.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/loggerx',  # если есть
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
