551f5659dfcf8032c2048b0d3cebbb178d0580dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:48 +0000

    Add package intelliopsopenfgasdk-python/0.1.4/intelliopsopenfgasdk_python-0.1.4.tar.gz

---
+++ packages/intelliopsopenfgasdk-python/intelliopsopenfgasdk_python-0.1.4.tar.gz/intelliopsopenfgasdk_python-0.1.4/src/intelliopsopenfgasdk_python/__init__.py
@@ -0,0 +1,6 @@
+from .http_client import HttpClient
+from .models import CreateFgaModel, CreateDataSourceModel, CheckAccessModel
+
+__all__ = ["HttpClient", "CreateFgaModel", "CreateDataSourceModel", "CheckAccessModel"]
+__version__ = "0.1.1"
+__author__ = "IntelliOps"
