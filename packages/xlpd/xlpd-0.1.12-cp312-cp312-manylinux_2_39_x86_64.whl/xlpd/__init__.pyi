adbf975aae67d3248658d5234f9fc2d5123ca17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:15 +0000

    Add package xlpd/0.1.12/xlpd-0.1.12-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/xlpd/xlpd-0.1.12-cp312-cp312-manylinux_2_39_x86_64.whl/xlpd/__init__.pyi
@@ -0,0 +1,11 @@
+import numpy as np
+
+def vector_add(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """
+    vector_add
+    """
+
+def color_to_grayscale(color: np.ndarray) -> np.ndarray:
+    """
+    color to grayscale
+    """
