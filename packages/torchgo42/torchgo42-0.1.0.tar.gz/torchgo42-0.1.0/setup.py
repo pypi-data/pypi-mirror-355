1b5f2d27a3cacdf85b64a8702d1f2bb5240ebc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:59 +0000

    Add package torchgo42/0.1.0/torchgo42-0.1.0.tar.gz

---
+++ packages/torchgo42/torchgo42-0.1.0.tar.gz/torchgo42-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchgo42",
+    version="0.1.0",
+    author="asd dasd",
+    description="zxczxczxc",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["pyperclip", "mistralai"], 
+)
