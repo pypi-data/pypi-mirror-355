16f3538d1a70e8e220f6dceb3313b70fcd2dbaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:09:05 +0000

    Add package whot/0.0.6/whot-0.0.6.tar.gz

---
+++ packages/whot/whot-0.0.6.tar.gz/whot-0.0.6/whot/__init__.py
@@ -0,0 +1,4 @@
+from .deck import Deck, Card, Suit
+from .player import Player
+from .game import Engine as Whot
+from .game import TestEngine as TestWhot
---
+++ packages/whot/whot-0.0.6.tar.gz/whot-0.0.6/whot/player.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, asdict
+
+from .deck import Card
+
+
+class Player:
+    """
+    A player has:
+    1. an id
+    2. cards
+    3. a method to transfer card(s)
+    4. a method to recieve card(s)
+    5. print cards
+    """
+
+    def __init__(self, player_id):
+        self._cards: list[Card] = []
+        self.player_id: str = player_id
+
+    def transfer(self, n):
+        card = self._cards[n]
+        self._cards.remove(card)
+        return card
+    
+    def cards(self):
+        card_list = []
+        for card in self._cards:
+            card_list.append(asdict(card))
+        
+        return card_list
+
+    def receive(self, card: list[Card]):
+        self._cards.extend(card)
+
+    def disp(self):
+        print(self._cards)
+    
+    def asdict(self):
+        return { "player_id" : self.player_id, "cards": self.cards() }
+
+    def __repr__(self):
+        return f"{self.player_id}"
+
