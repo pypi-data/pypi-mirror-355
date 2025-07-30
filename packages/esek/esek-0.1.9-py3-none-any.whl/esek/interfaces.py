e2533d5af662f16035f1d23922503f7646f933df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:20 +0000

    Add package esek/0.1.9/esek-0.1.9-py3-none-any.whl

---
+++ packages/esek/esek-0.1.9-py3-none-any.whl/esek/interfaces.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractTest(ABC):
+    """
+    An abstract base class for statistical tests.
+    This class defines the interface for statistical tests, requiring implementations
+    to provide methods for calculating results from a Z-score, parameters, and data.
+    """
+
+    @abstractmethod
+    def from_z_score(self):
+        """Calculate results from a Z-score."""
+        pass
+
+    def from_t_score(self):
+        """Calculate results from a T-score."""
+        pass
+
+    @abstractmethod
+    def from_parameters(self):
+        """Calculate results from parameters."""
+        pass
+
+    @abstractmethod
+    def from_data(self):
+        """Calculate results from data."""
+        pass
