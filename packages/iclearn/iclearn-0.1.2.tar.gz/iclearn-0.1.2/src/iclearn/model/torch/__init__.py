df6c8f08c3e274156a378ad061bf3c15a744cc66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:16 +0000

    Add package iclearn/0.1.2/iclearn-0.1.2.tar.gz

---
+++ packages/iclearn/iclearn-0.1.2.tar.gz/iclearn-0.1.2/src/iclearn/model/torch/__init__.py
@@ -0,0 +1,6 @@
+from .metrics import *  # NOQA
+from .model import *  # NOQA
+
+from .optimizers import load_optimizer
+
+__all__ = ["load_optimizer"]
---
+++ packages/iclearn/iclearn-0.1.2.tar.gz/iclearn-0.1.2/src/iclearn/model/torch/optimizers.py
@@ -0,0 +1,17 @@
+import torch
+
+from iclearn.model.machine_learning_model import Optimizer
+
+
+def load_optimizer(params, opt: Optimizer):
+
+    if opt.name == "torch.AdamW":
+        return torch.optim.AdamW(params=params, lr=opt.learning_rate)
+    elif opt.name == "torch.Adam":
+        return torch.optim.Adam(params=params, lr=opt.learning_rate)
+    elif opt.name == "torch.SGD":
+        return torch.optim.SGD(
+            params=params, lr=opt.learning_rate, momentum=opt.extra["momentum"]
+        )
+
+    return None
