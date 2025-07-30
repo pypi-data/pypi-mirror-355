0a6bd97573d60948ddb40a33465fb6f61a27cc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:00 +0000

    Add package calco/1.1.0/calco-1.1.0.tar.gz

---
+++ packages/calco/calco-1.1.0.tar.gz/calco-1.1.0/README.md
@@ -0,0 +1,22 @@
+# Calco
+
+Calco is a high-performance, cross-platform math library written in C and exposed as a Python extension.  
+It includes over 60 advanced functions for arithmetic, trigonometry, logarithms, exponentials, special functions, and more.
+
+## Installation
+
+```bash
+pip install calco
+```
+
+Or install a specific `.whl` from the `whl/` folder:
+
+```bash
+pip install whl/calco-1.0.0-cp311-win_amd64.whl
+```
+
+## Features
+
+- Fast C-level execution
+- 60+ math functions
+- Cross-platform compatibility (Windows/Linux/macOS)
---
+++ packages/calco/calco-1.1.0.tar.gz/calco-1.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="calco",
+    version="1.1.0",
+    author="Gafoo",
+    author_email="gafarssprts@gmail.com",
+    description="High-performance math library for Python (powered by C)",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://calcolib.netlify.app",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: C",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ],
+    python_requires='>=3.8',
+    packages=["calco"],
+    include_package_data=True,
+)
