78a7cdd2c8372dc0070227bff9087415f5876b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:23 +0000

    Add package rococo/1.0.35/rococo-1.0.35.tar.gz

---
+++ packages/rococo/rococo-1.0.35.tar.gz/rococo-1.0.35/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rococo',
+    version='1.0.35',
+    packages=find_packages(),
+    url='https://github.com/EcorRouge/rococo',
+    license='MIT',
+    author='Jay Grieves',
+    author_email='jaygrieves@gmail.com',
+    description='A Python library to help build things the way we want them built',
+    entry_points={
+        'console_scripts': [
+            'rococo-mysql = rococo.migrations.mysql.cli:main',
+            'rococo-postgres = rococo.migrations.postgres.cli:main',
+        ],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'DBUtils==3.1.0',
+        'surrealdb==1.0.4',
+        'boto3>=1.28.55',
+        'pika==1.3.2',
+        'python-dotenv==1.0.0',
+        'PyMySQL==1.1.1',
+        'PyMongo==4.6.3',
+        'psycopg2-binary==2.9.10'
+    ],
+    python_requires=">=3.10"
+)
