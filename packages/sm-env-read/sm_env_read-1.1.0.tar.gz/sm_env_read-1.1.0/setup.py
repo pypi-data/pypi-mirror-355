1bf9dd67ce28f84adc9c7805b73e2732bcd65768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:10 +0000

    Add package sm-env-read/1.1.0/sm_env_read-1.1.0.tar.gz

---
+++ packages/sm-env-read/sm_env_read-1.1.0.tar.gz/sm_env_read-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='sm_env_read',
+    version='1.1.0',
+    description='read env secrets from aws secrets manager',
+    author='Deepak M S',
+    author_email='deepakcoder80@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'boto3'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
