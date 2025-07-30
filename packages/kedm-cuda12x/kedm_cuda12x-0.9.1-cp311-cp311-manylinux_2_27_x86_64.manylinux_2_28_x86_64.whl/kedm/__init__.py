50477b2a10ae822be653de5d2fa58e2dd13e8725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:32 +0000

    Add package kedm-cuda12x/0.9.1/kedm_cuda12x-0.9.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/kedm-cuda12x/kedm_cuda12x-0.9.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/kedm/__init__.py
@@ -0,0 +1,21 @@
+"""
+Python bindings for kEDM
+------------------------
+
+.. currentmodule:: kedm
+
+.. autosummary::
+   :toctree: _generate
+
+   ccm
+   edim
+   simplex
+   eval_simplex
+   smap
+   eval_smap
+   xmap
+   get_kokkos_config
+"""
+
+from ._kedm import *
+from ._version import __version__
